4283b42b374d8871b27f81441792be28d5060420
mgoksun <devnull@localhost>
2009-11-12 10:36:41 +0100

    [svn r1401] testcommit

--- toolkit.py
+++ toolkit.py
@@ -1,5 +1,7 @@
 #!/bin/env python2.2
 
+#onzin edit om svn te testen
+
 import types,re,mx.DateTime,sys,time,os,random,math,gzip,pickle,optparse, threading, csv, htmlentitydefs, odict, collections, operator, functools, subprocess, colorsys
 from datetime import datetime
 
