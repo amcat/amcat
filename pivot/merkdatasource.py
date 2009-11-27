9bec04e3e342148aba4bc5c6197ec34abd740100
mgoksun <devnull@localhost>
2009-11-27 17:28:51 +0100

    [svn r1485] merkdatasource

--- pivot/merkdatasource.py
+++ pivot/merkdatasource.py
@@ -1,22 +1,59 @@
-import xapian, dbtoolkit, toolkit
+import xapian, amcatxapian, dbtoolkit, toolkit
 from datasource import DataSource, Mapping, Field
 from itertools import imap, izip
 
 class MerkDataSource(DataSource):
-    def __init__(self, db):
+    def __init__(self, db,indx):
         DataSource.__init__(self,self.createMappings())
         self.db = db
-
+        #self.brand = brand
+        self.index = indx
+        self.brands = ["dsb", "yakult", "melkunie"]  
     def createMappings(self):
-        article = MerkDataField(self, "article",["articles"],"articleid")
-##??        merk = MerkDataField(self, "Merk",["merken"],"merkid")
-
+        article = Field(self, "article")
+        merk = Field(self, "merk")
         return [
-            MerkDataMapping(article, merk),
-            }
+            MerkDataMapping(merk, article)
+            ]
 
-class MerkDataField(Field):
-    def __init__(self, datasource, concept, tables, column):
-        Field.__init__(self, datasource, concept)
-        self.tables = tables
-        self.column = column
+
+class MerkDataMapping(Mapping):
+    def __init__(self, a, b):
+        Mapping.__init__(self, a, b)
+    def map(self, value, reverse):
+        #print a,b
+        #if reverse:
+        print value
+        return self.getMerkenPerArticle(value)
+        #else:
+        #    return self.getArtikelenPerMerk(value)
+    def getMerkenPerArticle(self,value):
+        index = self.a.datasource.index
+        brands = self.a.datasource.brands
+        for brand in brands:
+            art = index.query(brand)
+            for ar in art:
+                if value == str(ar.id):
+                    yield brand
+    def getArtikelenPerMerk(self,value):
+        index = self.a.datasource.index.query(value)
+        for i in index:
+            yield i
+        
+
+if __name__ == '__main__':
+    import dbtoolkit, sys
+    db = dbtoolkit.amcatDB()
+      
+    from mst import getSolution
+    from datasource import FunctionalDataModel
+    import tabulator
+    #filters = {"merk" : ["yakult"] }
+    filters = {"article": ["44134035"]}
+    select = ["article", "merk"]
+    index =  amcatxapian.Index("/home/marcel/amcatindex", dbtoolkit.amcatDB())
+    merk = MerkDataSource(db,index)
+    dm = FunctionalDataModel(getSolution)
+    dm.register(merk)
+    data = tabulator.tabulate(dm,select,filters)
+    print data
