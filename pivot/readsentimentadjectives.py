6ec7a305a95a1a509a1d6c6b727e88ef409281fd
mgoksun <devnull@localhost>
2009-11-30 14:58:55 +0100

    [svn r1495] readsentiments.py

--- pivot/readsentimentadjectives.py
+++ pivot/readsentimentadjectives.py
@@ -4,17 +4,32 @@ file = open("sentiment-adj.txt")
 db = dbtoolkit.amcatDB()
 lines = file.readlines()
 startwords = sys.maxint
+
+#sql = "select * from words_lemmata where pos = 'A'" 
+#res = db.doQuery(sql)
+
 for nr, line in enumerate(lines):
     line = line.strip("\n")
     line = line.split("/")
     if line[0] == "cf-score": 
-        print line ## create table here
         startwords = nr 
     if startwords < nr:
-        print line[0] ## insert lines into database
-        confidence = int(float(line[0])*100)
-        print confidence
-        sql = "insert into words_sentiment ( lemmaid, sentiment, confidence ) values (%s, %s, %s)" % (line[1], line[3], confidence)
-        db.doInsert(sql)
-        #
+        confidence = int(float(line[0].replace(',','.'))*100)
+        sentiment = None
+        if line[3] == 'positive':
+            sentiment = 100
+        elif line[3] == 'negative':
+            sentiment = -100
+        elif line[3] == 'neutral':
+            sentiment = 0
+        else:
+            sentiment = 0
+        sql = "select * from words_lemmata where lemma = '%s' AND pos = 'A'" %(line[1])
+        res = db.doQuery(sql)
+        if res:
+            insertintodb = "insert into words_sentiment values (%s, %s, %s)"% (res[0][0], sentiment, confidence)
+            db.doInsert(insertintodb)
+        else:
+            insertinto
+
 file.close()
