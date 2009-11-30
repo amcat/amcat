77820e1eb749d84fd461e992ad26f2ef083ea10c
mgoksun <devnull@localhost>
2009-11-30 12:25:59 +0100

    [svn r1494] read sentiment adjectives

--- pivot/readsentimentadjectives.py
+++ pivot/readsentimentadjectives.py
@@ -1,10 +1,20 @@
-
+import dbtoolkit, sys
 
 file = open("sentiment-adj.txt")
-
-while 1:
-    line = file.readline()
-    words = line.split("/")
-    print words
-    if not line:
-        break
+db = dbtoolkit.amcatDB()
+lines = file.readlines()
+startwords = sys.maxint
+for nr, line in enumerate(lines):
+    line = line.strip("\n")
+    line = line.split("/")
+    if line[0] == "cf-score": 
+        print line ## create table here
+        startwords = nr 
+    if startwords < nr:
+        print line[0] ## insert lines into database
+        confidence = int(float(line[0])*100)
+        print confidence
+        sql = "insert into words_sentiment ( lemmaid, sentiment, confidence ) values (%s, %s, %s)" % (line[1], line[3], confidence)
+        db.doInsert(sql)
+        #
+file.close()
