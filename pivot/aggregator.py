d8f34527ac23f4b9ac879ada5364739d1f140c8a
mgoksun <devnull@localhost>
2009-12-08 10:22:04 +0100

    [svn r1529] updated aggregator

--- pivot/aggregator.py
+++ pivot/aggregator.py
@@ -1,16 +1,30 @@
+import table3
+
+""" 
+Uitdagingen:
+1) aggregateTable -> table3.Table object teruggeven
+2) sourceinterface.getTable(..) implementeren
+3) association als aggregatecolumn
+"""
+
 def aggregate(table, selectcolumns, aggregatecolumns):
     dict = {}
-    set_keys = set()
     for row in table.getRows():
-        t = tuple(selectcolumns)
-        set_keys.add(t)
-        if set_keys.__contains__() 
+        key = tuple(row[i] for i in selectcolumns)
+        if not dict.has_key(key):
+            vals = tuple(a.getAggregateFunction() for a in aggregatecolumns)
+            dict[key] = vals
+        for col, func in zip(aggregatecolumns, dict[key]):
+            col.running(table, row, func)            
+    for key,funcs in dict.iteritems():
+        values = tuple(col.final(func) for (col, func) in zip(aggregatecolumns, funcs))
+        yield key, values
 
-    aggr = SimpleAggregateColumn(aggregatecolumn)
-    sum = Sum(aggr)
-    
-    return Table()
-
+def getAggregateTable(table, selectcolumns, aggregatecolumns):
+    values = aggregate(table, selectcolumns, aggregatecolumns)
+    aggDict = dict(values)
+    aggTable = table3.Table(aggDict, [tuple((x, y)) for x, y in aggDict.iteritems()])
+    return aggTable
 
 class AggregateFunction(object):
     def running(self, value):
@@ -36,28 +50,70 @@ class Count(AggregateFunction):
             self.n += 1
     def final(self):
         return self.n   
-
-class AggregateColumn(object):
-    def running(self, row, table):
-        abstract
+    
+class Average(AggregateFunction):
+    def __init__(self):
+        self.count = 0
+        self.total = 0
+    def running(self, value):
+        if value is not None:
+            self.count += 1
+            self.total += value
     def final(self):
+        return self.total / float(self.count)
+            
+class AggregateColumn(object):
+    def __init__(self, factory=None):
+        self.factory = factory
+    def getAggregateFunction(self):
+        return self.factory()
+    def running(self, table, row, function):
+        function.running(self.getValue(table, row))
+    def getValue(self, table, row):
         abstract
-        return None
+    def final(self, function):
+        return function.final()
 
 class SimpleAggregateColumn(AggregateColumn):
-    def __init__(self, columns, aggregateFactory):
-        self.columns = columns
-        self.aggregateFactory = aggregateFactory
-    def getAggregate(self):
-        return self.aggregateFactory()
+    def __init__(self, column, aggregateFactory):
+        AggregateColumn.__init__(self, aggregateFactory)
+        self.column = column
     def getValue(self, table, row):
-        return [table.getValue(row, column) for column in self.columns]
-        
+        return table.getValue(row, self.column)
+    
+class ListTable(object):
+    def __init__(self, lst):
+        self.lst = lst
+        self.ncols = len(lst[0]) if lst else 0
+    def getRows(self):
+        return self.lst
+    def getColumns(self):
+        return range(self.ncols)
+    def getValue(self,row,column):
+        return row[column]
+
 if __name__ == '__main__':
-    s = SimpleAggregateColumn([], Count)
-    a = s.getAggregate()
-    print s.getValue(None, None)
-    a.running(s.getValue(None, None))
-    print a.final()
+    print "Test aggregate()"
+    #table = ListTable([[1,2,3,4,5],[3,4,8,9,10],[1,2,2213,53654,234234]])
+    #a = sumaggregate(table, [0,1],[3,4], Average)
+    
+    table = ListTable([[1,1,2],[1,1,5],[1,2,None], [1,2,8], [2,1,4]])
+    
+    #avg = SimpleAggregateColumn([1], Average)
+    a = aggregate(table, [0,1],[SimpleAggregateColumn(2, Sum), SimpleAggregateColumn(2, Count), SimpleAggregateColumn(2, Average)])
+    at = getAggregateTable(table,[0,1],[SimpleAggregateColumn(2, Sum), SimpleAggregateColumn(2, Count), SimpleAggregateColumn(2, Average)])
+    print at.getRows()
+    
+    #print dict(a)
+    #print a
+    for k, v in sorted(a):
+        print k, v
+        
+    print "\nTest simpleaggregatecolumn"
+    s = SimpleAggregateColumn(0, Sum)
+    f = s.getAggregateFunction()
+    for row in table.getRows():
+        s.running(table, row, f)
+    print s.final(f)
     
     
