ec3728a78e32273bbf8835c0f8683d525177a227
wva <devnull@localhost>
2010-04-28 23:13:24 +0200

    [svn r2255] bugfixes, uniform return values

--- query/objectfinder.py
+++ query/objectfinder.py
@@ -2,11 +2,9 @@
 Objectfinder module. Helps using several kinds of indices through one common interface.
 """
 
-import lucenelib
+import lucenelib, article
 
-class ObjectFinder:
-    index = None
-    languageid = None
+class ObjectFinder(object):
     def __init__(self, index, languageid=101):
         self.index = index
         self.languageid = languageid
@@ -18,15 +16,18 @@ class ObjectFinder:
         abstract
 
 class LuceneFinder(ObjectFinder):
+    def __init__(self, db, index, languageid=101):
+        ObjectFinder.__init__(self, index, languageid)
+        self.db = db
     def search(self, object):
         query = object.getSearchString(xapian=False, languageid=self.languageid)
         results = lucenelib.search(self.index, {"X" : query}.items())
-        for aid in results[0]["X"].keys():
-            yield article.Article(self.db, aid)
+        return results[0]["X"].keys()
 
     def searchMultiple(self, objects):
         query = dict((o.id, o.getSearchString(xapian=False, languageid=self.languageid)) for o in objects)
-        return lucenelib.search(self.index, query.items())[0]
+        for k,v in (lucenelib.search(self.index, query.items())[0]).iteritems():
+            yield k, v.keys()
 
 class XapianFinder(ObjectFinder):
     def search(self, object):
@@ -35,3 +36,14 @@ class XapianFinder(ObjectFinder):
 
     def searchMultiple(self, objects):
         return dict((o.id, set(self.index.query(o.getSearchString(xapian=True, languageid=self.languageid), returnAID=True))) for o in objects)
+
+    
+if __name__ == '__main__':
+    import ont, dbtoolkit
+
+    db = dbtoolkit.amcatDB()
+    lf = LuceneFinder(db, "/home/amcat/indices/AEX kranten JK 2007-09-24T10:32:18/", 13)
+    o = ont.Object(db, 1249)
+    print list(lf.search(o))
+    o2 = ont.Object(db, 2423)
+    print list(lf.searchMultiple([o, o2]))
