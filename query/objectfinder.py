37383ff9099b0a5a1342fb9f0feccb307423ac54
wva <devnull@localhost>
2010-04-28 23:16:16 +0200

    [svn r2257] bugfixes, uniform return values

--- query/objectfinder.py
+++ query/objectfinder.py
@@ -16,9 +16,6 @@ class ObjectFinder(object):
         abstract
 
 class LuceneFinder(ObjectFinder):
-    def __init__(self, db, index, languageid=101):
-        ObjectFinder.__init__(self, index, languageid)
-        self.db = db
     def search(self, object):
         query = object.getSearchString(xapian=False, languageid=self.languageid)
         results = lucenelib.search(self.index, {"X" : query}.items())
@@ -42,7 +39,7 @@ if __name__ == '__main__':
     import ont, dbtoolkit
 
     db = dbtoolkit.amcatDB()
-    lf = LuceneFinder(db, "/home/amcat/indices/AEX kranten JK 2007-09-24T10:32:18/", 13)
+    lf = LuceneFinder("/home/amcat/indices/AEX kranten JK 2007-09-24T10:32:18/", 13)
     o = ont.Object(db, 1249)
     print list(lf.search(o))
     o2 = ont.Object(db, 2423)
