92d2b9b8d1ca73669e3b496167e9f48b5ac90965
Wouter van Atteveldt <wouter@vanatteveldt.com>
2010-09-21 13:01:10 +0200

    move user to cachable2, cleanup

---
+++ test/test_user.py
@@ -0,0 +1,49 @@
+import dbtoolkit, unittest, user, system, authorisation, project, types, amcattest
+
+class TestUser(amcattest.AmcatTestCase):
+
+    def setUp(self):
+        self.db = dbtoolkit.amcatDB(use_app=True)
+        self.app = self.db.getUser()
+
+    def testProperties(self):
+        self.assertEqual(self.app.username, "app")
+        self.assertEqual(self.app.fullname, "'nepaccount' voor applicatie")
+        self.assertEqual(self.app.affiliation, "VU")
+
+    def testUserProjects(self):
+        for projectid in (1,2):
+            self.assertIn(projectid, [p.id for p in self.app.projects])
+        self.assertEqual(list(self.app.roles), [])
+
+    def testCurrentUserIsAdmin(self):
+        # this test will fail if ran by non-admins
+        db = dbtoolkit.amcatDB()
+        me = db.getUser()
+        self.assertIn(authorisation.Role(db, 1), me.roles)
+
+    def testTypes(self):
+        self.assertEqual(user.User.projects.getType(), project.Project)
+        self.assertTrue(user.User.projects.getCardinality())
+        
+        self.assertEqual(user.User.projectroles.getType(), (project.Project, authorisation.Role))
+        self.assertSubclass(user.User.projectroles.getCardinality(), dict)
+        
+    def testDeprecated(self):
+        db = dbtoolkit.amcatDB()
+        me = db.getUser()
+        self.assertTrue(me.isSuperAdmin)
+        self.assertTrue(me.canViewAllProjects)
+        self.assertFalse(me.canCreateNewProject)
+        
+        app = self.db.getUser()
+        self.assertFalse(app.isSuperAdmin)
+        self.assertFalse(app.canViewAllProjects)
+        self.assertFalse(app.canCreateNewProject)
+
+        for d in (db, self.db):
+            self.assertEqual(user.currentUser(d), d.getUser())
+            self.assertEqual(user.users(d), system.System(d).users)
+
+if __name__ == '__main__':
+    unittest.main()
--- user.py
+++ user.py
@@ -1,111 +1,61 @@
-from cachable import Cachable, DBPropertyFactory, DBFKPropertyFactory, CachingMeta
+from cachable2 import Cachable, DBProperty, ForeignKey, DBProperties
 import toolkit, permissions, project, authorisation
 
-_users = None
-def users(db):
-    """Access singleton Users object"""
-    global _users
-    if _users is None:
-        _users = Users(db)
-    return _users
-        
-class Users(object):
-    def __init__(self, db):
-        self.db = db
-        self.byid, self.byname = {}, {}
-        SQL = "select userid from users"
-        for uid, in db.doQuery(SQL):
-            u = User(db, uid)
-            self.byid[uid] = u
-            self.byname[u.username] = u
-    def getUser(self, userid):
-        return self.byid[userid]
-    def getByName(self, username):
-        return self.byname[username]
-    def __iter__(self):
-        for user in self.byid.values():
-            yield user
-            
-def addUser(db, uname, passwd, fullname, affiliation, email, permissionLevel):
-    """Add a user to the system. Return new UID."""
-    
-    sql = "exec newcoder '%s', '%s', '%s', '%s', '%s', '%s'"
-    args = (uname, passwd, fullname, email, affiliation, permissionLevel)
-    
-    db.commit()
-    uid = db.doSafeQuery(sql, *args)
-    db.commit()
-    
-    return uid
+def getProjectRole(db, projectid, roleid):
+    return project.Project(db, projectid), authorisation.Role(db, roleid)
 
 class User(Cachable):
-    __metaclass__ = CachingMeta
     __table__ = 'users'
     __idcolumn__ = 'userid'
     __labelprop__ = 'username'
-    
 
-    __dbproperties__ = ["username", "fullname", "affiliation", "active", "email", "language"]
-    permissionLevel = DBPropertyFactory("permissionid", table="permissions_users", func=permissions.UserPermission.get)
-    projects = DBFKPropertyFactory("permissions_projects_users", "projectid", dbfunc=lambda db, id : project.Project(db, id))
-    roles = DBFKPropertyFactory("users_roles", "roleid", factory= lambda:authorisation.Role)
-
-    def permissionLevel_update(self, value):
-        p = Permission(self.db, self.id)
-        p.update(permissionid=value)
+    permissionLevel = DBProperty(table="permissions_users", getcolumn="permissionid", deprecated=True)
     
-    @property
-    def permissionLevelName(self):
-        if self.permissionLevel is None:
-            return "None"
-        else:
-            return self.permissionLevel.label
-    
-    def getProjects(self, own):
-        return project.projects(self.db, self.id, own=own)
+    username, fullname, affiliation, active, email = DBProperties(5)
+    roles = ForeignKey(lambda : authorisation.Role, table="users_roles")
+    projects = ForeignKey(lambda : project.Project, table="projects_users_roles")
+    projectroles = ForeignKey(lambda : (project.Project, authorisation.Role),
+                              table="projects_users_roles", sequencetype=toolkit.multidict)
         
     @property
-    def canCreateNewProject(self):
-        return self.permissionLevel.value > permissions.UserPermission.CODER.value
+    @toolkit.deprecated
+    def canCreateNewProject(self): return False
     
     @property
-    def canViewAllProjects(self):
-        return self.permissionLevel.value > permissions.UserPermission.CODER.value
-    
+    @toolkit.deprecated
+    def canViewAllProjects(self): return self.permissionLevel > 2
+
     @property
-    def canViewAffiliationUserList(self):
-        return self.permissionLevel.value > permissions.UserPermission.CODER.value
+    @toolkit.deprecated
+    def canViewAffiliationUserList(self): return True
         
     @property
-    def canViewUserList(self):
-        return self.permissionLevel.value >= permissions.UserPermission.SUPER_ADMIN.value
+    @toolkit.deprecated
+    def canViewUserList(self): return True
 
     @property
-    def canAddNewUserToAffiliation(self):
-        return self.permissionLevel.value >= permissions.UserPermission.ADMIN.value
+    @toolkit.deprecated
+    def canAddNewUserToAffiliation(self): return False
     
     @property
-    def isSuperAdmin(self):
-        return self.permissionLevel.value >= permissions.UserPermission.SUPER_ADMIN.value
-    
+    @toolkit.deprecated
+    def isSuperAdmin(self): return self.permissionLevel >= 4
 
-
-class Permission(Cachable):
-    __metaclass__ = CachingMeta
-    __table__ = 'permissions_users'
-    __idcolumn__ = 'userid'
-    __labelprop__ = 'userid'
     
-    __dbproperties__ = ["permissionid"]
-        
+@toolkit.deprecated
 def currentUser(db):
-    uid = db.getValue("select dbo.anoko_user()")
-    return User(db, uid)
-    
+    return db.getUser()
         
+@toolkit.deprecated
+def users(db):
+    import system
+    return system.System(db).users
         
 if __name__ == '__main__':
     import dbtoolkit
-    db = dbtoolkit.amcatDB()
-    print currentUser(db)
+    print User.projects.getType()
+    print User.projects.getCardinality()
+    print dbtoolkit.amcatDB().getUser().projects
+    print list(dbtoolkit.amcatDB().getUser().projects)
+    
 
