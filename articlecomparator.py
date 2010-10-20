90a6de2c22780543439fc9a7b64af4b67d9440b0
Kasper Welbers <Kasperwelbers@gmail.com>
2010-10-20 19:43:07 +0200

    add articlecomparator

--- articlecomparator.py
+++ articlecomparator.py
@@ -1,9 +1,9 @@
 
 import dbtoolkit, article, toolkit, sys, re, sbd
-import split, datetime 
+import split, datetime, preprocessing
 
-db=dbtoolkit.anokoDB()
 
+db  = dbtoolkit.amcatDB()
 
 class ArticleComparator(object):
    """Abstract Base Class that represents a method to compare articles"""
@@ -11,7 +11,7 @@ class ArticleComparator(object):
    def compareALL(self, articles1, articles2):
        raise NotImplementedError("Comparator subclasses should implement this method!")
 
-   def compareArticles(self, art1, art2):
+   def compareArticles(self, art1, art2, alreadysplit=False):
        """ Compare two articles
 
        @param art1, art2: an articleid (integer)
@@ -20,7 +20,7 @@ class ArticleComparator(object):
        for dummy, dummy2, score in self.compareAll([art1], [art2]):
            return score
 
-   def findBestMatch(self, art, articles):
+   def findBestMatch(self, art, articles, alreadysplit=False):
        #return toolkit.best(art, articles, self.compareArticles) 
        score = 0
        for vergart in articles:
@@ -28,21 +28,27 @@ class ArticleComparator(object):
           if s > score:
              score = s
              bestmatch = vergart
-       return art, bestmatch, score
-
+       if score == 0:
+          print '--------BESTMATCH IS ZERO'
+          bestmatch, score = 0000, 0
+       return bestmatch, score
+          
 
 class OverlapComparator(ArticleComparator):
    """Abstract class that compares articles by computing an overlap in features"""
 
 
-   def compareAll(self, articles1, articles2):
+   def compareAll(self, articles1, articles2, alreadysplit=False):
        """ When comparing sets of articles, compareALL first stores features in a dictionary {articlids: features}, so that features do not need to be computed more than once
        
        @param articles1, articles2: a list of articleids
+       @param alreadysplit: if False, all articles will be preprocessed first (if unprocessed articles are used, overlapscores will be unreliable)
        @return: articleid1, articleid2 and the overlap
        """
-       if type(articles1) <> list: articles = [articles]
-       if type(articles2) <> list: articles = [articles]
+       if not alreadysplit:
+          db  = dbtoolkit.amcatDB()
+          allarts = list(set(articles1) | set(articles2))
+          preprocessing.splitArticles(db, allarts)                
        featuresdict1 = self.featuresdict(articles1)
        featuresdict2 = self.featuresdict(articles2)
        for art1 in featuresdict1:
@@ -57,6 +63,7 @@ class OverlapComparator(ArticleComparator):
        @param f1, f2: a set of features
        @return: the overlap of the features
        """
+       if (f1 or f2) == set(): print '\n\n ------------------------ WARNING ---------------------------- \n EMPTY NGRAMSETS. TRY PREPROCESSING \n\n'
        return float(len(f1 & f2)) / len(f1 | f2)
 
    def featuresdict(self, artlist):
@@ -77,13 +84,13 @@ class OverlapComparator(ArticleComparator):
 
 class NGramComparator(OverlapComparator):
     def __init__(self, n=1):
+        """ n = the desired length of the n-grams (n=3 -> trigrams) """
         self.n = n
 
     def getFeatures(self, art):
         """ Generates the n-grams for each sentence of an article
 
         @param art: an article object
-        @param n: the desired length of the n-grams (n=3 -> trigrams)
         @return: a list of strings, the n-grams tied together with underscores (['word1_word2_word3','word2_word3_word4']
         """
         sentencelist = self.sentlist(art.sentences)
@@ -102,7 +109,7 @@ class NGramComparator(OverlapComparator):
         result = []
         sents = set(sents)
         for sent in sents:
-            s = sent.text
+            s = safestr(sent.text)
             result.append(str(s.strip()))
         return result
 
@@ -110,7 +117,6 @@ class NGramComparator(OverlapComparator):
         """ Generates the n-grams of a sentence
 
         @param sentence: a string
-        @param n: the desired length of the n-grams (n=3 -> trigrams)
         @return: a list of strings, the n-grams tied together with underscores (['word1_word2_word3','word2_word3_word4']
         """
         l=[]
@@ -120,8 +126,63 @@ class NGramComparator(OverlapComparator):
             if len(l)==self.n:
                 result.append("_".join(l))
                 del(l[0])
-        return result    
+        return result   
 
-   
+def bestMatchInPeriod(art, articles, days=1, alreadysplit=False):
+   comp = NGramComparator(n=3)
+   articles = daysAfter(art, articles, days)
+   return comp.findBestMatch(art, articles, alreadysplit)
 
-    
+def bestMatchPerSource(art, articles, alreadysplit=False):
+   comp = NGramComparator(n=3)
+   sdict = sourceDict(articles)
+   for s in sdict:
+      bestmatch, score = comp.findBestMatch(art, sdict[s], alreadysplit)
+      if bestmatch == 0000: continue 
+      yield s, bestmatch, score
+
+def bestMatchPerSourceInPeriod(art, articles, days=1, alreadysplit=False):
+   comp = NGramComparator(n=3)
+   articles = daysAfter(art, articles, days)
+   sdict = sourceDict(articles)
+   for s in sdict:
+      bestmatch, score = comp.findBestMatch(art, sdict[s], alreadysplit)
+      if bestmatch == 0000: continue 
+      yield s, bestmatch, score
+      
+def daysAfter(art, articles, days=1):
+   """ reduces the list of 'articles' to only the articles that are published in a set period ('days') after the publication of 'art'
+
+   @param art: an articleid (integer)
+   @param articles: a list of articleids
+   @param days: period in days after publication of art (days=1 is the day directly after art)
+   @return: list of articleids that are published in a set period ('days') after the publication of 'art'
+   """
+   art=article.Article(db,art)
+   artdate = datetime.date(art.date.year, art.date.month, art.date.day)
+   relevant = []
+   for aid in articles:
+      a = article.Article(db,aid)
+      adate = datetime.date(a.date.year, a.date.month, a.date.day)
+      if not ((adate - artdate).days > days) and not ((adate - artdate).days < days): relevant.append(aid) 
+   return relevant
+
+def sourceDict(aiddata):
+   """ creates a dictionary out of a list of articleids in which the keys are the sources and the values are the articleids (in a list)
+
+   @param aiddata: a list of articleids
+   @return: a dictionary out of a list of articleids in which the keys are the sources and the values are the articleids (in a list)
+   """
+   sourcedict = {}
+   for aid in aiddata:
+      source = article.Article(db,aid).source
+      if not source in sourcedict: sourcedict[source] = [aid]
+      else: sourcedict[source].append(aid)
+   return sourcedict
+
+def safestr(x):
+    if type(x) == str: x = str.decode("latin-1")
+    if type(x) <> unicode: x = unicode(x)
+    return x.encode("ascii","replace")
+
+
--- test/test_articlecomparator.py
+++ test/test_articlecomparator.py
@@ -4,10 +4,22 @@ class ArticleComparatorTest(amcattest.AmcatTestCase):
     def testNGram(self):
         comp = articlecomparator.NGramComparator(n=3)
         for a1, a2, expectedresult in (
-            (353570, 353571, 0.0),
-            (353570, 353570, 1.0),
+            (45321989, 45322229, 0.0),
+            (45321989, 45321989, 1.0),
             ):
             self.assertEqual(comp.compareArticles(a1, a2), expectedresult)
+
+    def testCompareAll(self):
+        comp = articlecomparator.NGramComparator(n=3)
+        arts1, arts2 = [45321989, 4532222], [4532222,45321989]
+        expectedresult = set([(45321989, 45321989, 1.0), (45321989, 4532222, 0.0), (4532222,4532222, 1.0), (4532222,45321989, 0.0)])
+        self.assertEqual(set(comp.compareAll(arts1, arts2)), expectedresult)
+
+    def testfindBestMatch(self):
+        comp = articlecomparator.NGramComparator(n=3)
+        art, arts = 45321989, [45321989, 45322229]
+        expectedresult = (45321989, 1.0)
+        self.assertEqual((comp.findBestMatch(art, arts)), expectedresult)
 
 if __name__ == '__main__':
     amcattest.main()
