f02fdbdf67eb5ec8e8a4471f46cedf402b3b85b3
Wouter van Atteveldt <wouter@vanatteveldt.com>
2010-11-14 20:35:27 +0100

    add csv output

--- bin/query.py
+++ bin/query.py
@@ -6,7 +6,7 @@ import tableoutput
 
 db = dbtoolkit.anokoDB()
 
-outputs = ["plain"]
+outputs = ["plain", "csv"]
 output = None
 if sys.argv[1] in outputs:
     output = sys.argv[1]
@@ -14,11 +14,15 @@ if sys.argv[1] in outputs:
 
 sql = " ".join(sys.argv[1:])
 
-select = sql.strip().lower().startswith("select")
+
+select = sql.strip().lower().startswith("select") or (sql.strip().lower() == "sp_who")
 if select:
-    res, colnames = db.doQuery(sql, colnames=True)
+    sql = "SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED\n%s" % sql
+    res, colnames = db.doQuery(sql, colnames=True, select=True)
     if output == "plain":
         tableoutput.printTable(res)
+    elif output == "csv":
+        tableoutput.table2csv(res)
     else:
         print tableoutput.table2unicode(res, colnames)
 else:
