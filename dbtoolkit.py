31f9d41fd4ac2e53bbd3f361a02c775253c8d6aa
Martijn Bastiaan <devnull@localhost>
2010-11-19 19:28:51 +0100

    Error handling set to 'replace' instead of 'strict', preventing Unicode-errors

--- dbtoolkit.py
+++ dbtoolkit.py
@@ -705,7 +705,8 @@ def quotesql(strOrSeq):
             return "cast(%s as varchar(8000))" % sqlbytes(bytes)
     elif type(strOrSeq) in (str, unicode):
         if type(strOrSeq) == unicode:
-            strOrSeq = strOrSeq.encode('latin-1')
+            strOrSeq = toolkit.smart_str(strOrSeq, encoding='latin-1', errors='replace')
+            #strOrSeq = strOrSeq.encode('latin-1')
         strOrSeq = strOrSeq.replace("\r\n","\n")
         strOrSeq = strOrSeq.replace("\n\r","\n")
         strOrSeq = strOrSeq.replace("\r","\n")
