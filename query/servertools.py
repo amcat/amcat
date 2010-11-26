f59915b9623e620ade198f11af1d07b2bbed8de6
Jouke Jacobi <joukejacobi@gmail.com>
2010-11-26 20:02:21 +0100

    key should be found now on both servers

--- query/servertools.py
+++ query/servertools.py
@@ -1,8 +1,8 @@
 import hashlib
 
 PORT = 26221
-KEYFILE = "/home/amcat/resources/key" # for amcat
-KEYFILE = "/usr/home/netframe/resources/key" # for parc
+KEYFILE1 = "/home/amcat/resources/key" # for amcat
+KEYFILE2 = "/usr/home/netframe/resources/key" # for parc
 KEY = None
 
 REQUEST_LIST = 1
@@ -14,6 +14,10 @@ FILTER_INTERVAL = 2
 
 def hash(s):
     global KEY
-    if KEY is None: KEY = open(KEYFILE).read()
+    if KEY is None:
+        try:
+            KEY = open(KEYFILE1).read()
+        except IOError:
+            KEY = open(KEYFILE2).read()
     return hashlib.md5(KEY+s).digest()
 
