51e31c743254201b6fd83de68948d051056f653d
Wouter van Atteveldt <wouter@vanatteveldt.com>
2010-11-30 10:02:47 +0100

    extra debug output

--- table2spss.py
+++ table2spss.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import dbtoolkit, codingjob, table3, toolkit
 import sys, csv
 import re
@@ -5,8 +6,11 @@ import datetime
 from idlabel import IDLabel
 import collections
 import os.path
+import progress
 
 import logging; log = logging.getLogger(__name__)
+
+import amcatlogging; amcatlogging.debugModule()
 
 def clean(s, maxchars=None):
     if type(s) == str: s = s.decode('latin-1')
@@ -17,7 +21,7 @@ def clean(s, maxchars=None):
     return s
 
 def getSPSSFormat(type):
-    log.debug("Determining format of %s" % type)
+    #log.debug("Determining format of %s" % type)
     if type == int: return " (F8.0)"
     if issubclass(type, IDLabel): return " (F8.0)"
     if type == float: return " (F8.3)"
@@ -25,59 +29,68 @@ def getSPSSFormat(type):
     if type == datetime.datetime: return " (date10)"
     raise Exception("Unknown type: %s" % type)
 
-def table2spss(t, writer=sys.stdout, saveas=None):
-    cols = t.getColumns()
+def _getVarDef(col, seen=set()):
+    """Remove duplicates and spaces from field names"""
+    fn = col.fieldname.replace(" ","_")
+    if fn in seen:
+        for i in xrange(400):
+            if "%s_%i" % (fn, i) not in seen:
+                fn = "%s_%i" % (fn, i)
+    seen.add(fn)
+    vardef = "%s%s" % (fn, getSPSSFormat(col.fieldtype))
+    log.debug("Col %s vardef %s" % (col, vardef))
+    col.fieldname = fn # otherwise will get in trouble later
+    return vardef
 
-    # resolve duplicate field names - the hard way
-    seen = set()
-    for c in cols:
-        if c.fieldname in seen:
-            f = c.fieldname
-            for i in xrange(400):
-                if "%s_%i" % (f, i) not in seen:
-                    c.fieldname = "%s_%i" % (f, i)
-                    break
-        seen.add(c.fieldname)
-            
-    
-    vardefs = " ".join("%s%s" % (c.fieldname, getSPSSFormat(c.fieldtype))
-                       for c in cols)
-    toolkit.ticker.warn("Writing var list")
-    writer.write("DATA LIST LIST\n / %s .\nBEGIN DATA.\n" % vardefs)
+def table2spss(t, writer=sys.stdout, saveas=None, monitor=progress.NullMonitor()):
+    with monitor.monitored("Creating SPSS LIST DATA syntax", 100) as pm:
+        cols = t.getColumns()
 
-    toolkit.ticker.warn("Writing data")
-    valuelabels = collections.defaultdict(dict) # col : id : label
-    for row in t.getRows():
-        for i, col in enumerate(cols):
-            if i: writer.write(",")
-            val = t.getValue(row, col)
-            if val and col.fieldtype == IDLabel:
-                valuelabels[col][val.id] = val.label
-                val = val.id
-            if val and col.fieldtype == str:
-                val = '"%s"' % clean(val)
-            if val and col.fieldtype == datetime.datetime:
-                val = val.strftime("%d/%m/%Y")
-            val = "" if val is None else str(val)
-            writer.write(val)
-        writer.write("\n")
-    writer.write("END DATA.\n")
+        seen = set()
+        vardefs = " ".join(_getVarDef(col, seen) for col in cols)
 
-    toolkit.ticker.warn("Writing var labels")
-    varlabels = " / ".join("%s '%s'" % (c.fieldname, clean(c.label, 55)) for c in cols)
-    writer.write("VARIABLE LABELS %s.\n" % varlabels)
+        log.debug("Writing var list")
+        writer.write("DATA LIST LIST\n / %s .\nBEGIN DATA.\n" % vardefs)
 
-    toolkit.ticker.warn("Writing value labels")
-    for c in cols:
-        vl = valuelabels[c]
-        if vl:
-            writer.write("VALUE LABELS %s\n" % c.fieldname)
-            for id, lbl in sorted(vl.iteritems()):
-                writer.write("  %i  '%s'\n" % (id, clean(lbl, 250)))
-            writer.write(".\n")
-    if saveas:
-        toolkit.ticker.warn("Saving file")
-        writer.write("SAVE OUTFILE='%s'.\n" % saveas)
+        pm.worked(5)
+        
+        log.debug("Writing data")
+        valuelabels = collections.defaultdict(dict) # col : id : label
+        for row in pm.tickerate(t.getRows(),"Writing SPSS data rows", submonitorwork=80):
+            for i, col in enumerate(cols):
+                if i: writer.write(",")
+                val = t.getValue(row, col)
+                oval = val
+                if val and issubclass(col.fieldtype, IDLabel):
+                    valuelabels[col][val.id] = val.label
+                    val = val.id
+                if val and col.fieldtype == str:
+                    val = '"%s"' % clean(val)
+                if val and col.fieldtype == datetime.datetime:
+                    val = val.strftime("%d/%m/%Y")
+                val = "" if val is None else str(val)
+                writer.write(val)
+                #log.debug("Wrote %r --> %s" % (oval, val))
+            writer.write("\n")
+        writer.write("END DATA.\n")
+
+        log.debug("Writing var labels")
+        varlabels = " / ".join("%s '%s'" % (c.fieldname, clean(c.label, 55)) for c in cols)
+        writer.write("VARIABLE LABELS %s.\n" % varlabels)
+        pm.worked(5)
+        
+        log.debug("Writing value labels")
+        for c in cols:
+            vl = valuelabels[c]
+            if vl:
+                writer.write("VALUE LABELS %s\n" % c.fieldname)
+                for id, lbl in sorted(vl.iteritems()):
+                    writer.write("  %i  '%s'\n" % (id, clean(lbl, 250)))
+                writer.write(".\n")
+        if saveas:
+            log.debug("Saving file")
+            writer.write("SAVE OUTFILE='%s'.\n" % saveas)
+        pm.worked(10)
 
 class EchoWriter(object):
     def __init__(self, writer):
@@ -86,22 +99,30 @@ class EchoWriter(object):
         sys.stdout.write(bytes)
         self.writer.write(bytes)
         
-def table2sav(t, filename=None):
-    if filename is None: filename = toolkit.tempfilename(suffix=".sav",prefix="table-")
-    pspp = toolkit.executepipe("pspp -p")
-    writer = pspp.next()
-    #writer = EchoWriter(writer)
-    table2spss(t, writer=writer, saveas=filename)
-    out, err = pspp.next()
-    err = err.replace('pspp: error creating "pspp.jnl": Permission denied','')
-    err = err.replace('pspp: ascii: opening output file "pspp.list": Permission denied','')
-    if err.strip():
-        raise Exception(err)
-    if "error:" in out.lower():
-        raise Exception("PSPP Exited with error: \n\n%s"  % out)
-    if not os.path.exists(filename):
-        raise Exception("PSPP Exited without errors, but file was not saved.\n\nOut=%r\n\nErr=%r"% (out, err))
-    return filename
+def table2sav(t, filename=None, monitor=progress.NullMonitor()):
+    with monitor.monitored("Exporting to .SAV", 100):
+        if filename is None: filename = toolkit.tempfilename(suffix=".sav",prefix="table-")
+        log.debug("Executing PSPP")
+        pspp = toolkit.executepipe("pspp -p")
+        writer = pspp.next()
+        monitor.worked(10)
+        #writer = EchoWriter(writer)
+        log.debug("Creating SPS script and sending to PSPP")
+        table2spss(t, writer=writer, saveas=filename, monitor=monitor.submonitor(80))
+        log.debug("Closing PSPP")
+        out, err = pspp.next()
+        monitor.worked(10)
+        log.debug("PSPPP err: %s" % err)
+        log.debug("PSPPP out: %s" % out)
+        err = err.replace('pspp: error creating "pspp.jnl": Permission denied','')
+        err = err.replace('pspp: ascii: opening output file "pspp.list": Permission denied','')
+        if err.strip():
+            raise Exception(err)
+        if "error:" in out.lower():
+            raise Exception("PSPP Exited with error: \n\n%s"  % out)
+        if not os.path.exists(filename):
+            raise Exception("PSPP Exited without errors, but file was not saved.\n\nOut=%r\n\nErr=%r"% (out, err))
+        return filename
 
 if __name__ == '__main__':
     db = dbtoolkit.amcatDB()
