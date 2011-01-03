6d4cfc3516ed005408a5db7d2fed9cf833181307
Wouter van Atteveldt <wouter@vanatteveldt.com>
2011-01-03 02:10:53 +0100

    cleaned up article, postgres ready

--- article.py
+++ article.py
@@ -1,20 +1,33 @@
-def doCreateArticle(db, aid, **cache):
-    return Article(db, aid, **cache)
-def doCreateSentence(db, aid, **cache):
-    import sentence
-    return sentence.Sentence(db, aid, **cache)
+from __future__ import unicode_literals, print_function, absolute_import
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
 
-import toolkit, dbtoolkit, re, project, sources, types, batch
+"""
+Model module containing the Article class representing documents in the
+articles database table
+"""
 
-try:
-    from nlp import quote
-except:
-    import quote
-from itertools import izip, count
-from functools import partial
-from cachable import Cachable, DBPropertyFactory, DBFKPropertyFactory, CachingMeta, cacheMultiple
+from cachable2 import Cachable, DBProperty, ForeignKey, DBProperties
+import toolkit
+import sentence, project, sources
 
-
+import logging; log = logging.getLogger(__name__)
     
 class Article(Cachable):
     """
@@ -23,29 +36,21 @@ class Article(Cachable):
     __table__ = 'articles'
     __idcolumn__ = 'articleid'
     __labelprop__ = 'headline'
-    __encodingprop__ = 'encoding'
-    __dbproperties__ = ["date", "length", "pagenr", "url", "encoding","externalid"]
-    __metaclass__ = CachingMeta
-    headline = DBPropertyFactory(decode=True)
-    byline = DBPropertyFactory(decode=True)
-    metastring = DBPropertyFactory(decode=True)
-    section = DBPropertyFactory(decode=True)
-    batch = DBPropertyFactory("batchid", factory=lambda : batch.Batch)
-    source = DBPropertyFactory("mediumid", factory=lambda : sources.Source)
-    sentences = DBFKPropertyFactory("sentences", "sentenceid", dbfunc=doCreateSentence)
-    
-    @property
-    def text(self):
-        return(self.db.getText(self.id))
+    __dbproperties__ = ["date", "length", "pagenr", "url", "externalid"]
+
+    headline, byline, metastring, section, date, length, pagenr, url, externalid, text = DBProperties(10)
+
+    project = DBProperty(lambda : project.Project)
+    source = DBProperty(lambda : sources.Source)
+    sentences = ForeignKey(lambda : sentence.Sentence)
 
     @property
     def fullmeta(self):
+        "@return: a dictionary representing the 'prose' metastring as a dict"
         return toolkit.dictFromStr(self.metastring)
-    @property
-    def project(self):
-        return self.batch.project
 
     def fulltext(self):
+        "@return: a string containing the headline, byline, and text as paragraphs"
         result = (self.headline or '') +"\n\n"+ (self.byline or "")+"\n\n"+(self.text or "")
         return result.replace("\\r","").replace("\r","\n")
 
@@ -53,49 +58,14 @@ class Article(Cachable):
         "Convenience function also present in CodedArticle, CodedUnit"
         return self
 
-    def words(self, onlyWords = False, lemma=0, headline=False): #lemma: 1=lemmaP, 2=lemma, 3=word
-        import ctokenizer
-        text = self.text
-        if headline and self.headline: text = self.headline + "\n\n" + text
-        if not text: return []
-        text = toolkit.stripAccents(text)
-        text = text.encode('ascii', 'replace')
-        text = ctokenizer.tokenize(text)
-        #toolkit.warn("Yielding words for %i : %s" % (self.id, text and len(text) or `text`))
-        text = re.sub("\s+", " ", text)
-        return text.split(" ")
+    def words(self):
+        "@return: a generator yielding all words in all sentences"
+        for sentence in self.sentences:
+            for word in sentence.words:
+                yield word
 
-    def quote(self, words_or_wordfilter, **kargs):
-        return quote.quote(list(self.words(headline = True)), words_or_wordfilter, **kargs)
-    
     def getSentence(self, parnr, sentnr):
+        "@return: a Sentence object with the given paragraph and sentence number"
         for s in self.sentences:
             if s.parnr == parnr and s.sentnr == sentnr:
                 return s
-
-def Articles(aids, db):
-    if db is None: raise Exception("Need a db connection to create articles")
-    arts = [Article(db, aid) for aid in aids]
-    if any(a.db is None for a in arts): raise Exception("Article has no db??")
-    return arts
-
-def cacheText(articles):
-    cacheMultiple(articles, "encoding", "headline", "text")
-
-def encodeAndLimitLength(variables, lengths):
-    originals = map(lambda x: x and x.strip(), variables)
-    numchars = 5
-    while True:
-        variables, enc = dbtoolkit.encodeTexts(variables)
-        done = True
-        for i, (var, maxlen, original) in enumerate(zip(variables, lengths, originals)):
-            if var and (len(var) > maxlen):
-                done = False
-                variables[i] = original[:maxlen-numchars] + " ..."
-        if done: return variables, enc
-        numchars += 5
-
-if __name__ == '__main__':
-    import dbtoolkit
-    a = Article(dbtoolkit.amcatDB(), 33308863)
-    print a.quote(["de"])
--- test/test_article.py
+++ test/test_article.py
@@ -1,21 +1,21 @@
-import dbtoolkit, amcattest, article
+import amcattest, article, toolkit
 
 class TestArticle(amcattest.AmcatTestCase):
 
-    def setUp(self):
-        self.db = dbtoolkit.amcatDB(use_app=True)
 
     def testProperties(self):
-        for (aid, batchid, sourceid, section, headline) in [
-            (353570, 8, 5, 'Economie;', "Lezers Brabants Nieuwsblad zijn 'massaal' ongerust"),
+        for (aid, projectid, sourceid, date, section, pagenr, headline, length) in [
+            (57356176, 428, 707, '2006-11-14', None, 1, u"Van Cau a bien favoris\xe9 son ami", 151),
             ]:
 
             a = article.Article(self.db, aid)
             self.assertEqual(a.id, aid)
             self.assertEqual(a.source.id, sourceid)
-            self.assertEqual(a.batch.id, batchid)
+            self.assertEqual(a.project.id, projectid)
             self.assertEqual(a.section, section)
             self.assertEqual(a.headline, headline)
+            self.assertEqual(toolkit.writeDate(a.date), date)
+            self.assertEqual(a.length, length)
     
         
 if __name__ == '__main__':
