d86c552fc88c2311a554854c31785572920d1efe
Wouter van Atteveldt <wouter@vanatteveldt.com>
2011-01-09 19:52:39 +0100

    libpy->lib

--- test/test_hg.py
+++ test/test_hg.py
@@ -5,23 +5,23 @@ class TestHG(amcattest.AmcatTestCase):
     def test_getrepo(self):
         for input, output in (
             ("/home/wva", None),
-            ("/home/wva/libpy/amcat", "/home/wva/libpy/amcat"),
-            ("/home/wva/libpy/amcat/tools/toolkit.py", "/home/wva/libpy/amcat"),
-            ("/home/wva/libpy/amcat/test", "/home/wva/libpy/amcat"),
+            ("/home/wva/lib/amcat", "/home/wva/lib/amcat"),
+            ("/home/wva/lib/amcat/tools/toolkit.py", "/home/wva/lib/amcat"),
+            ("/home/wva/lib/amcat/test", "/home/wva/lib/amcat"),
             ):
             self.assertEqual(output, hg.getRepo(input))
 
     def test_getstatus(self):
         for input, output in (
-            ("/home/wva/libpy/amcat/tools/toolkit.py", "CM"),
-            ("/home/wva/libpy/amcat/tools/toolkit.pyc", "I"),
-            ("/home/wva/libpy/a,cat/thisfiledoesnotexist.py", [None]),
+            ("/home/wva/lib/amcat/tools/toolkit.py", "CM"),
+            ("/home/wva/lib/amcat/tools/toolkit.pyc", "I"),
+            ("/home/wva/lib/amcat/thisfiledoesnotexist.py", [None]),
             ("/home/wva/thisfiledoesnotexist.py", [None]),
             ):
             self.assertTrue(hg.getStatus(input) in output)
 
     def test_getrepostatus(self):
-        repodict = dict(hg.getRepoStatus("/home/wva/libpy/amcat"))
+        repodict = dict(hg.getRepoStatus("/home/wva/lib/amcat"))
         self.assertTrue(repodict['tools/toolkit.py'] in "CM")
     
 if __name__ == '__main__':
