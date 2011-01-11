3e9fb62727cf145b44b0e7fa9fe92ead9e2427d8
Wouter van Atteveldt <wouter@vanatteveldt.com>
2011-01-11 15:47:31 +0100

    split codingjob, passes unittests

--- model/coding/codingjob.py
+++ model/coding/codingjob.py
@@ -1,41 +1,7 @@
-from functools import partial
-
-from amcat.tools import toolkit
-from amcat.db import dbtoolkit
 from amcat.tools.cachable.cachable import Cachable, DBProperty, ForeignKey, DBProperties
-from amcat.tools.cachable.cacher import  cache, cacheMultiple
 from amcat.tools.cachable.latebind import LB
 
-from amcat.tools.table import table3
-from amcat.tools.idlabel import IDLabel
-
-from amcat.model.coding import annotationschema
-
 import logging; log = logging.getLogger(__name__)
-#import amcatlogging; amcatlogging.debugModule()
-
-def getCodedArticle(db, cjaid):
-    data = db.doQuery("select codingjobid, setnr from codingjobs_articles where codingjob_articleid=%i"% cjaid)
-    if not data: return None
-    cjid, setnr = data[0]
-    return Codingjob(db, cjid).getSet(setnr).getArticle(cjaid)
-    
-def getCodedArticleIdsFromArticleId(db, articleid):
-    data = db.doQuery("select codingjob_articleid from codingjobs_articles where articleid=%i"% articleid)
-    if not data: return []
-    return (row[0] for row in data)
-
-def getCodedArticlesFromArticleId(db, articleid):
-    for cjaid in getCodedArticleIdsFromArticleId(db, articleid):
-        yield getCodedArticle(db, cjaid)
-
-def getCodedSentencesFromArticleId(db, articleid):
-    for ca in getCodedArticlesFromArticleId(db, articleid):
-        for cs in ca.sentences:
-            yield cs
-
-def getCodedArticlesFromCodingjobIds(db, codingjobids):
-    return getCodedArticlesFromCodingjobs(map(partial(CodingJob, db), codingjobids))
 
 def getCodedArticlesFromCodingjobs(codingjobs):
     for cj in codingjobs:
@@ -43,15 +9,8 @@ def getCodedArticlesFromCodingjobs(codingjobs):
             for ca in s.articles:
                 yield ca
                     
-def getCodedSentencesFromCodingjobIds(db, codingjobids):
-    cjs =[Codingjob(db, id) for id in codingjobids]
-    return getCodedSentencesFromCodingjobs(cjs)
-
-
 def getCodedSentencesFromCodingjobs(codingjobs):
-    toolkit.ticker.warn("Caching")
     cache(codingjobs, sets=dict(articles=dict(article=["sentences"], sentences=[])))
-    toolkit.ticker.warn("Returning")
     for ca in getCodedArticlesFromCodingjobs(codingjobs):
         for cs in ca.sentences:
             yield cs
@@ -67,7 +26,7 @@ class CodingJob(Cachable):
     project = DBProperty(LB("Project"))
     owner = DBProperty(LB("User"), getcolumn="owner_userid")
 
-    sets = ForeignKey(lambda:CodingJobSet, includeOwnID=True)
+    sets = ForeignKey(LB("CodingJobSet", sub="coding"), includeOwnID=True)
         
     def getLink(self):
         return "codingjobDetails?codingjobid=%i" % self.id
@@ -133,198 +92,8 @@ class CodingJob(Cachable):
         self.db.commit()
 Codingjob = CodingJob
 
-class CodingJobSet(Cachable):
-    __table__ = 'codingjobs_sets'
-    __idcolumn__ = ['codingjobid', 'setnr']
-    
-    coder = DBProperty(LB("User"), getcolumn="coder_userid")
-    articles = ForeignKey(lambda:CodedArticle)
-
-    @property
-    def job(self):
-        return CodingJob(self.db, self.jobid)
-    @property
-    def setnr(self):
-        return self.id[1]
-    @property
-    def jobid(self):
-        return self.id[0]
-
-    def getArticle(self, cjaid):
-        for a in self.articles:
-            if a.id == cjaid:
-                return a
-
-    def getArticles(self):
-        cacheMultiple(self.articles, "article")
-        return [a.article for a in self.articles]
-    def getArticleIDS(self):
-        return set(a.id for a in self.getArticles())
-
-    def getNArticleCodings(self):
-        SQL = """select count(*) from %s x
-              inner join codingjobs_articles ca on x.codingjob_articleid = ca.codingjob_articleid
-              where codingjobid = %d and setnr = %d""" % (self.job.articleSchema.table, self.job.id, self.setnr)
-        return self.job.db.getValue(SQL)
-        
-    def getNUnitCodings(self):
-        SQL = """select count(distinct articleid), count(*) from %s x
-              inner join codingjobs_articles ca on x.codingjob_articleid = ca.codingjob_articleid
-              where codingjobid = %d and setnr = %d""" % (self.job.unitSchema.table, self.job.id, self.setnr)
-        return self.job.db.doQuery(SQL)[0]
-    @property
-    def label(self):
-        return repr(self)
-        
-CodingjobSet = CodingJobSet       
-        
-class CodedUnit(object):
-    def __init__(self):
-        self._fields = None # {fieldname : deserializer}
-        self._values = {}
-        
-    def __getattr__(self, attr):
-        # if not found, check fields
-        if attr == "fields" or attr.startswith("_"): return super(CodedUnit, self).__getattribute__(attr)
-        log.debug("__getattr__(%r), in fields %s?" % (attr, self.fields))
-        if attr in self.fields:
-            if attr not in self._values:
-                self.loadValues()
-            val = self._values.get(attr)
-            return self.fields[attr](val)
-
-        return super(CodedUnit, self).__getattribute__(attr)
-
-    def loadValues(self):
-        s = self.annotationschema
-        vals = self.db.select(s.table, self._fields, self._getWhere())
-        if len(vals) == 0:
-            self._values = dict(map(None, self.fields, [])) # {f : None \forall f in fields}
-            log.debug("No values found")
-        elif len(vals) > 1:
-            raise ValueError("CodedUnit values %r has len>1 %s %s %s" % (vals, s.table, self._fields, self._getWhere()))
-        else:
-            self._values = dict(zip(self.fields, vals[0]))
-            log.debug("Loaded %r" % self._values)
-    
-    @property
-    def fields(self):
-        if self._fields is None:
-            self._fields = dict((f.fieldname, f.deserialize) for f in self.annotationschema.fields)
-        return self._fields
-    
-    def getValue(self, field):
-        if isinstance(field, annotationschema.AnnotationSchemaField):
-            field = field.fieldname
-        return getattr(self, field)
-
-class CodedArticle(Cachable, CodedUnit):
-    __table__ = 'codingjobs_articles'
-    __idcolumn__ = 'codingjob_articleid'
-
-    def __init__(self, *args, **kargs):
-        Cachable.__init__(self, *args, **kargs)
-        CodedUnit.__init__(self)
-    
-    article = DBProperty(LB("Article"))
-    set = DBProperty(CodingJobSet)
-
-    def getSentenceTable(self):
-        return self.set.job.unitSchema.table
-    def createSentence(self, db, arrowid):
-        return CodedSentence(db, arrowid, self)
-    sentences = ForeignKey(lambda:CodedSentence, constructor=createSentence)
-    #sentences.tablehook=getSentenceTable
-#        self.addDBroperty("confidence", table=job.articleSchema.table, func = lambda c : c and (float(c) / 1000))
-
-    @property
-    def annotationschema(self):
-        return self.set.job.articleSchema
-    def getArticle(self):
-        return self.article
-
-    #confidence = DBProperty()self.addDBProperty("confidence", table=job.articleSchema.table, func = lambda c : c and (float(c) / 1000))
-
-class CodedSentence(CodedUnit, Cachable):
-    __idcolumn__ = 'arrowid'
-    __table__ = 'net_arrows' #TODO: dit is niet altijd zo! zie tablehook hierboven
-    ca = DBProperty(CodedArticle)
-    sentence = DBProperty(LB("Sentence"))
-    
-    
-    def __init__(self, db, arrowid, ca):
-        Cachable.__init__(self, db, arrowid)
-        CodedUnit.__init__(self)
-        self.ca = ca
-        self.annotationschema = ca.set.job.unitSchema
-        self.__table__ = self.annotationschema.table
-
-# def getCodedArticle(db, cjaid):
-#     cjid = db.getValue("select codingjobid from codingjobs_articles where codingjob_articleid = %i"  % cjaid)
-#     job = getCodingJob(db, cjid)
-#     return job.findCodedArticle(cjaid = cjaid)
 
 
-
-def getCAJobs(cas):
-    return sorted(set([ca.set.job for ca in cas]))
-def getCACoders(cas):
-    return sorted(set(ca.set.coder for ca in cas))
-
-
-def getValue(row, schemafield):
-    val = row.getValue(schemafield)
-    return val
-
-def getCodingTable(units, valfunc=getValue, coder=True):
-    units.sort(key = lambda r:(r.ca.set.coder, r.sentence))
-    t = table2.ObjectTable(rows=units)
-    t.addColumn("Coder", lambda r: r.ca.set.coder)
-    t.addColumn("Sentence", lambda r: "%i.%i : %s" % (r.sentence.parnr, r.sentence.sentnr, r.sentence.text[:20]))
-    for c in units[0].ca.set.job.unitSchema.fields:
-        t.addColumn(c.label, functools.partial(valfunc, schemafield=c))
-    return t
-
-def countUnits(jobs, field=None):
-    if not toolkit.isSequence(jobs): jobs = [jobs]
-    tot, coded = 0,0
-    for job in jobs:
-        for ca in job.getAllCodedArticles():
-            tot += 1
-            values = ca.getValueObject(field) if field else ca.values
-            if values is not None: coded += 1
-    return tot, coded
-
-
-def getk06CodedSentences(db):
-    SQL = """select codingjob_articleid from codingjobs_groups g inner join codingjobs_articles a on g.codingjobid = a.codingjobid
-             where normative=1 and g.groupid=1"""
-    for cjaid, in db.doQuery(SQL):
-        ca = CodedArticle(db, cjaid)
-        for cs in ca.sentences:
-            yield cs
-    
-
-
-def createCodingJob(project, name, unitschema, articleschema, coders=[]):
-    #TODO: move to CodingJob.Create
-    if not type(unitschema) == int: unitschema = unitschema.id
-    if not type(articleschema) == int: articleschema = articleschema.id
-    cjid = project.db.insert("codingjobs", dict(projectid=project.id, unitschemaid=unitschema, articleschemaid=articleschema, name=name))
-    for i, coder in enumerate(coders):
-        if not type(coder) == int: coder = coder.id
-        project.db.insert("codingjobs_sets", dict(codingjobid=cjid, setnr=i+1, coder_userid=coder), retrieveIdent=False)
-    return CodingJob(project.db, cjid)
-
-def cloneCodingJob(codingjob, newname = None, coders=[]):
-    if newname is None: newname = "%s (kopie)" % (codingjob.label,)
-    return createCodingJob(codingjob.project, newname, codingjob.unitSchema, codingjob.articleSchema, coders=coders)
-    
-def createCodedArticle(codingjobset, article):
-    cjid, setnr = codingjobset.id
-    if not type(article) == int: article = article.id 
-    cjaid = codingjobset.db.insert("codingjobs_articles", dict(codingjobid=cjid, setnr=setnr, articleid=article))
-    return CodedArticle(codingjobset.db, cjaid)
     
 if __name__ == '__main__':
     import dbtoolkit
--- test/test_codingjob.py
+++ test/test_codingjob.py
@@ -1,57 +1,8 @@
 from amcat.test import amcattest
-from amcat.model.coding import codingjob
+from amcat.model.coding import codingjob, codedarticle, codingjobset
 from amcat.tools.cachable import cacher
 
 class TestCodingJob(amcattest.AmcatTestCase):
-
-    def testObjects(self):
-        # check whether we can create objects without errors
-        for cjid in [5175]:
-            cj = codingjob.Codingjob(self.db, cjid)
-            unitschema, artschema = cj.unitSchema, cj.articleSchema
-            for schema in unitschema, artschema:
-                print schema.id
-                print schema.label
-                schema.table
-                list(schema.fields)            
-            for cjset in cj.sets:
-                arts = list(cjset.articles)
-        
-    def testCache(self):
-        c = codingjob.Codingjob(self.db, 5175)
-        cacher.cache(c, **{'sets': {'articles' : {'article' : {'source' : ["name"]}}}})
-
-    def testGetSentences(self):
-        #test an article with net codings
-        cjaid = 16284
-        ca = codingjob.CodedArticle(self.db, cjaid)
-        s = list(ca.sentences)[0]
-        
-    def testCodedArticle(self):
-        # test an article with agenda codings
-        cjaid = 1609147
-        ca = codingjob.CodedArticle(self.db, cjaid)
-        self.assertEqual(ca.article.id, 45833360)
-        self.assertEqual(ca.article.headline, 'Matiging')
-        self.assertEqual(ca.set.job.id, 5175)
-        self.assertEqual(ca.set.setnr, 1)
-        self.assertEqual(ca.annotationschema.id, 26)
-        self.assertEqual(ca.getValue("topic").id, 10490)
-        self.assertEqual(ca.topic.id, 10490)
-        self.assertEqual(list(ca.sentences), [])
-
-    def testCodedSentence(self):
-        #test an article with net codings
-        cjaid = 16284
-        arrowid = 30188
-        ca = codingjob.CodedArticle(self.db, cjaid)
-        self.assertEqual(ca.article.id, 33379889)
-        self.assertNotEmpty(list(ca.sentences))
-        self.assertIn(30188, [cs.id for cs in ca.sentences])
-        cs = [cs for cs in ca.sentences if cs.id == 30188][0]
-        self.assertEqual(cs.getValue("object").id, 1625)
-        self.assertEqual(cs.subject.id, 1098)
-        self.assertEqual(cs.predicate, 'idealistisch, niet de hele dag')
 
     def testCodingJob(self):
         # test basic properties
@@ -65,10 +16,61 @@ class TestCodingJob(amcattest.AmcatTestCase):
         cj = codingjob.CodingJob(self.db, 5251)
         self.assertIn(1, [s.setnr for s in cj.sets])
         s = [s for s in cj.sets if s.setnr == 1][0]
-        self.assertIn(codingjob.CodedArticle(self.db, 1649911), s.articles)
-        self.assertEqual(cj.articleSchema, codingjob.CodedArticle(self.db, 1649911).annotationschema)
+        self.assertIn(codedarticle.CodedArticle(self.db, 1649911), s.articles)
+        self.assertEqual(cj.articleSchema, codedarticle.CodedArticle(self.db, 1649911).annotationschema)
            
         
+    def testObjects(self):
+        # check whether we can create objects without errors
+        for cjid in [5175]:
+            cj = codingjob.Codingjob(self.db, cjid)
+            unitschema, artschema = cj.unitSchema, cj.articleSchema
+            for schema in unitschema, artschema:
+                print schema.id
+                print schema.label
+                schema.table
+                list(schema.fields)            
+            for cjset in cj.sets:
+                arts = list(cjset.articles)
+        
+    def testGetSentences(self):
+        #test an article with net codings
+        cjaid = 16284
+        ca = codedarticle.CodedArticle(self.db, cjaid)
+        s = list(ca.sentences)[0]
+
+    def testCache(self):
+        c = codingjob.Codingjob(self.db, 5175)
+        cacher.cache(c, **{'sets': {'articles' : {'article' : {'source' : ["name"]}}}})
+
+        
+    def testCodedArticle(self):
+        # test an article with agenda codings
+        cjaid = 1609147
+        ca = codedarticle.CodedArticle(self.db, cjaid)
+        self.assertEqual(ca.article.id, 45833360)
+        self.assertEqual(ca.article.headline, 'Matiging')
+        self.assertEqual(ca.set.job.id, 5175)
+        self.assertEqual(ca.set.setnr, 1)
+        self.assertEqual(ca.annotationschema.id, 26)
+        self.assertEqual(ca.getValue("topic").id, 10490)
+        self.assertEqual(ca.topic.id, 10490)
+        self.assertEqual(list(ca.sentences), [])
+
+    def testCodedSentence(self):
+        #test an article with net codings
+        cjaid = 16284
+        arrowid = 30188
+        ca = codedarticle.CodedArticle(self.db, cjaid)
+        self.assertEqual(ca.article.id, 33379889)
+        self.assertNotEmpty(list(ca.sentences))
+        self.assertIn(30188, [cs.id for cs in ca.sentences])
+        cs = [cs for cs in ca.sentences if cs.id == 30188][0]
+        self.assertEqual(cs.getValue("object").id, 1625)
+        self.assertEqual(cs.subject.id, 1098)
+        self.assertEqual(cs.predicate, 'idealistisch, niet de hele dag')
+
+  
         
         
 if __name__ == '__main__':
