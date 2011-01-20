9e53681eecb72f9eb3c565772fc5570dfc5c4e0a
Wouter van Atteveldt <wouter@vanatteveldt.com>
2011-01-20 10:53:00 +0100

    restructured and documented

--- scripts/export_codingjobresults.py
+++ scripts/export_codingjobresults.py
@@ -1,11 +1,45 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+Default export script and base class for custom exporters.
+
+Main class is the ExportScript, which derives from ExternalScriptBase. This
+class can be used to export a selection of codingjobs to different formats
+through a Table object. Steps:
+
+    1. Create the right columns based on the coding schema(s)
+    2. Create a row generator that yields all coded units
+    3. Export the resulting table to the chosen format
+"""
+
+from __future__ import print_function, absolute_import
+
 import collections
 import datetime
+import itertools
 import logging; log = logging.getLogger(__name__)
 
 from amcat.tools.table import table3, tableoutput, table2spss
 from amcat.tools import toolkit, idlabel
 from amcat.tools.cachable import cacher
-from amcat.tools.logging import amcatlogging, ticker
+from amcat.tools.logging import amcatlogging, ticker, progress
 from amcat.model.coding.annotationschema import FieldColumn
 from amcat.model.coding import codingjob
 
@@ -14,18 +48,38 @@ from amcat.db import dbtoolkit
 
 BUFFERSIZE = 500
 
-amcatlogging.infoModule()
+amcatlogging.debugModule()
 amcatlogging.infoModule('progress')
 
 class Row(object):
+    "Class representing a row in a coding job export, with a coded article and possibly coded sentence"
     def __init__(self, ca, cs):
         self.ca = ca
         self.cs = cs
         self.art = ca.article
         
 class ExportScript(externalscripts.ExternalScriptBase):
+    """
+    Default CodingJob Export Script and base class for custom scripts.
 
+    Data argument for this script is a list of codingjobids.
+    Optional 'exportformat' argument is a string describing an export format (eg csv/spss)
+
+    ._run first class .getTable to create a table, which calls .getRows and .getcolumns.
+    .exportTable is then called to export the table in the requested format.
+
+    Most functions are called with a progress monitor argument to supply feedback to the end user.
+
+    A number of .cacheX functions exist to facilitate efficient database access.
+    """
+
+    def __init__(self):
+        super(ExportScript, self).__init__()
+        self.rowlimit = None
+        self.requireSentence=False
+    
     def _run(self, jobidlist, out, err, exportformat='csv', requireSentence=False, rowlimit=None):
+        "Entry point for exporting, sets up db and monitoring and calls getTable and exportTable"
         super(ExportScript, self)._run(jobidlist, out, err)
         self.rowlimit = rowlimit
         #self.rowlimit = 100
@@ -37,34 +91,49 @@ class ExportScript(externalscripts.ExternalScriptBase):
             self.requireSentence = requireSentence
             self.allowidlabel = False
             self.pm.worked(5)
-            t = self.getTable(jobs, self.pm.submonitor(10))
+            t = self.getTable(jobs, self.pm.submonitor(10), self.pm.submonitor(80, name="export"))
             log.info("Table created, starting export")
-            self.exportTable(t, exportformat, self.pm.submonitor(80, name="export"))
+            self.exportTable(t, exportformat)
             self.pm.worked(5)
-	#db.printProfile()
+        #db.printProfile()
 
-    def exportTable(self, table, format, monitor):
+    def exportTable(self, table, format):
+        """Export the table to self.out in the requested format
+        @type table: L{Table<amcat.tools.table.table3.Table>}
+        @param table: the table to export
+        @type format: string
+        @param format: the export format to use
+        @param monitor: a progress monitor to report feedback to
+        """
         if format == 'spss':
             #w = open('/tmp/log.spss', 'w')
             #table2spss.table2spss(table, w)
-            fn = table2spss.table2sav(table, monitor=monitor)
-            
+            fn = table2spss.table2sav(table)
             log.info("Saved as %s" % fn)
             self.out.write(open(fn).read())
         else:
-            tableoutput.table2csv(table, outfile=self.out, monitor=monitor)
+            tableoutput.table2csv(table, outfile=self.out)
         log.info("Done!")
         
     
-    def getTable(self, jobs, monitor):
+    def getTable(self, jobs, monitor=progress.NullMonitor(), exportmonitor=progress.NullMonitor()):
+        """Create a L{Table<amcat.tools.table.table3.Table>} object from the given jobs
+
+        The resulting Table object uses a row generator and column functions to create the
+        data as needed, so the 'hard work' is only done when actually using the table.
+
+        @param jobs: a sequence of CodingJob objects
+        @param monitor: a progress monitor to report setup feedback to
+        @param exportmonitor: a progress monitor to report iteration feedback to
+        @return: a Table object
+        """
         with monitor.monitored("Setting up table", 100) as pm:
-            self.cacheJobs(jobs)
-            pm.worked(20)
-            self.cacheSchemas(jobs)
-            pm.worked(20)
-        #log.debug(jobs[0].db.printProfileHTML())
+            self.cacheInitial(jobs)
+            pm.worked(40)
             log.debug("Setting up table - Creating row generator")
-            rows = toolkit.splitlist(self.getRows(jobs), buffercall=self.cacheRows, itemsperbatch=BUFFERSIZE, yieldelements=True)
+            rows = toolkit.splitlist(self.getRows(jobs, exportmonitor), buffercall=self.cacheRows, itemsperbatch=BUFFERSIZE, yieldelements=True)
+            if self.rowlimit:
+                rows = itertools.islice(rows, stop=self.rowlimit)
             pm.worked(20)
             log.debug("Setting up table - Creating columns")
             cols = self.getColumns(jobs)
@@ -74,125 +143,137 @@ class ExportScript(externalscripts.ExternalScriptBase):
             log.debug("Setting up table - Done")
             return t
 
-    def cacheJobs(self, jobs):
-        cacher.cache(jobs, "unitSchema", "articleSchema", "name", sets=dict(coder=["username"]))
+    def cacheInitial(self, jobs):
+        """Cache initial values, i.e. codingjob schema"""
+        codingjob.cacheCodingJobs(jobs, articles=False)
+        codebooks = set()
+        for job in jobs:
+            for schema in (job.unitSchema, job.articleSchema):
+                for field in schema.fields:
+                    if field.codebook: codebooks.add(field.codebook)
+        for c in codebooks:
+            c.cacheHierarchy()
+            c.cacheLabels()
 
-    def cacheSchemas(self, jobs):
-        schemas = set()
-        for j in jobs:
-            schemas |= set((j.unitSchema, j.articleSchema))
-        cacher.cache(schemas, "fields","location")
-
-        
-    def cacheMeta(self, rows):
-        return
-        cas = set(row.ca for row in rows)
-        cacher.cacheMultiple(cas, "article")
-        cacher.cacheMultiple(set(ca.article for ca in cas),
-                               "encoding", "headline", "date", "length", "pagenr", "source")
-        cacher.cacheMultiple(set(ca.article.source for ca in cas), "name")
-        css = set(row.cs for row in rows if row.cs)
-        #cacher.cache(css, sentence=["parnr","sentnr"])
-        
+    def cacheSentences(self, codedarticles):
+        """Cache the codedsentences for these codedarticles"""
+        log.debug("Caching sentences for %i codedarticles" % len(codedarticles))
+        cacher.cache(codedarticles, "sentences")
         
     def getCodedArticles(self, jobs):
-        cacher.cache(jobs, sets=["articles"])
-        return codingjob.getCodedArticlesFromCodingjobs(jobs)
-        
-    def getRows(self, jobs):
-        i = [0]
+        """Return the codedarticles objects in these jobs"""
+        return codingjob.getCodedArticlesFromCodingjobs(jobs, cacheValues=False)
+    
+    def getRows(self, jobs, monitor):
+        """Yield the Rows to be extracted from the jobs"""
         log.debug("Getting coded articles")
         cas = list(self.getCodedArticles(jobs))
-        log.debug("Got coded articles")
-        n = len(cas)
-        ndone = 0
-        def dobuffer(arts):
-            j =  i[0] +len(arts)
-            pct = int(float(i[0] * 100) / n)
-            log.debug("Getting articles %i - %i / %i (%i%%)" % (i[0], j, n, pct))
-            i[0] = j
-            cacher.cache(arts, "sentences", "article")
-            #log.debug(jobs[0].db.printProfileHTML())
-        for ca in toolkit.splitlist(cas, BUFFERSIZE, dobuffer, yieldelements=True):
-            sents = False
-            for cs in ca.sentences:
-                sents = True
-                yield Row(ca,cs)
-                ndone += 1
-                if self.rowlimit and ndone >= self.rowlimit: return
-                                
-            if (not sents) and (not self.requireSentence):
-                yield Row(ca, None)
-                ndone += 1
-                if self.rowlimit and ndone >= self.rowlimit: return
+        log.debug("Iterating over articles to return rows")
+        with monitor.monitored("Exporting", units=len(cas)):
+            for ca in toolkit.splitlist(cas, BUFFERSIZE, self.cacheSentences, yieldelements=True):
+                monitor.worked()
+                for row in self.getRowsFromCodedArticle(ca):
+                    yield row
 
-
+    def getRowsFromCodedArticle(self, codedarticle):
+        """yield the rows to be extracted from this coded article
+        Yields one Row for each codedsentence in each article, and
+        (unless self.requireSentence) one row for each codedarticle
+        with no codedsentences
+        """
+        sents = False
+        for cs in codedarticle.sentences:
+            sents = True
+            yield Row(codedarticle,cs)
+        if not sents and (not self.requireSentence):
+            yield Row(codedarticle, None)
+                
+                
     def cacheRows(self, rows):
-        self.cacheMeta(rows)
-        self.cacheFields(rows)
+        """Cache the values for these row objects
+        Calls cacheMeta and cacheFields with the codedarticles and codedsentences
+        """
+        log.debug("Caching values for %i rows" % len(rows))
+        cas = set(row.ca for row in rows)
+        css = set(row.cs for row in rows if row.cs)
+        self.cacheMeta(cas, css)
+        self.cacheFields(cas, css)
 
-    def cacheFields(self, rows):
-        units = collections.defaultdict(set) # schema : units
-        for row in rows:
-            ca = row.ca
-            job = ca.set.job
-            units[job.articleSchema].add(ca)
-            cs = row.cs
-            if cs: units[job.unitSchema].add(cs)
-       # for schema, units in units.iteritems():
-            #schema.cacheMany(units)
-        
+    def cacheMeta(self, codedarticles, codedsentences):
+        """Cache meta fields (headline, parnr etc.) for these articles and sentences"""
+        cacher.cache(codedarticles, article=dict(headline=[], date=[], length=[], pagenr=[], source=["name"]))
+        cacher.cache(codedsentences, sentence=["parnr","sentnr"])
+        sets = (ca.set for ca in codedarticles)
+        cacher.cache(sets, coder="username")
 
-        
+    def cacheFields(self,  codedarticles, codedsentences):
+        """Cache the coded values for these articles and sentences"""
+        cacher.cache(codedarticles, "values")
+        cacher.cache(codedsentences, "values")
         
     def getMetaColumns(self):
-	def getDate(row):
-	    d = row.art.date
-	    d2 = toolkit.toDate(d)
-	    return d
-	
-	return [
-	    table3.ObjectColumn("CodingJob", lambda row: row.ca.set.job, fieldtype=idlabel.IDLabel),
-	    table3.ObjectColumn("Set", lambda row: row.ca.set.setnr, fieldtype=int),
-	    table3.ObjectColumn("Coder", lambda row: row.ca.set.coder, fieldtype=idlabel.IDLabel),
-	    table3.ObjectColumn("ArticleId", lambda row: row.art.id, fieldtype=int),
-	    table3.ObjectColumn("Medium", lambda row: row.art.source, fieldtype=idlabel.IDLabel),
-	    table3.ObjectColumn("Pagenr", lambda row: row.art.pagenr, fieldtype=int),
-	    table3.ObjectColumn("Length", lambda row: row.art.length, fieldtype=int),
-	    #table3.ObjectColumn("Date", lambda row: row.art.date, fieldtype=datetime.datetime),
-	    table3.ObjectColumn("Date", getDate, fieldtype=datetime.datetime),
-	    table3.ObjectColumn("Headline", lambda row: row.art.headline, fieldtype=str),
-	    table3.ObjectColumn("CodingjobArticleid", lambda row: row.ca.id, fieldtype=int),
-	    table3.ObjectColumn("Arrowid", lambda row: row.cs and row.cs.id, fieldtype=int),
-	    ]
-    
-    def getSchemaFields(self, jobs, article):
-        fields = []
-        for job in jobs:
-            schema = job.articleSchema if article else job.unitSchema
-            sfields = schema.fields
-            for field in sfields:
-                if field not in fields: fields.append(field)
-        return fields
+        """Get Columns reprsenting meta fields (articleid, headline etc)
+        @returns: a sequence of L{Column<amcat.tools.table.table3.Column>} objects """
+        def getDate(row):
+            d = row.art.date
+            d2 = toolkit.toDate(d)
+            return d
+        
+        return [
+            table3.ObjectColumn("CodingJob", lambda row: row.ca.set.job, fieldtype=idlabel.IDLabel),
+            table3.ObjectColumn("Set", lambda row: row.ca.set.setnr, fieldtype=int),
+            table3.ObjectColumn("Coder", lambda row: row.ca.set.coder, fieldtype=idlabel.IDLabel),
+            table3.ObjectColumn("ArticleId", lambda row: row.art.id, fieldtype=int),
+            table3.ObjectColumn("Medium", lambda row: row.art.source, fieldtype=idlabel.IDLabel),
+            table3.ObjectColumn("Pagenr", lambda row: row.art.pagenr, fieldtype=int),
+            table3.ObjectColumn("Length", lambda row: row.art.length, fieldtype=int),
+            #table3.ObjectColumn("Date", lambda row: row.art.date, fieldtype=datetime.datetime),
+            table3.ObjectColumn("Date", getDate, fieldtype=datetime.datetime),
+            table3.ObjectColumn("Headline", lambda row: row.art.headline, fieldtype=str),
+            table3.ObjectColumn("CodingjobArticleid", lambda row: row.ca.id, fieldtype=int),
+            table3.ObjectColumn("Arrowid", lambda row: row.cs and row.cs.id, fieldtype=int),
+            ]
 
-    def getColumn(self, field, article):
-        return [FieldColumn(field, article)]
+
+    def getColumns(self, jobs):
+        """Create the columns for the table to be exported
+        Combines the results of getMetaCoilumns, getArticleAnnotationColumns, and getUnitAnnotationColumns
+        @returns: a sequence of L{Column<amcat.tools.table.table3.Column>} objects """
+        
+        return (self.getMetaColumns()
+                + list(self.getArticleAnnotationColumns(jobs))
+                + list(self.getUnitAnnotationColumns(jobs)))
     
-    def getAnnotationColumns(self, jobs, article):
-        for field in self.getSchemaFields(jobs, article):
-            for column in self.getColumn(field, article):
+    def getSchemaFields(self, jobs, articlecodings):
+        """get the annotationschemafields corresponding to these jobs
+        @param jobs: a sequence of CodingJob objects
+        @param isarticle: if True, return article annotation schema fields, otherwise unit fields
+        @returns: a sequence of L{Column<amcat.tools.table.table3.Column>} objects """
+
+        getfields = ((lambda job: job.articleSchema.fields) if articlecodings else
+                     (lambda job: job.unitSchema.fields))
+        return toolkit.unique(toolkit.flatten(itertools.imap(getfields, jobs)))
+
+    def getColumnsForField(self, field):
+        """get the column(s) representing this schemafield"""
+        return [FieldColumn(field)]
+    
+    def getAnnotationColumns(self, jobs, articlecodings):
+        """Create the annotationcolumns for these jobs"""
+        for field in self.getSchemaFields(jobs, articlecodings):
+            for column in self.getColumnsForField(field):
                 yield column
             
 
     def getArticleAnnotationColumns(self, jobs):
+        """Create the article annotation columns for these jobs
+        returns getAnnotationColumns(jobs, True)"""
         return self.getAnnotationColumns(jobs, True)
     def getUnitAnnotationColumns(self, jobs):
+        """Create the unit annotation columns for these jobs
+        returns getAnnotationColumns(jobs, True)"""
         return self.getAnnotationColumns(jobs, False)
             
-    def getColumns(self, jobs):
-        return (self.getMetaColumns()
-                + list(self.getArticleAnnotationColumns(jobs))
-                + list(self.getUnitAnnotationColumns(jobs)))
     
     def _getOutputType(self, exportformat='csv', *args):
         """How to interpret the output file, i.e. return its (mime) type"""
--- scripts/export_codingjobresults_dnn.py
+++ scripts/export_codingjobresults_dnn.py
@@ -3,11 +3,11 @@ import logging; log = logging.getLogger(__name__)
 
 from amcat.scripts.export_codingjobresults import ExportScript, FieldColumn, Row
 from amcat.tools import toolkit
-from amcat.model.coding import codingjob
+from amcat.model.coding import codingjob, annotationschema
 from amcat.tools.table import table3, tableoutput
 from amcat.model.ontology.object import Object
 from amcat.model.ontology.tree import Tree
-from amcat.model.coding.codingjob import getValue
+
 
 #from amcat.tools.logging import amcatlogging; amcatlogging.debugModule()
 
@@ -16,25 +16,25 @@ def getTable(jobs, *args, **kargs):
 
 CATEGORIES = {"cat":2,"root":1,"class":0}
 
-CODEBOOK_CACHE = {}
-DIM_TREE = None
 
+CODEBOOK_CACHE = {}
 def getCodebook(codebook):
     if codebook.id not in CODEBOOK_CACHE:
-	CODEBOOK_CACHE[codebook.id] = codebook
+        CODEBOOK_CACHE[codebook.id] = codebook
     return CODEBOOK_CACHE[codebook.id]
 
+DIM_TREE = None
 def getDimTree(db):
     global DIM_TREE
     if DIM_TREE is None:
-	DIM_TREE = Tree(db, 5001)
+        DIM_TREE = Tree(db, 5001)
     return DIM_TREE
 
 class DNNFieldColumn(FieldColumn):
-    def __init__(self, field, article, ontoption=None):
-	log.debug("Created DNNFIeldColumn for %s / %s" % (field, ontoption))
-		
-        FieldColumn.__init__(self, field, article)
+    def __init__(self, field, ontoption=None):
+        log.debug("Created DNNFIeldColumn for %s / %s" % (field, ontoption))
+                
+        FieldColumn.__init__(self, field)
         self.ontoption = ontoption
         self.label = field.fieldname
         if self.ontoption:
@@ -52,45 +52,46 @@ class DNNFieldColumn(FieldColumn):
         elif self.ontoption == "dim":
             val = self.codebook.categorise(val, date=row.art.date, depth=2)[-1]
             if val:
-		val = getDimTree(val.objekt.db).getParent(val)
-		if val is None: return ""
+                val = getDimTree(val.objekt.db).getParent(val)
+                if val is None: return ""
         return val.objekt
 
 class AggrQualColumn(FieldColumn):
-    def __init__(self, field, article, ontoption='cat'):
+    def __init__(self, field, ontoption='cat'):
         self.label = "aggrqual"
         self.ontoption = ontoption
         if ontoption != 'cat': self.label += ontoption
-        FieldColumn.__init__(self, field, article, fieldname=self.label, label=self.label)
+        FieldColumn.__init__(self, field, fieldname=self.label, label=self.label)
 
     def getCell(self, row):
         val = super(self.__class__, self).getCell(row)
         if val is None: return
         for fieldname in ("subject", "object"):
             field = self.field.schema.getField(fieldname)
-	    codebook = getCodebook(field.serializer.codebook)
-            obj = getValue(self.getUnit(row), field)
+            codebook = getCodebook(field.serializer.codebook)
+            obj = annotationschema.getValue(self.getUnit(row), field)
             if not obj: continue
             omklap = codebook.categorise(obj, date=row.art.date, depth=CATEGORIES[self.ontoption],
-					 returnReverse=True, returnObject=False)[-1]
+                                         returnReverse=True, returnObject=False)[-1]
             log.debug("omklap for %s %s %s = %s" % (self.ontoption, fieldname, obj, omklap))
             
-	    if omklap: val *= -1
+            if omklap: val *= -1
         return val
         
 CODER_HIERARCHY = {206:280}
 class DNNExportScript(ExportScript):
-    def getColumn(self, field, article):
+    def getColumnsForField(self, field):
         if issubclass(field.getTargetType(), Object):
             #for option in [None, "cat", "root", "class", "dim"]:
-	    for option in [None, "cat", "root", "dim"]:
-                yield DNNFieldColumn(field, article, option)
+            for option in [None, "cat", "root", "dim"]:
+                yield DNNFieldColumn(field, option)
         else:
-            yield FieldColumn(field, article)
+            for col in super(DNNExportScript, self).getColumnsForField(field):
+                yield col
         if field.fieldname == 'quality':
             #yield AggrQualColumn(field, article)#, ontoption=option)
             for option in ["cat"]:
-                yield AggrQualColumn(field, article, ontoption=option)
+                yield AggrQualColumn(field, ontoption=option)
 
     def getMetaColumns(self):
         return super(DNNExportScript, self).getMetaColumns() + [
@@ -100,48 +101,37 @@ class DNNExportScript(ExportScript):
             ]
 
     def getCodedArticles(self, jobs):
-        # Need to fix caching FK relations with composite keys !
-        #cachable.cacheMultiple(jobs, "sets")
-        #toolkit.ticker.warn("--- !b")
-        #sets = []
-        #for j in jobs: sets += j.sets
-        #cachable.cacheMultiple(sets, "articles", "coder")
-        #toolkit.ticker.warn("--- !c")
-        #cas = []
-        #for set in sets: cas += set.articles
-        #cachable.cacheMultiple(cas, "article")
-        #cachable.cache(jobs, sets=dict(articles=["article"], coder=[]))
         casPerArt = collections.defaultdict(set)
         for ca in codingjob.getCodedArticlesFromCodingjobs(jobs):
             casPerArt[ca.article].add(ca)
         for cas in casPerArt.values():
             yield sorted(cas, key=lambda ca:CODER_HIERARCHY.get(ca.set.coder.id, ca.set.coder.id))[0]
             
-        
-    def cacheField(self, field):
-        if isinstance(field, codingjob.OntologyAnnotationSchemaField):
-            toolkit.ticker.warn(">>>>>>>>>>> %s" % field.fieldname)
-            field.set.cacheHierarchy()
-            toolkit.ticker.warn("<<<<<<<<<< %s" % field.fieldname)
-        else:
-            super(DNNExportScript, self).cacheField(field)
 
 def test():
-    import dbtoolkit
-    db = dbtoolkit.amcatDB(profile=True)
-    db.beforeQueryListeners.add(lambda a: toolkit.ticker.warn(a[:250]))
-    #ids = db.doQuery("select codingjobid from codingjobs where projectid=388")
-    #ids = [id for (id,) in ids]
-    ids = [3789]
-    #ids = [4240]
-    toolkit.ticker.warn(ids)
-    jobs = [codingjob.Codingjob(db, id) for id in ids]
-    t = DNNExportScript().getTable(jobs)
-    import table2spss
-    table2spss.table2sav(t, "/tmp/test.sav")
+    from amcat.db import dbtoolkit
+    from amcat.model.coding import codedsentence
 
+    from amcat.tools.logging import amcatlogging
+    amcatlogging.setup()
+    
+    db = dbtoolkit.amcatDB(profile=True)
+    arrowid = 686970
+    cs = codedsentence.CodedSentence(db, arrowid)
+    ca = cs.codedarticle
+    job = ca.codingjobset.job
+    row = Row(ca, cs)
+    print row
+    print row.cs
+    print row.cs.values
+    
+    s = DNNExportScript()
+    cols = s.getColumns([job])
+    for col in cols:
+        print col, col.getCell(row)
 
     
 if __name__ == '__main__':
-    DNNExportScript().runFromCommand()
+    test()
+    #DNNExportScript().runFromCommand()
     
