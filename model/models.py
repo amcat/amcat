c1a6104a1860fea533b3d4c0a114643b56153967
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2011-06-20 16:42:01 +0200

    Moved test.py

---
+++ model/models.py
@@ -0,0 +1,27 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""This module holds all models defined in this package."""
+
+from amcat.model.article import Article
+from amcat.model.language import Language
+from amcat.model.medium import Medium
+from amcat.model.project import Project
+from amcat.model.sentence import Sentence
+from amcat.model.user import User, Affiliation
