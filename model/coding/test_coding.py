01f72e05a29190b394377026ddd52c6a0d992769
Wouter van Atteveldt <wouter@vanatteveldt.com>
2011-11-08 17:00:04 +0100

    added annotation, stub integration test for coding

---
+++ model/coding/test_coding.py
@@ -0,0 +1,32 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+"""
+Test module to make sure that the amcat facilities for manual coding work
+as expected. The tests on the individual model classes should test whether
+they work separately. This tests whether the end-use of the classes taken
+together works as intended (eg a sort of integration test).
+"""
+
+
+from amcat.tools import amcattest
+
+class TestAnnotations(amcattest.PolicyTestCase):
+    def test_test(self):
+        print(1)
