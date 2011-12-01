38984cd139b1755af266fbc2b8a9e285fe7f9683
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2011-12-01 12:41:40 +0100

    Added MediumDict model

--- model/medium.py
+++ model/medium.py
@@ -20,6 +20,26 @@ class Medium(AmcatModel):
     #type = models.ForeignKey(MediumSourcetype, db_column='medium_source_id')
     language = models.ForeignKey("amcat.Language")
 
+    @classmethod
+    def get_by_name(cls, name, ignore_case=True):
+        """
+        Get a medium by label, accounting for MediumDict.
+
+        @param label: label to look for
+        @param ignore_case: use __iexact
+        """
+        query = dict(name__iexact=name) if ignore_case else dict(name=name)
+
+        try:
+            return Medium.objects.get(**query)
+        except Medium.DoesNotExist:
+            pass
+
+        try:
+            return MediumDict.objects.get(**query).medium
+        except Medium.DoesNotExist:
+            raise Medium.DoesNotExist("%s could be found in medium nor medium_dict" % name)
+
     def __unicode__(self):
         return self.name
 
@@ -28,4 +48,18 @@ class Medium(AmcatModel):
         verbose_name_plural = 'media'
         app_label = 'amcat'
     
+class MediumDict(AmcatModel):
+    """
+    Provide multiple names names per medium. Please use get_by_name on
+    Medium to select a medium.
+    """
+    medium = models.ForeignKey(Medium)
+    name = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.name
+
+    class Meta():
+        db_table = 'media_dict'
+        verbose_name_plural = 'media_dicts'
+        app_label = 'amcat'
