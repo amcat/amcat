81c64c6b7740b44db11623ab408f96c3383ba79e
Wouter van Atteveldt <wouter@vanatteveldt.com>
2012-02-15 01:40:22 +0100

    fixed amcatsolr and added unit test

--- tools/amcatsolr.py
+++ tools/amcatsolr.py
@@ -22,11 +22,10 @@ Function for adding/removing Amcat articles to Solr from the model
 """
 from __future__ import unicode_literals, print_function, absolute_import
 
+import datetime, time, collections, re
 import solr
-import datetime, time
-from amcat.models.article import Article
 from django.db import connection
-import re
+from amcat.models.article import Article
 
 import logging
 log = logging.getLogger(__name__)
@@ -47,30 +46,30 @@ def _stripChars(text):
     """
     if not text: return None
     return  re.sub('[\x00-\x08\x0B\x0C\x0E-\x1F]', ' ', text);
-      
-def index_articles(article_ids):
-    article_ids = set(article_ids)
-    to_add = set(a.id for a in Article.objects.filter(
-            pk__in=article_ids, project__indexed=True, project__active=True))
-    ids_to_add = set(a.id for a in to_add)
-    ids_to_remove = articleids - articleidsToAdd
-    
-    log.debug("adding/updating %s articles, removing %s" % (len(articleidsToAdd), len(articleidsToRemove)))
 
-    s = solr.SolrConnection('http://localhost:8983/solr')
-    if to_add:
-        log.debug("finding sets") # TODO: make the code below nicer
-        cursor = connection.cursor()
-        cursor.execute("SELECT articleset_id, article_id FROM articlesets_articles WHERE article_id in (%s)" % ','.join(map(str, ids_to_add)))
-        rows = cursor.fetchall()
-        log.debug("%s sets found" % len(rows))
-        setsDict = collections.defaultdict(list)
-        for setid, articleid in rows:
-            setsDict[articleid].append(setid)
+def _include(article):
+    return article.project.active and article.project.indexed
 
-        log.debug("creating article dicts")
-            
-        articles = [dict(id=a.id,
+def _ids_to_solr_mutations(article_ids):
+    """Given a sequence of ids, return a pair of (article dicts to add, ids to remove)"""
+    articles = Article.objects.filter(pk__in=article_ids).select_related('project__active', 'project__indexed')
+
+    # cache set membership
+    # TODO: use django querysets instead of sql
+    # see development version https://docs.djangoproject.com/en/dev/ref/models/querysets/
+    #                                   #django.db.models.query.QuerySet.prefetch_related
+    setsDict = collections.defaultdict(list)
+    idstr = ",".join(str(a.id) for a in articles if _include(a))
+    cursor = connection.cursor()
+    sql = "SELECT articleset_id, article_id FROM articlesets_articles WHERE article_id in (%s)" % idstr
+    cursor.execute(sql)
+    for setid, articleid in cursor.fetchall():
+        setsDict[articleid].append(setid)
+
+    to_add, to_remove = [], []
+    for a in articles:
+        if _include(a):
+            to_add.append(dict(id=a.id,
                          headline=_stripChars(a.headline), 
                          body=_stripChars(a.text),
                          byline=_stripChars(a.byline), 
@@ -78,17 +77,60 @@ def index_articles(article_ids):
                          projectid=a.project_id,
                          mediumid=a.medium_id,
                          date=a.date.replace(tzinfo=GMT1()),
-                         sets=setsDict.get(a.id))
-                    for a in to_add]
-             
-        log.debug("adding %s articles" % len(articlesDicts))
-        s.add_many(articlesDicts)
+                         sets=setsDict.get(a.id)))
+        else:
+            to_remove.append(a.id)
+    return to_add, to_remove
+            
+        
     
+
+def index_articles(article_ids):
+    to_add, to_remove = _ids_to_solr_mutations(article_ids)   
+    
+    log.debug("adding/updating %s articles, removing %s" % (len(to_add), len(to_remove)))
+
+    s = solr.SolrConnection('http://localhost:8983/solr')
+    if to_add:
+        s.add_many(to_add)
     if to_remove:
-        log.debug("deleting")
         s.delete_many(to_remove)
-    solr.commit()
+    s.commit()
 
                  
 if __name__ == '__main__':
+    from amcat.tools import amcatlogging
+    amcatlogging.setup()
+    amcatlogging.debug_module()
     index_articles([1,2])
+
+    
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+        
+from amcat.tools import amcattest
+
+
+class TestSolr(amcattest.PolicyTestCase):
+    def test_ids2solr(self):
+        p1 = amcattest.create_test_project(active=True, indexed=True)
+        p2 = amcattest.create_test_project(active=True, indexed=False)
+
+        a1 = amcattest.create_test_article(headline="a1", project=p1)
+        a2 = amcattest.create_test_article(headline="a2", project=p1)
+        a3 = amcattest.create_test_article(headline="a3", project=p2)
+
+
+        # allow two queries:
+        #   1 to get article info (inc. project)
+        #   1 to get set membership
+        with self.checkMaxQueries(2, "Get mutations"):
+            to_add, to_remove = _ids_to_solr_mutations([a1.id, a2.id, a3.id])
+        self.assertEqual(to_remove, [a3.id])
+        self.assertEqual(len(to_add), 2)
+        self.assertEqual(set(a['headline'] for a in to_add), set(["a1","a2"]))
+        
+        
+    
