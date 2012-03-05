d503a68a92fcd3228d5f589a84b6f641be4e47fe
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2012-03-05 13:19:31 +0100

    Using 'real' articleids

--- scraping/document.py
+++ scraping/document.py
@@ -56,7 +56,7 @@ class Document(object):
         """@param parent: """
         self.props = Properties()
         self.parent = parent
-        self.id = None
+        self.article = None
 
         for k,v in kargs.items():
             setattr(self.props, k, v)
@@ -97,6 +97,8 @@ class Document(object):
                 _metastring[prop] = value
 
         art.metastring = str(_metastring)
+        self.article = art
+
         return art
 
 class HTMLDocument(Document):
@@ -168,7 +170,7 @@ class IndexDocument(HTMLDocument):
         self.children.append(child)
 
     def getprops(self):
-        committed = [(child.id is not None) for child in self.children]
+        committed = [(child.article.id is not None) for child in self.children]
         if not all(committed):
             raise(ValueError("Please yield the index-page *after* all children."))
 
@@ -185,7 +187,7 @@ class IndexDocument(HTMLDocument):
         for child in self.children:
             for coord in child.coords:
                 coord = ", ".join(map(str, coord))
-                text.append("[%s -> %s]" % (coord, child.id))
+                text.append("[%s -> %s]" % (coord, child.article.id))
 
         return dict(headline=headline, text="\n".join(text),
                     page=self.page, **self.props.__dict__)
