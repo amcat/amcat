e760542b58b2f7e03d0612838e6e2783f2bf5fd2
Wouter van Atteveldt <wouter@vanatteveldt.com>
2012-04-29 21:37:44 +0200

    generalized, renamed, and simplified solr clean script

--- scripts/actions/solr_clean.py
+++ scripts/actions/solr_clean.py
@@ -28,54 +28,47 @@ import logging; log = logging.getLogger(__name__)
 import solr
 from django import forms
 
-from amcat.models.project import Project
-from amcat.models.article import Article
+from amcat.models import Project, ArticleSet, Article, ArticleSetArticle
 from amcat.models.analysis import add_to_queue
-
 from amcat.scripts.script import Script
-
-BATCH = 100
-def get_articleids(projectid):
-    query = "projectid:{projectid}".format(**locals())
-    s = solr.Solr('http://localhost:8983/solr')
-
-    start =0
-    while True:
-        result =s.select(query,fields=["id"],score=False, start=start, rows=BATCH).results
-        if not result: break
-        for row in result:
-            yield row["id"]
-        start += BATCH
-
-class CleanProjectForm(forms.Form):
-    project = forms.ModelChoiceField(queryset=Project.objects.all())
+from amcat.tools.amcatsolr import Solr
 
 
-class CleanProject(Script):
-    options_form = CleanProjectForm
+class SolrCleanForm(forms.Form):
+    projects = forms.ModelMultipleChoiceField(queryset=Project.objects.all(), required=False)
+    sets = forms.ModelMultipleChoiceField(queryset=ArticleSet.objects.all(), required=False)
+    include_project_sets = forms.BooleanField(initial=False, required=False)
+
+    def clean(self):
+        data = super(SolrCleanForm, self).clean()
+        if not data['projects'] and not data['sets']:
+            raise forms.ValidationError("Please list the projects or sets to clean")
+        return data
+        
+class SolrClean(Script):
+    options_form = SolrCleanForm
 
     def run(self, _input):
-        project = self.options["project"]
-        log.info("Cleaning project {project.id} : {project}".format(**locals()))
-        log.debug("Getting article list from db")
-        db_ids = set(aid for (aid,) in Article.objects.filter(project=project).values_list("id"))
-        log.debug("Retrieved {n} articles, getting list of articles to add or delete"
-                  .format(n=len(db_ids)))
-        to_delete = []
-        for id in get_articleids(project.id):
-            try:
-                db_ids.remove(id)
-            except KeyError:
-                to_delete.append(id)
+        projects, sets = list(self.options["projects"]), list(self.options["sets"])
+        if self.options["include_project_sets"]:
+            sets += list(ArticleSet.objects.filter(project__in=projects))
+        log.info("Cleaning projects {projects}, sets {sets}".format(**locals()))
 
-        log.info("Deleting {} articles from index".format(len(to_delete)))
-        if to_delete:
-            s = solr.SolrConnection(b'http://localhost:8983/solr')
-            s.delete_many(to_delete)
-            s.commit()
-        log.info("Adding {} articles to be checked".format(len(db_ids)))
-        if db_ids:
-            add_to_queue(*db_ids)
+        articles = set()
+        if projects:
+            q = Article.objects.filter(project__in=projects)
+            articles |= set(aid for (aid,) in q.values_list("id"))
+        if sets:
+            q = ArticleSetArticle.objects.filter(articleset__in=sets)
+            articles |= set(aid for (aid,) in q.values_list("article_id"))
+
+        log.info("Cleaning {n} articles".format(n=len(articles)))
+
+        Solr().add_articles(articles)
+        
+        log.info("Done!")
+
+        #TODO: also delete articles that should be deleted...
 
 if __name__ == '__main__':
     from amcat.scripts.tools import cli
