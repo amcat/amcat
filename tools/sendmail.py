43b8883ce2a8aeff19538b99f2f8fed8e071f19c
Wouter van Atteveldt <wouter@vanatteveldt.com>
2012-11-20 01:23:20 +0100

    switch to 'django mail'

--- tools/sendmail.py
+++ tools/sendmail.py
@@ -17,69 +17,22 @@
 # License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
 ###########################################################################
 
-"""Module for creating an email message and sending it using /usr/sbin/sendmail"""
+"""
+Module for creating an email message and sending it using django
+Can probably be refactored away since it is so trivial now?
+"""
 
 from __future__ import unicode_literals, print_function, absolute_import
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-from amcat.tools import toolkit
-
-
-SENDMAIL = "/usr/sbin/sendmail"
-
-def _create_message(sender, recipient, subject, html, plain):
-    """Create a mime multipart message ready for sending"""
-    # thanks to http://code.activestate.com/recipes/473810/ 
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = subject
-    msgRoot['From'] = sender
-    msgRoot['To'] = recipient
-    msgRoot.preamble = 'This is a multi-part message in MIME format.'
-
-    # Encapsulate the plain and HTML versions of the message body in an
-    # 'alternative' part, so message agents can decide which they want to display.
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
-
-    msgAlternative.attach(MIMEText(plain))
-
-    msgAlternative.attach(MIMEText(html, 'html'))
-    
-    return msgRoot.as_string()
+from django.core.mail import EmailMultiAlternatives
 
 def sendmail(sender, recipient, subject, html, plain):
-    """Send an email from sender to recipient using the unix sendmail command"""
-    mail = _create_message(sender, recipient, subject, html, plain)
-    cmd = "%s -t" % SENDMAIL
-    out = toolkit.execute(cmd, mail, outonly=True)
-    if out.strip(): raise Exception(out)
+    msg = EmailMultiAlternatives(subject, plain, sender, [recipient])
+    msg.attach_alternative(html, "text/html")
+    msg.send()
     
 if __name__ == '__main__':
     # actual test in __name__ to prevent getting an email everytime the tests run
     sendmail("wouter@vanatteveldt.com", "vanatteveldt@gmail.com", "Test mailtje",
              "bla <b>nla!</b>", "bla bla")
         
-
-
-###########################################################################
-#                          U N I T   T E S T S                            #
-###########################################################################
-        
-from amcat.tools import amcattest
-
-class TestSendMail(amcattest.PolicyTestCase):
-
-    def test_create_message(self):
-        """Can we create a message?"""
-        msg = _create_message("wouter@vanatteveldt.com", "vanatteveldt@gmail.com",
-                              "Test mailtje", "bla <b>nla!</b>", "bla bla")
-        self.assertIn("\nFrom: wouter@vanatteveldt.com\n", msg)
-        self.assertIn("\nTo: vanatteveldt@gmail.com\n", msg)
-        self.assertIn('Content-Type: text/html; charset="us-ascii"'
-                      '\nMIME-Version: 1.0\nContent-Transfer-Encoding: 7bit\n\n'
-                      'bla <b>nla!</b>\n', msg)
-
-        
-     
