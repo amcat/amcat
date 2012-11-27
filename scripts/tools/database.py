27bf16744d94e04e8fb5c8364091f56238e447ab
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2012-11-27 16:12:36 +0100

    Fixed issue #199

--- scripts/tools/database.py
+++ scripts/tools/database.py
@@ -9,9 +9,10 @@ def getQuerySet(projects=None, articlesets=None, mediums=None, startDate=None, e
     if articlesets:
         queryset = queryset.filter(articlesets__in=articlesets)
     elif projects: 
-        queryset = queryset.filter(project__in=projects)
+        queryset = queryset.filter(articlesets__project__in=projects)
     else:
         raise ValueError("Either projects or article sets needs to be specified")
+
     if mediums:
         queryset = queryset.filter(medium__in=mediums)
     if startDate:
