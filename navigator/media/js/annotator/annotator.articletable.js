daa602026e852b7bde0be4c858dd1d6fada676e4
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-02-18 15:21:04 +0100

    Fixed issue #245

--- navigator/media/js/annotator/annotator.articletable.js
+++ navigator/media/js/annotator/annotator.articletable.js
@@ -17,6 +17,7 @@
 * License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  *
 ***************************************************************************/
 
+(function(){
 
 annotator.articletable = {};
 annotator.articletable.MAX_STR_COLUMN_LENGTH = 35; // length of string when limiting length (in article table for headline/comment)
@@ -81,6 +82,124 @@ annotator.articletable.highlight = function(){
     });
 };
 
+// Function to force redraw (recalculation) of element
+var redraw = function(){
+    var ut = $("#unitcoding-table");
+    var _prev = ut.css("display");
+    ut.css("display", "none");
+    ut.css("display", _prev);
+}
+
+/*
+ * Helper function of fit_table(), which decreases the headers.
+ *
+ * @return: true if table resized, false if not
+ */
+annotator.articletable.decrease_headers = function(min_size){
+    var ut = $("#unitcoding-table");
+    var headers = $("th", ut);
+    var old_table_size = ut.outerWidth();
+
+    // Determine largest header
+    var max_found = 0;
+    $.each(headers, function(i, header){
+        max_found = Math.max(max_found, $(header).text().length);
+    });
+
+    if (max_found <= min_size || min_size <= _DECREASE_BY){
+        // All headers are already resized to their minimum
+        return false;
+    }
+
+    var aim = max_found - _DECREASE_BY;
+    var decreased_headers = [];
+    $.each(headers, function(i, header){
+        header = $(header);
+
+        var cur_size = header.text().length;
+        var new_size = cur_size - _DECREASE_BY;
+
+        if (cur_size <= min_size || cur_size <= aim){
+            // Smaller than we can accept.
+            return;
+        }
+
+        // We would decrease the current header too much if
+        // we blindly accept the new_size in case this headers
+        // length is less than we aim for.
+        new_size = (new_size < aim) ? aim : new_size;
+
+        // Save original header for users hovering
+        if (header.attr("title") === undefined){
+            header.attr("title", header.text());
+        }
+
+        // Save current state in case resizing fails
+        header.attr("__prev", header.text());
+        header.text(header.text().substr(0, aim));
+
+        //console.log(headers.toArray().indexOf(header.get(0)));
+        decreased_headers.push(header);
+    });
+
+    // Force redraw
+    redraw();
+    
+    if (old_table_size <= ut.outerWidth()){
+        // Resizing failed :-(
+        $.each(decreased_headers, function(i, header){
+            header.text(header.attr("__prev"));
+            header.attr("__prev", undefined);
+        });
+
+        return false;
+    }
+
+    return true;
+}
+
+/*
+ * Some codingschemas have a lot of fields, causing the table
+ * to grow larger than it actually can.
+ *
+ * @return: resizing succesfull
+ */
+annotator.articletable.fit_table = function(min_size_input, min_size_header){
+    // Default min_size_input / min_size_header to 50px / 8px
+    min_size_input = (min_size_input === undefined) ? 50 : min_size_input;
+    min_size_header = (min_size_header === undefined) ? 8 : min_size_header;
+    
+    var ut = $("#unitcoding-table");
+    var headers = $("th", ut);
+    var inputs = $("input[type=text]", ut);
+    var aimed_size = ut.parent().outerWidth();
+
+
+    // Check if table initialised yet
+    if(ut.children().length < 2){
+        window.setTimeout(annotator.articletable.fit_table, 200);
+        return;
+    }
+
+    // Check if previous resize matched our goals
+    if (ut.outerWidth() <= aimed_size){
+        return true;
+    }
+
+    // Try to resize headers by deleting characters
+    var decr_h = annotator.articletable.decrease_headers;
+    while(ut.outerWidth() >= aimed_size && decr_h(min_size_header)){ }
+
+    if (ut.outerWidth() > aimed_size){
+        console.log("Could not resize table to width of screen..");
+    } else {
+        $.each(inputs, function(i, input){
+            redraw();
+            //$(input).width("100%");
+        });
+    }
+
+}
 
 annotator.articletable.showArticleFromRow = function(row){
     var aid = row.children('td:first').text(); // get article id from first column of current row
@@ -111,6 +230,7 @@ annotator.articletable.showArticleFromRow = function(row){
             console.debug('set fields for unit');
 
             annotator.articletable.highlight();
+            annotator.articletable.fit_table();
         },
         "error": function(jqXHR, textStatus, errorThrown){
             console.debug('error loading article');
@@ -240,4 +360,4 @@ annotator.articletable.trimTableColumnString = function(obj) { // make sure that
     return '<div class="widthlimit">' + sReturn + '</div>';
 };
 
-
+})();
