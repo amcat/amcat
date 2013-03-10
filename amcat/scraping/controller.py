67185250f8da806ed4812e79d78412af093301b1
Toon Alfrink <toon.alfrink@gmail.com>
2013-03-10 17:45:59 +0100

    fix unicode error in controller logging

--- amcat/scraping/controller.py
+++ amcat/scraping/controller.py
@@ -85,7 +85,7 @@ class RobustController(Controller):
         log.info("RobustController starting scraping for scraper {}".format(scraper))
         result = []
         for unit in scraper.get_units():
-            log.debug("{scraper} received unit {unit}".format(**locals()))
+            log.debug("{scraper} received unit {unit}".format(**locals()).decode('utf-8'))
             
             try:
                 for article in self.scrape_unit(scraper, unit):
