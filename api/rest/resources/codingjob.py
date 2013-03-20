7545ed4e8a4bf4e317bb16d55188b25bc811f99b
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-03-20 15:33:10 +0100

    Partially fixed issue number (do not mark as fixed Google Code\!) 364

--- api/rest/resources/codingjob.py
+++ api/rest/resources/codingjob.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public        #
 # License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
 ###########################################################################
-from amcat.tools.caching import cached
+from amcat.tools.caching import cached_named
 from amcat.models import CodingJob 
 from amcat.models.coding import coding
 
@@ -28,8 +28,38 @@ from api.rest.resources.amcatresource import AmCATResource
 from api.rest.serializer import AmCATModelSerializer
 
 from django.db.models import Count
+from django.db import transaction, connection, DatabaseError
+
+import logging; log = logging.getLogger(__name__)
 
 STATUS_DONE = (coding.STATUS_COMPLETE, coding.STATUS_IRRELEVANT)
+
+def exec_without_seqscan(func):
+    """
+    Execute this function with (Postgres) seqscan disabled. This
+    forces Postgres to use its indices. Its purpose is to correct
+    the behaviour of postgres' query-planner, when retrieving the
+    amount of articles of a codingjob.
+
+    Because these amounts are small compared to the vast amount of
+    articles in 'normal' articlesets, the planner mistakenly estimates
+    that >= 5% of the total amount of entries in the db will be
+    touched. Because of this, it decides to use a sequential scan
+    which should be faster. (But since its estimates are wrong, it
+    is - in reality - a lot slower.)
+    """
+    def wrapped(self):
+        try:
+            with transaction.commit_on_success():
+                cursor = connection.cursor()
+                cursor.execute("set enable_seqscan=false;")
+                log.info("blaat")
+                return func(self)
+        except DatabaseError as e:
+            log.exception("Could not disable sequential scan!")
+
+        return func(self)
+    return wrapped
 
 class CodingJobSerializer(AmCATModelSerializer):
     """
@@ -41,16 +71,23 @@ class CodingJobSerializer(AmCATModelSerializer):
     n_articles = serializers.SerializerMethodField('get_n_articles')
     n_codings_done = serializers.SerializerMethodField('get_n_done_jobs')
 
-    @cached
-    def _get_n_done_jobs(self):
-        return dict(self.context['view'].object_list.qs.distinct().filter(
-                    codings__status__in=STATUS_DONE).annotate(Count("codings"))
-                    .values_list("id", "codings__count"))
+    @property
+    def qs(self):
+        # Reset distinct on specific field (not supported with annotate())
+        return self.context['view'].object_list.qs.distinct()
 
-    @cached
+    @cached_named("done_jobs")
+    @exec_without_seqscan
+    def _get_n_done_jobs(self):
+        return dict(
+            self.qs.filter(codings__status__in=STATUS_DONE)
+            .annotate(Count("codings")).values_list("id", "codings__count")
+        )
+
+    @cached_named("article_count")
+    @exec_without_seqscan
     def _get_n_articles(self):
-        return dict(self.context['view'].object_list.qs.distinct()
-                .annotate(n=Count("articleset__articles")).values_list("id", "n"))
+        return dict(self.qs.annotate(n=Count("articleset__articles")).values_list("id", "n"))
 
     def get_n_articles(self, obj):
         return self._get_n_articles().get(obj.id, 0)
