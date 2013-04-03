84cbab9151db0045a8159140830fa751cf410903
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-04-03 11:56:46 +0200

    Forgot to add articleset.py..

---
+++ api/rest/resources/articleset.py
@@ -0,0 +1,38 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+
+from amcat.models import ArticleSet
+from api.rest.resources.amcatresource import AmCATResource
+from api.rest.serializer import AmCATModelSerializer
+
+from rest_framework import serializers
+
+class ArticleSetSerializer(AmCATModelSerializer):
+    index_status = serializers.SerializerMethodField('get_status')
+
+    def get_status(self, aset):
+        return aset.index_state
+
+    class Meta:
+        model = ArticleSet
+
+class ArticleSetResource(AmCATResource):
+    model = ArticleSet
+    serializer_class = ArticleSetSerializer
+
