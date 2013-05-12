aaa72b6af8c49ff20225b57807efa7eb5cf1dc30
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-05-12 22:53:48 +0200

    Clicking favourites now workes

--- navigator/templates/navigator/project/datatable.js
+++ navigator/templates/navigator/project/datatable.js
@@ -3,14 +3,40 @@
  * not. This star can be clicked to change state.
  */
 (function(favourite, type, row){
-    if(type === "display"){
-        var star = favourite ? "icon-star" : "icon-star-empty";
-        var opposite = !favourite ? "icon-star" : "icon-star-empty";
+    // Function to get icon for favourite == { true, false }
+    var get_icon = function(f){ return f ? "icon-star" : "icon-star-empty"; }
 
-        return $("<i>").addClass(star).hover(function(event){
-            $(event.currentTarget).addClass(opposite);
+    if(type === "display"){
+        return $("<i>").addClass(get_icon(row.favourite)).hover(function(event){
+            $(event.currentTarget).removeClass(get_icon(row.favourite)).addClass(get_icon(!row.favourite));
         }, function(event){
-            $(event.currentTarget).addClass(star);
+            $(event.currentTarget).removeClass(get_icon(!row.favourite)).addClass(get_icon(row.favourite));
+        }).click(function(event){
+            // We need to fetch the project url with ?star={0,1}
+            row.favourite = !row.favourite;
+            event.stopPropagation();
+
+            // This script is parsed with Django templates, so we can use url.
+            var url = "{% url 'project' 123 %}".replace("123", new String(row.id)) + "?star=" + (favourite ? "1" : "0");
+            $.get(url).success(function(data, textStatus, jqXHR){
+                row.__notify.pnotify({ hide : true, delay : 0 });
+            }).error(function(data, textStatus, jqXHR){
+                row.__notify.pnotify({
+                    type : "error",
+                    closer : true, 
+                    text : "Something went wrong while (un)setting your favrouites :-(. " + 
+                            "The server returned HTTP " + jqXHR.status + "."
+                });
+            });
+
+            row.__notify = $.pnotify({ 
+                type : "info",
+                text : (row.favourite ? "Setting" : "Removing") + " project " + row.id + " as a favourite..",
+                nonblock: true,
+                hide: false,
+                closer: false,
+                sticker: false
+            });
         });
     }
 
