48bf6d770ac54397b22a6d8752e0a2f3e8bc2bc8
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-05-28 16:44:17 +0300

    add unique constraint

--- amcat/tools/sql/upgrade_1.sql
+++ amcat/tools/sql/upgrade_1.sql
@@ -1,3 +1,4 @@
 alter table codes add column uuid uuid not null default uuid_generate_v1();
+ALTER TABLE codes ADD CONSTRAINT codes_uuid_key UNIQUE (uuid);
 
 UPDATE amcat_system SET db_version = 2;
