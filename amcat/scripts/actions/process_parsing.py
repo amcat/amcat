ce2de49badc428b48fde140f173236addc9701a7
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-06-04 13:30:40 +0300

    add articleset options to process_parsing

--- amcat/scripts/actions/process_parsing.py
+++ amcat/scripts/actions/process_parsing.py
@@ -28,20 +28,27 @@ import logging; log = logging.getLogger(__name__)
 from django import forms
 from django.db import transaction
 
-from amcat.models import AnalysedArticle, Plugin
+from amcat.models import AnalysedArticle, Plugin, ArticleSet
 from amcat.scripts.script import Script
 
 PLUGINTYPE_PARSER=1
 
 class CheckParsing(Script):
     class options_form(forms.Form):
+        articleset = forms.ModelChoiceField(queryset=ArticleSet.objects.all(), required=False)
         analysed_articles = forms.ModelMultipleChoiceField(queryset=AnalysedArticle.objects.all(), required=False)
         check_only = forms.BooleanField(initial=False, required=False)
         plugin = forms.ModelChoiceField(queryset=Plugin.objects.filter(plugin_type__id=1), required=False)
         
-    def _run(self, analysed_articles=None, check_only=False, plugin=None):
+    def _run(self, articleset=None, analysed_articles=None, check_only=False, plugin=None):
         if not analysed_articles:
-            analysed_articles = AnalysedArticle.objects.filter(done=False, error=False).select_related("plugin")
+            if articleset:
+                analysed_articles = (AnalysedArticle.objects.filter(done=False, error=False, article__articlesets_set=articleset.id)
+                                     .select_related("plugin"))
+                
+            else:
+                # select all (!)
+                analysed_articles = AnalysedArticle.objects.filter(done=False, error=False).select_related("plugin")
             if plugin:
                 analysed_articles = analysed_articles.filter(plugin=plugin)
         self.process(analysed_articles)
