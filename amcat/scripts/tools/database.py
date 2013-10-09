a206aef48640cfbac65af04de16f2759aaa8898f
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-10-09 14:13:42 +0200

    Hack to speed up articlelist querying

--- amcat/scripts/tools/database.py
+++ amcat/scripts/tools/database.py
@@ -2,16 +2,26 @@
 A number of functions that help the Amcat3 selection page to retrieve selections from the database
 """
 
-from amcat.models import ArticleSet
+from amcat.models import ArticleSet, Project
 from amcat.models import article
 from django.db.models import Q
 from amcat.tools.djangotoolkit import db_supports_distinct_on
 
-def get_queryset(articlesets, mediums=None, start_date=None, end_date=None, article_ids=None, **kargs):
+def get_queryset(articlesets, mediums=None, start_date=None, end_date=None, article_ids=None, projects=None, **kargs):
     queryset = article.Article.objects
     queryset = queryset.filter(articlesets_set__in=articlesets)
 
-    if mediums:
+    # Disabling filtering on medium if mediums equals all
+    # articles in this project.
+    if projects and len(projects) == 1:
+        p = projects[0]
+        if isinstance(p, int):
+            p = Project.objects.get(id=p)
+
+        if set(p.get_mediums().values_list("id")) == set(mediums.values_list("id")):
+            mediums = None
+
+    if mediums and "WHERE" in unicode(mediums.query):
         queryset = queryset.filter(medium__in=mediums)
     if start_date:
         queryset = queryset.filter(date__gte=start_date)
