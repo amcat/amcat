c0b1d9d5c44bcf19366100d79e13bb4387c58861
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-10-15 14:20:21 +0200

    Added unittests to sync_plugins.py

--- amcat/management/commands/sync_plugins.py
+++ amcat/management/commands/sync_plugins.py
@@ -16,8 +16,8 @@
 # You should have received a copy of the GNU Affero General Public        #
 # License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
 ###########################################################################
-from django.utils.functional import memoize
 from functools import partial
+from django.utils.functional import memoize
 from inspect import isclass
 from itertools import imap, chain
 from django.core.management.base import BaseCommand
@@ -27,9 +27,7 @@ from amcat.contrib import plugins
 import importlib
 import os
 
-import logging;
-from amcat.tools.toolkit import cached
-
+import logging
 log = logging.getLogger(__name__)
 
 
@@ -58,18 +56,18 @@ def is_module(path):
         or os.path.isdir(path)
     )
 
-def _get_plugins(module_path):
+def _get_plugins(module_path, plugin_module):
     module_path = module_path[:-3] if module_path.endswith(".py") else module_path
-    mod = importlib.import_module(".".join((PLUGIN_MODULE, os.path.basename(module_path))))
+    mod = importlib.import_module(".".join((plugin_module, os.path.basename(module_path))))
     attrs = (getattr(mod, a) for a in dir(mod) if not a.startswith('__'))
     return filter(is_plugin, attrs)
 
+def get_plugins(module_paths, plugin_module=PLUGIN_MODULE):
+    """Return all classes which represent a plugin. Search modules `directories`."""
+    return chain.from_iterable(imap(partial(_get_plugins, plugin_module=plugin_module), module_paths))
+
 def get_qualified_name(cls):
     return cls.__module__ + "." + cls.__name__
-
-def get_plugins(module_paths):
-    """Return all classes which represent a plugin. Search modules `directories`."""
-    return chain.from_iterable(imap(_get_plugins, module_paths))
 
 def get_plugin_type(cls):
     """Returns PluginType instance based on given class."""
@@ -121,5 +119,66 @@ class Command(BaseCommand):
 from amcat.tools import amcattest
 
 class TestSyncPlugins(amcattest.PolicyTestCase):
-    pass
+    def test_get_plugin_types(self):
+        all_types = set(p.get_class() for p in PluginType.objects.all())
+
+        # Call two times to test caching
+        self.assertEqual(all_types, set(get_plugin_types()))
+        self.assertEqual(all_types, set(get_plugin_types()))
+
+    def test_is_plugin(self):
+        pclass = get_plugin_types()[0]
+        class Test1(pclass): pass
+        class Test2(object): pass
+
+        self.assertFalse(is_plugin(pclass))
+        self.assertTrue(is_plugin(Test1))
+        self.assertFalse(is_plugin(Test2))
+
+    def test_is_module(self):
+        self.assertFalse(is_module("test/_bla.py"))
+        self.assertFalse(is_module("test/_bla"))
+        self.assertTrue(is_module("/"))
+        self.assertTrue(is_module("/test.py"))
+        self.assertFalse(is_module("/test.pyc"))
+        self.assertFalse(is_module("/testpy"))
+
+    def test_get_plugin_type(self):
+        pt = PluginType.objects.all()[0]
+        ptclass = pt.get_class()
+
+        class Test1(ptclass): pass
+        class Test2(object): pass
+
+        self.assertEquals(get_plugin_type(Test1), pt)
+        self.assertEquals(get_plugin_type(Test2), None)
+
+    def _test_get_plugins(self, dir):
+        import tempfile
+
+        testmod = os.path.join(dir, "test_module")
+        os.mkdir(testmod)
+        open(os.path.join(testmod, "__init__.py"), "w")
+
+        self.assertEqual(set(), set(get_plugins([], plugin_module="plugin_test")))
+        self.assertEqual(set(), set(get_plugins([testmod], plugin_module="plugin_test")))
+
+
+    def test_get_plugins(self):
+        import tempfile
+        import shutil
+        import sys
+
+        try:
+            # Create test-pacakge
+            root = tempfile.mkdtemp()
+            plugin_folder = os.path.join(root, "plugin_test")
+            os.mkdir(plugin_folder)
+            open(os.path.join(plugin_folder, "__init__.py"), "w")
+            sys.path.append(root)
+            self._test_get_plugins(plugin_folder)
+        finally:
+            shutil.rmtree(root)
+            sys.path.remove(root)
+
 
