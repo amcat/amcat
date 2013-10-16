b24fe5584313234a7dfc1e5100e2f5f04c038d86
Toon Alfrink <toon.alfrink@gmail.com>
2013-10-16 22:52:23 +0200

    expanded logging in daily

--- amcat/scripts/maintenance/daily.py
+++ amcat/scripts/maintenance/daily.py
@@ -97,12 +97,11 @@ class DailyScript(Script):
         else:
             return True
 
-
 from amcat.tools.amcatlogging import AmcatFormatter
 import sys
 
 def setup_logging():
-    loggers = (logging.getLogger("amcat.scraping"), logging.getLogger(__name__), logging.getLogger("scrapers"))
+    loggers = (logging.getLogger("amcat"), logging.getLogger(__name__), logging.getLogger("scrapers"), logging.getLogger("daily"))
     d = datetime.date.today()
     filename = "/home/amcat/log/daily_{d.year:04d}-{d.month:02d}-{d.day:02d}.txt".format(**locals())
     sys.stderr = open(filename, 'a')
