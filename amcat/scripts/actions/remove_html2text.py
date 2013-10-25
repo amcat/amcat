df5ac452a8bfda84a78e090ccc022279c6399c15
Toon Alfrink <toon.alfrink@gmail.com>
2013-10-25 18:23:51 +0200

    first version of remove_html2text.py

--- amcat/scripts/actions/remove_html2text.py
+++ amcat/scripts/actions/remove_html2text.py
@@ -18,8 +18,10 @@
 ###########################################################################
 
 """
-Script for removing html2text tags such as [link]this one[/link] in articles
+Script for removing html2text tags such as [this one](url) in articles
 """
+import logging; log = logging.getLogger(__name__)
+
 import re
 from django import forms
 from django.db.models import Max
@@ -28,24 +30,33 @@ from amcat.scripts.script import Script
 from amcat.models.article import Article
 from amcat.models.articleset import ArticleSet
 
+#Note: this is only a first version. Do not expect 100% clean articles, for exactly getting it right seems quite hard.
 class RemoveHTML2TextForm(forms.Form):
-    articleset = forms.ModelChoiceField(ArticleSet.objects.all(), required = False)
+    articleset = forms.ModelChoiceField(ArticleSet.objects.all(), required = True)
     
 class RemoveHTML2TextScript(Script):
+    options_form = RemoveHTML2TextForm
     def run(self, _input):
-        articles = Article.objects.filter(articlesetarticle__articleset = self.options['articleset'].id)
+        articles = Article.objects.filter(articlesets_set = self.options['articleset'].id)
         for article in articles:
+            len_before = len(article.text)
             article.text = self.clean_text(article.text)
-        #article.save()
+            len_after = len(article.text)
+            if len_after < 0.50*len_before:
+                log.warning("article text truncated by >50%, aborting save")
+            else:
+                article.save()
 
     def clean_text(self, text):
-        pass
-        
+        #hyperlinks: doesn't work in various cases. Interestingly, finding urls in text with re is impossible.
+        text = re.sub("\[(.*)\]\(.+\)", lambda x: x.group(1), text)
+        #images: same story
+        text = re.sub("\!\[.*\]\(.+\)", "", text)
+        #titles and bold text
+        text = "\n".join([line.strip("#* ") for line in text.split("\n")])
+        return text
 
 if __name__ == "__main__":
-    #testing
-    article = Article.objects.get(id=9192776)
-    script = RemoveHTML2TextScript()
-    text = script.clean_text(article.text)
-    print(text)
+    from amcat.scripts.tools.cli import run_cli
+    run_cli(RemoveHTML2TextScript)
 
