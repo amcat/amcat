87c1889ab592916bacce3bca378758097d1341bd
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-11-03 13:30:46 +0100

    allow lexis style search queries

--- amcat/tools/queryparser.py
+++ amcat/tools/queryparser.py
@@ -26,17 +26,25 @@ Also paves the way for more customization, i.e. allowing Lexis style queries
 
 from __future__ import unicode_literals, print_function, absolute_import
 from pyparsing import ParseResults
-    
+import itertools
 
-class BaseTerm(object):
-    def __init__(self, text, field):
-        self.text = text
+class ParseError(ValueError):
+    pass
+
+class FieldTerm(object):
+    def __init__(self, field):
         self.field = field
     @property
     def qfield(self):
         return self.field or "_all"
     def __str__(self):
         return unicode(self).encode('utf-8')
+        
+
+class BaseTerm(FieldTerm):
+    def __init__(self, text, field):
+        super(BaseTerm, self).__init__(field=field)
+        self.text = text.replace("!", "*")
 
 class Term(BaseTerm):
     def __unicode__(self):
@@ -49,7 +57,7 @@ class Term(BaseTerm):
 
 class Quote(BaseTerm):
     def __unicode__(self):
-        return u'{self.qfield}::"{self.text}"'.format(**locals())
+        return u'{self.qfield}::QUOTE[{self.text}]'.format(**locals())
     def __str__(self):
         return unicode(self).encode('utf-8')
     def get_dsl(self):
@@ -58,37 +66,12 @@ class Quote(BaseTerm):
         else:
             return {"match_phrase" : {self.qfield : self.text}}
 
-class Span(BaseTerm):
-    def __init__(self, text, field, slop, in_order=False):
-        super(Span, self).__init__(text, field)
-        self.slop = slop
-        self.in_order=in_order
-    def __unicode__(self):
-        return u'{self.qfield}::"{self.text}"~{self.slop}'.format(**locals())
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-    def get_dsl(self):
-        qfield = self.field if self.field else '_all'
-        # we should parse this a second time for internal structure...
-        
-        clauses = [get_span_term(c, self.qfield) for c in self.text.split()]
-        return {"span_near" : {"slop": self.slop, "in_order" : self.in_order,
-                               "clauses" : clauses}}
-
-def get_span_term(text, field):
-    if text.endswith("*"):
-        text = text[:-1]
-        return {"span_multi":{"match":{"prefix" : { field :  { "value" : text } }}}}
-    else:
-        return {"span_term" : {field : text}}
-
 class Boolean(object):
-    def __init__(self, tokens):
-        self.operator = tokens.operator
-        self.terms = [(Boolean(t) if isinstance(t, ParseResults) else t)
-                      for t in tokens if t != self.operator]
-        
+    def __init__(self, operator, terms, implicit=False):
+        self.operator = operator
+        self.terms = terms
+        self.implicit = implicit
+                
     def __unicode__(self):
         terms = " ".join(unicode(t) for t in self.terms)
         return '{self.operator}[{terms}]'.format(**locals())
@@ -105,9 +88,83 @@ class Boolean(object):
             op = dict(OR="should", AND="must", NOT="must_not")[self.operator]
             return {"bool" : {op : [term.get_dsl() for term in self.terms]}}
 
+def _check_span(terms, field=None, allow_boolean=True):
+    """
+    Checks whether a span contains terms using the same field and
+    consists of a single implcit list of terms or simple disjunctions
+    @param field: if given and 'True', all terms must use this field
+    @return: the shared field
+    """
+    f = field
+    for term in terms:
+        if isinstance(term, Term):
+            fld = term.field
+            term.field = None
+        elif allow_boolean and isinstance(term, Boolean) and term.operator == "OR":
+            fld = _check_span(term.terms, allow_boolean = False)
+        else:
+            raise ParseError("Proximity queries cannot contain: {term}".format(**locals()))
+
+        if fld:
+            if not f:
+                f = fld
+            elif f != fld:
+                raise ParseError("Proximity queries should refer to a unique field, found {f!r} and {fld!r}"
+                                 .format(**locals()))                
+    return f
+            
+class Span(Boolean, FieldTerm):
+    def __init__(self, terms, slop, field=None, in_order=False):
+        Boolean.__init__(self, "SPAN", terms)
+        FieldTerm.__init__(self, _check_span(terms, field))
+        self.slop = slop
+        self.in_order = in_order
+        
+    def __unicode__(self):
+        terms = " ".join(unicode(t) for t in self.terms)
+        terms = terms.replace("_all::","")
+        return u'{self.qfield}::PROX/{self.slop}[{terms}]'.format(**locals())
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def get_dsl(self):
+        # we cannot directly use disjunctions in a span query, but we can put the disjunction outside the span
+        # e.g. (a OR b) w/10 c is the same as (a w/10 c) OR (b w/10 c)
+        # So, the get_clause returns a list of clauses, and we take the product of those lists as a disjunction
+
+        def get_clause(term, field):
+            if isinstance(term, Term):
+                if term.text.endswith("*"):
+                    text = term.text[:-1]
+                    return [{"span_multi":{"match":{"prefix" : { field :  { "value" : text } }}}}]
+                else:
+                    return [{"span_term" : {field : term.text}}]
+            else:
+                # term is a disjunction: return a list of clauses
+                # index [0] because get_clause returns a list again, which we don't want here
+                return [get_clause(t, field)[0] for t in term.terms]
+        clauses = (get_clause(t, self.qfield) for t in self.terms)
+        #import json; clauses = list(clauses); print("\n", json.dumps(clauses, indent=4))
+        clauses = itertools.product(*clauses)
+        clauses = [{"span_near" : {"slop": self.slop, "in_order" : self.in_order, "clauses" : list(c)}}
+                   for c in clauses]
+        if len(clauses) == 1:
+            return clauses[0]
+        else:
+            return {"bool" : {"should" : clauses}}
+
+
+def lucene_span(quote, field, slop):
+    '''Create a span query from a lucene style string, i.e. "terms"~10'''
+    clause = parse_to_terms(quote)
+    if not (isinstance(clause, Boolean) and clause.operator == "OR" and clause.implicit):
+        raise ParseError("Lucene-style proximity queries must contain a list of terms, not {clause!r}"
+                         .format(**locals()))
+    return Span(clause.terms, slop, field)
+        
 def get_term(tokens):
     if 'slop' in tokens:
-        return Span(tokens.quote, tokens.field, tokens.slop)
+        return lucene_span(tokens.quote, tokens.field, tokens.slop)
     elif 'quote' in tokens:
         # this is where it gets weird: phrase queries don't support general
         # prefixes, but span (=slop) queries do. So, make a span query
@@ -125,7 +182,16 @@ def get_boolean_or_term(tokens):
     if isinstance(token, (Boolean, BaseTerm)):
         return token
     else:
-        return Boolean(token)
+        op = token['operator']
+        terms = [t for t in token if isinstance(t, (Boolean, BaseTerm))]
+        if op[0] == 'W/':
+            # create span query
+            op, slop = op
+            return Span(terms, slop)
+        else:
+            implicit = op.startswith("implicit_")
+            if implicit: op = op.replace("implicit_", "")
+            return Boolean(op, terms, implicit)
     
 
 def pprint(q, indent=0):
@@ -151,6 +217,7 @@ def get_grammar():
         AND = Literal("AND").setResultsName("operator")
         OR = Literal("OR").setResultsName("operator")
         NOT = Literal("NOT").setResultsName("operator")
+        SPAN = (Literal("W/") + Word(nums)).setResultsName("operator")
         COLON = Literal(":").suppress()
         TILDE = Literal("~").suppress()
         LETTERS = u''.join(unichr(c) for c in xrange(65536) 
@@ -168,16 +235,93 @@ def get_grammar():
 
         # boolean combination
         boolean_expr = operatorPrecedence(fterm, [
-                (AND, 2, opAssoc.LEFT),
-                (NOT, 2, opAssoc.LEFT),
-                (Optional(OR, default="OR"),  2, opAssoc.LEFT),
-                ])
+            (AND, 2, opAssoc.LEFT),
+            (NOT, 2, opAssoc.LEFT),
+            (SPAN, 2, opAssoc.LEFT),
+            (Optional(OR, default="implicit_OR"),  2, opAssoc.LEFT),
+        ])
         boolean_expr.setParseAction(get_boolean_or_term)
         _grammar = boolean_expr
     return _grammar
+
+def parse_to_terms(s):
+    return get_grammar().parseString(s, parseAll=True)[0]
     
 def parse(s):
-    terms =  get_grammar().parseString(s, parseAll=True)[0]
-    #print; pprint(terms); print
-    return terms.get_dsl()
+    return parse_to_terms(s).get_dsl()
 
+
+###########################################################################
+#                          U N I T   T E S T S                            #
+###########################################################################
+
+from amcat.tools import amcattest
+
+class TestQueryParser(amcattest.PolicyTestCase):
+    def test_parse(self):
+        q = lambda s : unicode(parse_to_terms(s))
+
+        self.assertEqual(q('a'), '_all::a')
+        self.assertEqual(q('a b'), 'OR[_all::a _all::b]')
+        self.assertEqual(q('a OR b'), 'OR[_all::a _all::b]')
+        self.assertEqual(q('a AND b'), 'AND[_all::a _all::b]')
+        self.assertEqual(q('a NOT b'), 'NOT[_all::a _all::b]')
+        self.assertEqual(q('a AND (b c)'), 'AND[_all::a OR[_all::b _all::c]]')
+        self.assertEqual(q('(a AND b) c'), 'OR[AND[_all::a _all::b] _all::c]')
+
+        # quotes
+        self.assertEqual(q('"a b"'), '_all::QUOTE[a b]')
+        self.assertEqual(q('"a b" c'), 'OR[_all::QUOTE[a b] _all::c]')
+        
+        # proximity queries
+        self.assertEqual(q('a W/10 b'), '_all::PROX/10[a b]')
+        self.assertEqual(q('(a b) W/10 c'), '_all::PROX/10[OR[a b] c]')
+        self.assertEqual(q('(x:a b) W/10 c'), 'x::PROX/10[OR[a b] c]')
+        
+        # proximity queries must have unique field and can only contain wildcards and disjunctions
+        self.assertRaises(ParseError, q, 'a W/10 (b OR (c OR d))')
+        self.assertRaises(ParseError, q, 'x:a W/10 y:b')
+        self.assertRaises(ParseError, q, 'a W/10 (b AND c)')
+        self.assertRaises(ParseError, q, 'a W/10 (b W/5 c)')
+
+        # lucene notation
+        self.assertEqual(q('"a b"~5'), '_all::PROX/5[a b]')
+        self.assertEqual(q('x:"a b"~5'), 'x::PROX/5[a b]')
+        self.assertEqual(q('"a (b c)"~5'), '_all::PROX/5[a OR[b c]]')
+
+        # disallow AND in lucene notation
+        self.assertRaises(ParseError, q, '"a (b AND c)"~5')
+
+    def test_dsl(self):
+        q = parse
+
+        
+        self.assertEqual(q('a'), {'term' : {'_all' : 'a'}})
+        self.assertEqual(q('a*'), {'wildcard' : {'_all' : 'a*'}})
+        self.assertEqual(q('a!'), {'wildcard' : {'_all' : 'a*'}})
+
+        self.assertEqual(q('a AND b'), {'bool' : {'must' : [{'term' : {'_all' : 'a'}},
+                                                            {'term' : {'_all' : 'b'}},
+                                                            ]}})
+                                                            
+        self.assertEqual(q('a W/10 b'), {"span_near" : {"clauses" : [
+            {"span_term" : {"_all" : "a"}},
+            {"span_term" : {"_all" : "b"}},
+            ], "slop" : "10", "in_order": False}})
+        self.assertEqual(q('a* W/10 b'), {"span_near" : {"clauses" : [
+            {"span_multi" : {"match" : {"prefix" : {"_all" : {"value" : "a"}}}}},
+            {"span_term" : {"_all" : "b"}},
+            ], "slop" : "10", "in_order": False}})
+        
+        expected = {u'bool': {u'should': [
+            {u'span_near': {u'in_order': False, u'clauses': [
+                {u'span_term': {u'_all': u'a'}}, 
+                {u'span_term': {u'_all': u'b'}}
+            ], u'slop': u'10'}}, 
+            {u'span_near': {u'in_order': False, u'clauses': [
+                {u'span_term': {u'_all': u'a'}}, 
+                {u'span_term': {u'_all': u'c'}}
+            ], u'slop': u'10'}}
+        ]}}
+
+        self.assertEqual(q('a W/10 (b c)'), expected)
