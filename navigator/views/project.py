64c3f859e316894cbf8dbb7eb0fe76513bd2cfab
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-11-28 16:51:15 +0100

    can_create / can_update remove from AmCATModel, changing project.py to reflect these changes.

--- navigator/views/project.py
+++ navigator/views/project.py
@@ -781,8 +781,7 @@ def codebooks(request, project):
     linked_codebooks = (Datatable(CodebookResource, rowlink='./codebook/{id}')
                         .filter(projects_set=project))
 
-    can_import = project.can_update(request.user)
-    can_create = Codebook.can_create(request.user) and project.can_update(request.user)
+    can_import = can_create = project.get_role_id(request.user) >= Role.objects.get(projectlevel=True, label="admin")
 
     deleted = session_pop(request.session, "deleted_codebook")
     
