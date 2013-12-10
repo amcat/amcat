a86a2083020b8f80020f24a4bc99307c5425f5b5
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-12-11 00:47:57 +0100

    improved query, buttons, landing page

--- navigator/views/project_views.py
+++ navigator/views/project_views.py
@@ -38,6 +38,23 @@ class ProjectListView(BreadCrumbMixin, DatatableMixin, ListView):
 
     def get_breadcrumbs(self):
         return [("Projects", "#")]
+
+    def filter_table(self, table):
+        what = self.kwargs.get('what', 'favourites')
+        if what == 'favourites':
+            # ugly solution - get project ids that are favourite and use that to filter, otherwise would have to add many to many to api?
+            # (or use api request.user to add only current user's favourite status). But good enough for now...
+            ids = self.request.user.get_profile().favourite_projects.all().values_list("id")
+            ids = [id for (id, ) in ids]
+            if ids: 
+                return table.filter(pk=ids, active=True)
+            else:
+                return table.filter(name="This is a really stupid way to force an empty table (so sue me!)")
+        elif what == "own":
+            return table.filter(projectrole__user=self.request.user, active=True)
+        elif what == "all":
+            return table
+
         
     def get_datatable(self):
         """Create the Datatable object"""
@@ -46,7 +63,6 @@ class ProjectListView(BreadCrumbMixin, DatatableMixin, ListView):
         table = FavouriteDatatable(resource=self.get_resource(), label="project",
                                    set_url=url + "?star=1", unset_url=url+"?star=0")
         table = table.rowlink_reverse('project', args=['{id}'])
-        #table = table.rowlink_reverse('article set-details', args=[self.project.id, '{id}'])
-        #table = table.hide("project")
+
         table = self.filter_table(table)
         return table
