6c58c3a0be75dc2729339d91579762ab2c23a88d
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-12-24 12:50:21 +0100

    Annotator gets its table from API, removed dead code.

--- annotator/views/overview.py
+++ annotator/views/overview.py
@@ -18,21 +18,9 @@
 ###########################################################################
 
 from django.shortcuts import render
-from amcat.models.coding import codingtoolkit
-from django.http import HttpResponse
-from amcat.scripts.output.datatables import TableToDatatable
-import datetime
+
 
 def index(request):
     """show the main HTML"""
     return render(request, "annotator/overview.html")
     
-    
-def table(request):
-    """returns a DataTable JSON table with a list of recent codingjobs"""
-    startDate = datetime.datetime.now() - datetime.timedelta(days=365) # only show jobs from the past year
-    table = codingtoolkit.get_table_jobs_per_user(request.user, insertdate__gt=startDate) 
-    out = TableToDatatable().run(table)
-    response = HttpResponse(mimetype='text/plain')
-    response.write(out)
-    return response
