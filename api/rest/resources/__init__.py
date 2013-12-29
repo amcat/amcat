c9b911005bb0c4bac5a25ff1ea41afc231e45fdd
Wouter van Atteveldt <wouter@vanatteveldt.com>
2013-12-29 21:07:41 +0100

    add search option to label, allow aggregate without axes to get total count

--- api/rest/resources/__init__.py
+++ api/rest/resources/__init__.py
@@ -29,7 +29,7 @@ from amcat.tools import classtools
 from api.rest.resources.amcatresource import AmCATResource
 from api.rest.resources.project import ProjectResource
 from api.rest.resources.user import UserResource
-from api.rest.resources.codebook import CodebookHierarchyResource, CodebookResource
+from api.rest.resources.codebook import CodebookHierarchyResource, CodebookResource, LabelResource
 from api.rest.resources.article import ArticleMetaResource
 from api.rest.resources.articleset import ArticleSetResource
 from api.rest.resources.codingjob import CodingJobResource
@@ -49,7 +49,7 @@ MODELS = ['Article', 'AmCAT',
           'Role', 'ProjectRole',
           'Medium', 'Language',
           'CodingSchema', 'CodingSchemaField',
-          'CodebookCode', 'Label', 'CodingSchemaFieldType',
+          'CodebookCode', 'CodingSchemaFieldType',
           'django.contrib.auth.models.Group', 'django.contrib.auth.models.Permission',
           'Plugin', 'Scraper', 'PluginType',
           "RuleSet",
