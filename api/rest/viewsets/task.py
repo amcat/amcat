05155232d1c7c8b4f303b0d50e7d8d627355df9d
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2013-12-29 23:23:00 +0100

    Fixed "PENDING" bug

--- api/rest/viewsets/task.py
+++ api/rest/viewsets/task.py
@@ -18,6 +18,7 @@
 ###########################################################################
 from rest_framework import serializers
 from amcat.models import Task
+from amcat.tools import amcattest
 from api.rest.serializer import AmCATModelSerializer
 
 __all__ = ("TaskSerializer", "TaskResultSerializer")
@@ -28,11 +29,28 @@ class TaskSerializer(AmCATModelSerializer):
     status = serializers.SerializerMethodField('get_status')
     ready = serializers.SerializerMethodField('get_ready')
 
+    def __init__(self, *args, **kwargs):
+        super(TaskSerializer, self).__init__(*args, **kwargs)
+        self._tasks = {}
+
+    def set_status_ready(self, task):
+        ready = task.get_async_result().ready()
+        status = task.get_async_result().status
+        self._tasks[task] = (status, ready)
+
+    def get_status_ready(self, task):
+        """Returns tuple with (status, ready) => (str, bool)"""
+        if task not in self._tasks:
+            self.set_status_ready(task)
+        return self._tasks[task]
+
     def get_status(self, task):
-        return task.get_async_result().status
+        status, _ = self.get_status_ready(task)
+        return status
 
     def get_ready(self, task):
-        return task.get_async_result().ready()
+        _, ready = self.get_status_ready(task)
+        return ready
 
     class Meta:
         model = Task
@@ -53,3 +71,38 @@ class TaskResultSerializer(AmCATModelSerializer):
     class Meta:
         model = Task
         fields = ("uuid", "ready", "result")
+
+class TestTaskSerializer(amcattest.AmCATTestCase):
+    def test_order(self):
+        class MockTask:
+            def __init__(self, ready=False, status="PENDING"):
+                self._ready = ready
+                self._status = status
+
+            def ready(self):
+                return self._ready
+
+            @property
+            def status(self):
+                return self._status
+
+            def get_async_result(self):
+                return self
+
+        ts = TaskSerializer()
+        mt = MockTask()
+        mt2 = MockTask(ready=True, status="SUCCESS")
+        mt3 = MockTask()
+
+        # Test simple getting / caching
+        self.assertEqual("PENDING", ts.get_status(mt))
+        self.assertEqual(False, ts.get_ready(mt))
+        self.assertEqual("SUCCESS", ts.get_status(mt2))
+        self.assertEqual(True, ts.get_ready(mt2))
+
+        # Test order of ready/status
+        self.assertEqual("PENDING", ts.get_status(mt3))
+        mt3._ready = True
+        self.assertEqual(False, ts.get_ready(mt))
+
+
