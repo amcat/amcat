f91b6e42e9a30ccf30b53ab86468f542880692bb
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-01-23 01:36:22 +0100

    split no longer gives errors, but I'm not quite sure it works

--- navigator/views/article_views.py
+++ navigator/views/article_views.py
@@ -18,14 +18,25 @@
 ###########################################################################
 
 from .articleset_views import ArticleSetDetailsView
-from amcat.models import Article
-from navigator.views.projectview import ProjectViewMixin, HierarchicalViewMixin, BreadCrumbMixin
+from amcat.models import Article, ArticleSet, Sentence
+from navigator.views.projectview import ProjectViewMixin, HierarchicalViewMixin, BreadCrumbMixin, ProjectFormView
 from django.views.generic.detail import DetailView
+from django import forms
+from amcat.forms import widgets
+from amcat.nlp import sbd
+from itertools import chain
+from django.shortcuts import render
 
 class ArticleSetArticleDetailsView(HierarchicalViewMixin, ProjectViewMixin, BreadCrumbMixin, DetailView):
     parent = ArticleSetDetailsView
     model = Article
     context_category = 'Articles'
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleSetArticleDetailsView, self).get_context_data(**kwargs)
+        context['articleset_id'] = self.kwargs['articleset_id']
+        return context
+    
 
 class ProjectArticleDetailsView(HierarchicalViewMixin,ProjectViewMixin, BreadCrumbMixin, DetailView):
     model = Article
@@ -42,3 +53,193 @@ class ProjectArticleDetailsView(HierarchicalViewMixin,ProjectViewMixin, BreadCru
     @classmethod
     def get_view_name(cls):
         return "project-article-details"
+
+
+################################################################################
+# Splitting. Yes, it was that much work                                        #
+################################################################################
+        
+def get_articles(article, sentences):
+    """
+    Split `article` with `sentences` as delimeters. For each sentence the text
+    before it, including itself, is copied to a new article with is yield.
+
+    @param sentences: delimeters for splitting
+    @type sentences: QuerySet
+
+    @param article: article which contains sentences
+    @type article: models.Article
+
+    @requires: ordering of sentences ("parnr", "sentnr")
+    @requires: sbd.get_or_create_sentences() called on `article`
+    @requires: all(a in article.sentences.all() for a in sentences)
+
+    @returns: generator with newly splitted articles (not saved)
+    @raises: ValueError if a sentence in `sentences` is not in article.sentences
+    """
+    new_article = copy_article(article)
+
+    # Get sentence, skipping the headline 
+    all_sentences = list(article.sentences.all()[1:])
+
+    not_in_article = set(sentences) - set(all_sentences)
+    if not_in_article:
+        raise ValueError(
+            "Sentences specified as delimters, but not in article: {not_in_article}. Did you try to split on a headline?"
+            .format(**locals())
+        )
+
+    prev_parnr = 1 
+    for parnr, sentnr in chain(sentences.values_list("parnr", "sentnr"), ((None, None),)):
+        # Skip headline paragraph
+        if parnr == 1: continue
+
+        while True:
+            try: sent = all_sentences.pop(0)
+            except IndexError:
+                new_article.text = new_article.text.strip()
+                yield new_article
+                break
+
+            if sent.parnr != prev_parnr:
+                new_article.text += "\n\n"
+
+            new_article.text += sent.sentence
+            new_article.text += ". "
+            prev_parnr = sent.parnr
+
+            if (sent.sentnr == sentnr and sent.parnr == parnr):
+                new_article.text = new_article.text.strip()
+                yield new_article
+                new_article = copy_article(article)
+                break
+
+
+def handle_split(form, project, article, sentences):
+    articles = list(get_articles(article, sentences))
+
+    # We won't use bulk_create yet, as it bypasses save() and doesn't
+    # insert ids
+    for art in articles:
+        art.save()
+        sbd.create_sentences(art)
+
+    # Context variables for template
+    form_data = form.cleaned_data 
+    all_sets = list(project.all_articlesets().filter(articles=article))
+
+    # Keep a list of touched sets, so we can invalidate their indices
+    dirty_sets = ArticleSet.objects.none()
+
+    # Add splitted articles to existing sets
+    ArticleSet.articles.through.objects.bulk_create([
+        ArticleSet.articles.through(articleset=aset, article=art) for
+            art in articles for aset in form_data["add_splitted_to_sets"]
+    ])
+
+    # Collect changed sets
+    for field in ("add_splitted_to_sets", "remove_from_sets", "add_to_sets"):
+        dirty_sets |= form_data[field]
+
+    # Add splitted articles to sets wherin the original article live{d,s}
+    if form_data["add_splitted_to_all"]:
+        articlesetarts = ArticleSet.articles.through.objects.filter(article=article, articleset__project=project)
+
+        ArticleSet.articles.through.objects.bulk_create([
+            ArticleSet.articles.through(articleset=asetart.articleset, article=art)
+                for art in articles for asetart in articlesetarts
+        ])
+
+        dirty_sets |= project.all_articlesets().filter(articles=article).only("id")
+
+    if form_data["remove_from_sets"]:
+        for aset in form_data["remove_from_sets"]:
+            aset.remove_articles([article])
+        
+    if form_data["remove_from_all_sets"]:
+        for aset in ArticleSet.objects.filter(project=project, articles=article).distinct():
+            aset.remove_articles([article])
+
+    if form_data["add_splitted_to_new_set"]:
+        new_splitted_set = ArticleSet.create_set(project, form_data["add_splitted_to_new_set"], articles)
+
+    if form_data["add_to_sets"]:
+        for articleset in form_data["add_to_sets"]:
+            articleset.add_articles([article])
+
+    if form_data["add_to_new_set"]:
+        new_set = ArticleSet.create_set(project, form_data["add_to_new_set"], [article])
+
+    return locals()
+
+def copy_article(article):
+    new = Article.objects.get(id=article.id)
+    new.id = None
+    new.uuid = None
+    new.text = ""
+    new.length = None
+    new.byline = None
+    return new
+
+def _get_sentences(sentences, prev_parnr=1):
+    """
+    Yields (sentence, bool) where bool indicates whether this sentences starts
+    a new paragraph.
+    """
+    for sentence in sentences:
+        yield (sentence, prev_parnr != sentence.parnr)
+        prev_parnr = sentence.parnr
+
+def parse_sentence_name(name):
+    if not name.startswith("sentence-"): return
+
+    try:
+        return int(name.split("-")[1])
+    except IndexError, ValueError:
+        pass
+
+def get_sentence_ids(post):
+    for name, checked in post.items():
+        if checked != "on": continue
+        yield parse_sentence_name(name)
+
+class ArticleSplitView(ProjectFormView):
+    parent = ArticleSetArticleDetailsView
+    url_fragment = "split"
+    template_name = "project/article_split.html"
+    
+    class form_class(forms.Form):
+        add_to_new_set = forms.CharField(required=False) 
+        add_to_sets = forms.ModelMultipleChoiceField(queryset=ArticleSet.objects.none(), widget=widgets.JQueryMultipleSelect, required=False)
+        
+        remove_from_sets = forms.ModelMultipleChoiceField(queryset=ArticleSet.objects.none(), widget=widgets.JQueryMultipleSelect, required=False)
+        remove_from_all_sets = forms.BooleanField(initial=True, required=False, help_text="Remove all instances of the original article in this project")
+    
+        add_splitted_to_sets = forms.ModelMultipleChoiceField(queryset=ArticleSet.objects.none(), widget=widgets.JQueryMultipleSelect, required=False)
+        add_splitted_to_new_set = forms.CharField(required=False)
+        add_splitted_to_all = forms.BooleanField(initial=False, required=False, help_text="Add new (splitted) articles to all sets containing the original article")
+
+    def get_form(self, form_class):
+        form = super(ArticleSplitView, self).get_form(form_class)
+        form.fields["add_splitted_to_sets"].queryset = self.project.all_articlesets()
+        form.fields["remove_from_sets"].queryset = self.project.all_articlesets().filter(articles=self.article)
+        form.fields["add_to_sets"].queryset = self.project.all_articlesets()
+        return form
+
+    @property
+    def article(self):
+        return Article.objects.get(pk=self.kwargs['article_id'])
+        
+    def form_valid(self, form):
+        selected_sentence_ids = set(get_sentence_ids(self.request.POST)) - {None,}
+        if selected_sentence_ids:
+            sentences = Sentence.objects.filter(id__in=selected_sentence_ids)
+            context = handle_split(form, self.project, self.article, sentences)
+            return render(self.request, "project/article_split_done.html", context)
+
+    def get_context_data(self, **kwargs):
+        ctx = super(ArticleSplitView, self).get_context_data(**kwargs)
+        sentences = sbd.get_or_create_sentences(self.article).only("sentence", "parnr")
+        ctx["sentences"] = _get_sentences(sentences)
+        ctx["sentences"].next() # skip headline
+        return ctx
