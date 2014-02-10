b4c60e95f95239d8399de1b7b7aedad85f74eae5
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-02-10 12:10:37 +0100

    All inputs now have a class 'input-sm'

--- annotator/static/js/annotator/annotator.widgets.js
+++ annotator/static/js/annotator/annotator.widgets.js
@@ -52,7 +52,7 @@ widgets = (function(self){
          * procedure of notifying Annotator via set_coding_value shoudl be followed.
          */
         get_html : function(schemafield, _intval, _type){
-            var widget = $("<input>").addClass("codingvalue form-control");
+            var widget = $("<input>").addClass("codingvalue form-control input-sm");
             widget = (_intval) ? widget.attr("intval", self.EMPTY_INTVAL) : widget;
             widget = widget.change(self[_type||"default"].change);
 
@@ -110,7 +110,7 @@ widgets = (function(self){
                 }
             });
 
-            var widget = $("<input>").addClass("sentence form-control").change(self.sentence.change);
+            var widget = $("<input>").addClass("sentence form-control input-sm").change(self.sentence.change);
             autocomplete.set(widget, choices);
             return widget;
         },
@@ -219,8 +219,8 @@ widgets = (function(self){
                 var descendant = $("<input>");
                 var hidden = $("<input>").attr("intval", self.EMPTY_INTVAL).hide();
 
-                root.addClass("codebook_root form-control").attr("placeholder", "Root..").attr("intval", self.EMPTY_INTVAL);
-                descendant.addClass("codebook_descendant form-control").attr("placeholder", "Descendant..");
+                root.addClass("codebook_root form-control input-sm").attr("placeholder", "Root..").attr("intval", self.EMPTY_INTVAL);
+                descendant.addClass("codebook_descendant form-control input-sm").attr("placeholder", "Descendant..");
                 descendant.attr("intval", self.EMPTY_INTVAL);
                 hidden.addClass("codebook_value codingvalue");
 
@@ -235,7 +235,7 @@ widgets = (function(self){
                 hidden.change(self.default.change.bind(hidden));
                 html.append(root).append(descendant).append(hidden);
             } else {
-                var input = $("<input>").attr("intval", self.EMPTY_INTVAL).addClass("codingvalue form-control");
+                var input = $("<input>").attr("intval", self.EMPTY_INTVAL).addClass("codingvalue form-control input-sm");
                 autocomplete.set(input, schemafield.choices);
                 html.append(input);
                 input.change(self.default.change.bind(input));
