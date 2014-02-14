a51a711e9b6f2ba2b871a4e705aa87a664f3bee4
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-02-14 15:20:50 +0100

    make AMCAT pip-installable
    * README updated to reflect this
    * manage.py is now a package with an __main__ package, run with python -m
      (see README)
    * setup.py is not installed along, so version info encoded in amcat._version

--- amcat/__init__.py
+++ amcat/__init__.py
@@ -14,4 +14,4 @@ Main organisation:
 # from http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
 from __future__ import absolute_import
 from amcat.amcatcelery import app
-
+from amcat._version import __version__
--- amcat/manage/__main__.py
+++ amcat/manage/__main__.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
-from django.core import management
+from django.core.management import execute_from_command_line
 
-
-import os
-import os.path
+import sys
 
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
@@ -16,11 +13,8 @@ except ImportError:
 # to prevent the import from loading the settings using the "old" DJANGO_SETTINGS_MODULE
 from amcat.tools import initialize
 
-if __name__ == "__main__":
-    initialize.set_signals()
+initialize.set_signals()
 
-    import sys
-    from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
-    #execute_manager(settings)
+execute_from_command_line(sys.argv)
+#execute_manager(settings)
