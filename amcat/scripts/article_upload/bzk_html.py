0329047428a7a51ac73408f988bbbe9c12c323f0
Toon Alfrink <toon.alfrink@gmail.com>
2014-03-03 17:53:19 +0100

    update bzk html uploader to deal with another type of input

--- amcat/scripts/article_upload/bzk_html.py
+++ amcat/scripts/article_upload/bzk_html.py
@@ -34,7 +34,6 @@ from amcat.scripts.article_upload.bzk_aliases import BZK_ALIASES as MEDIUM_ALIAS
 
 class BZK(UploadScript):
     def _scrape_unit(self, _file):
-        print("unit")
         if type(_file) == unicode: #command line
             etree = html.fromstring(_file)
         else: #web interface
@@ -48,14 +47,17 @@ class BZK(UploadScript):
         if title:
             title_text = title[0].text.lower().strip()
             if "intranet/rss" in title_text or "werkmap" in title_text:
-                for article in self.scrape_old(etree, title_text):
+                for article in self.scrape_1(etree, title_text):
                     yield article
+        elif etree.cssselect("h1"):
+            for article in self.scrape_2(etree):
+                yield article
         else:
-            for article in self.scrape_new(etree):
+            for article in self.scrape_3(etree):
                 yield article
 
-    def scrape_old(self, _html, t):
-        """Old format"""
+    def scrape_1(self, _html, t):
+        """format of mostly 2013"""
         if "werkmap" in t:
             divs = _html.cssselect("#articleTable div")
         elif "intranet/rss" in t:
@@ -79,7 +81,7 @@ class BZK(UploadScript):
             else:
                 yield article
 
-    def scrape_new(self, _html):
+    def scrape_2(self, _html):
         """New format as of 2014 and a few days before"""
 
         docdate = readDate(_html.cssselect("h1")[0].text.split("-")[1])
@@ -105,6 +107,42 @@ class BZK(UploadScript):
                 article.date = docdate
             yield article
 
+    def scrape_3(self, _html):
+        """Some ugly MS Word format, as of 2014-03-03"""
+        # Partition articles
+        part = []
+        articles = []
+        for tag in _html.cssselect("body > div > *"):
+            if tag.cssselect("hr"):
+                articles.append(part)
+                part = []
+            else:
+                part.append(tag)
+        for tags in articles[1:]:
+            article = Article()
+            dateline = tags[1].text_content().strip()
+            article = self.parse_dateline(dateline, article)
+            article.headline = tags[1].text_content().strip()
+            html_str = "".join([html.tostring(t) for t in tags[2:]])
+            article.text = html2text(html_str)
+            article.metastring = {'html': html_str}
+            
+            yield article
+
+
+    def parse_dateline(self, text, article):
+        bits = text.split()
+        if "-" in bits[-1]:
+            article.date = readDate(bits[-1])
+            article.medium = self.get_medium(" ".join(bits[:-1]))
+        elif bits[-1].isdigit():
+            article.date = readDate(" ".join(bits[-3:]))
+            article.medium = self.get_medium(" ".join(bits[:-3]))
+        else:
+            article.medium = self.get_medium(" ".join(bits))
+            article.date = None
+        return article
+
     def parse_item(self, item):
         #item: a list of html tags
         article = Article(metastring = {})
@@ -117,16 +155,7 @@ class BZK(UploadScript):
             elif tag.tag == "h2":
                 article.headline = tag.text
             elif tag.tag == "i":
-                bits = tag.text.split()
-                if "-" in bits[-1]:
-                    article.date = readDate(bits[-1])
-                    article.medium = self.get_medium(" ".join(bits[:-1]))
-                elif bits[-1].isdigit():
-                    article.date = readDate(" ".join(bits[-3:]))
-                    article.medium = self.get_medium(" ".join(bits[:-3]))
-                else:
-                    article.medium = self.get_medium(" ".join(bits))
-                    article.date = None
+                article = self.parse_dateline(tag.text, article)
         #process html
         article.text = "\n".join([html2text(html.tostring(bit)) for bit in article.text])
         return article
@@ -146,7 +175,6 @@ class BZK(UploadScript):
         if section:
             section = section.strip()
         return int(pagenum), section
-
 
 if __name__ == "__main__":
     from amcat.scripts.tools import cli
