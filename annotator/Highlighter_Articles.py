e05ecabc47669f0bdcb8955c0e051c21c5bd6864
ckling <github@c-kling.de>
2014-03-17 11:45:25 +0100

    ups

--- annotator/Highlighter_Articles.py
+++ annotator/Highlighter_Articles.py
@@ -10,76 +10,74 @@ class HighlighterArticles:
 #variable_keywords: one-dimenstional array of comma-separated keywords
 #variable_pages: array of the page numbers assigned to the variables (index identical to variable_keywords).
 #output: three-dimensional array: paragraph-sentence-variable, meaning that for every sentence in a paragraph we have scores \in [0,1] giving the highlighting for each variable
-  def getHighlightingsArticles(self, article, variable_keywords, variable_pages):
-	stemmer = snowballstemmer.stemmer("german")
-	#goodchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÄÖÜäöüß'"
-	for i in range(0, len(article)):
-		for j in range(0, len(article[i])):
-  			article[i][j] = article[i][j].split(" ");
-			for k in range(0, len(article[i][j])):
-				#article[i][j][k]=chrtran(article[i][j][k], goodchars, "")
-				article[i][j][k]=stemmer.stemWord(article[i][j][k])
+	def getHighlightingsArticles(self, article, variable_keywords, variable_pages):
+		stemmer = snowballstemmer.stemmer("german")
+		#goodchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÄÖÜäöüß'"
+		for i in range(0, len(article)):
+			for j in range(0, len(article[i])):
+	  			article[i][j] = article[i][j].split(" ");
+				for k in range(0, len(article[i][j])):
+					#article[i][j][k]=chrtran(article[i][j][k], goodchars, "")
+					article[i][j][k]=stemmer.stemWord(article[i][j][k])
+	
 
+		for i in range(0, len(variable_keywords)):
+			#variable_keywords[i]=chrtran(variable_keywords[i], goodchars, "")
+			variable_keywords[i]=stemmer.stemWord(variable_keywords[i])
+	
+		highlight = []
 
-	for i in range(0, len(variable_keywords)):
-		#variable_keywords[i]=chrtran(variable_keywords[i], goodchars, "")
-		variable_keywords[i]=stemmer.stemWord(variable_keywords[i])
+		for i in range(0, len(article)):
+			highlight_article = []
 
-	highlight = []
-
-	for i in range(0, len(article)):
-		highlight_article = []
-
-		for j in range(0, len(article[i])):
-			highlight_variables = []
-			for k in range(0, len(variable_keywords)):
-  				highlight_variables.append(random.random())
-			highlight_article.append(highlight_variables)
-
-		highlight.append(highlight_article)
+			for j in range(0, len(article[i])):
+				highlight_variables = []
+				for k in range(0, len(variable_keywords)):
+  					highlight_variables.append(random.random())
+				highlight_article.append(highlight_variables)
+	
+			highlight.append(highlight_article)
 			
 
 
- 	return highlight
-
-pass
+ 		return highlight
 
 #input: 
 #article: two-dimensional array of the article: the first dimension corresponds to the paragraph, second corresponds to sentences. 
 #variable_keywords: one-dimenstional array of comma-separated keywords
 #variable_pages: array of the page numbers assigned to the variables (index identical to variable_keywords).
 #output: three-dimensional array: paragraph-sentence-variables, meaning that for every sentence in a paragraph we have a score for every variable \in [0,1]
-  def getHighlightingsVariables(self, article, variable_keywords, variable_pages):
-	stemmer = snowballstemmer.stemmer("german")
-	#goodchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÄÖÜäöüß'"
-	for i in range(0, len(article)):
-		for j in range(0, len(article[i])):
-  			article[i][j] = article[i][j].split(" ");
-			for k in range(0, len(article[i][j])):
-				#article[i][j][k]=chrtran(article[i][j][k], goodchars, "")
-				article[i][j][k]=stemmer.stemWord(article[i][j][k])
+	def getHighlightingsVariables(self, article, variable_keywords, variable_pages):
+		stemmer = snowballstemmer.stemmer("german")
+		#goodchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÄÖÜäöüß'"
+		for i in range(0, len(article)):
+			for j in range(0, len(article[i])):
+  				article[i][j] = article[i][j].split(" ");
+				for k in range(0, len(article[i][j])):
+					#article[i][j][k]=chrtran(article[i][j][k], goodchars, "")
+					article[i][j][k]=stemmer.stemWord(article[i][j][k])
 
 
-	for i in range(0, len(variable_keywords)):
-		#variable_keywords[i]=chrtran(variable_keywords[i], goodchars, "")
-		variable_keywords[i]=stemmer.stemWord(variable_keywords[i])
+		for i in range(0, len(variable_keywords)):
+			#variable_keywords[i]=chrtran(variable_keywords[i], goodchars, "")
+			variable_keywords[i]=stemmer.stemWord(variable_keywords[i])
 
-	highlight = []
+		highlight = []
 
-	for i in range(0, len(article)):
-		highlight_article = []
+		for i in range(0, len(article)):
+			highlight_article = []
+	
+			for j in range(0, len(article[i])):
+				highlight_variables = []
+				for k in range(0, len(variable_keywords)):
+	  				highlight_variables.append(random.random())
+				highlight_article.append(highlight_variables)
 
-		for j in range(0, len(article[i])):
-			highlight_variables = []
-			for k in range(0, len(variable_keywords)):
-  				highlight_variables.append(random.random())
-			highlight_article.append(highlight_variables)
-
-		highlight.append(highlight_article)
+			highlight.append(highlight_article)
 			
 
 
- 	return highlight
+	 	return highlight
 
 pass
 
