1beaaf253610c395ed02c96325df77b29f064d13
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-05-31 17:48:07 +0200

    Selecting codingjobs screen now hidden

--- navigator/static/js/jquery.exportids.js
+++ navigator/static/js/jquery.exportids.js
@@ -4,15 +4,21 @@
  * this fetches all selected rows in `table`, and extracts their firsts columns
  * text. These contents are joined by '&id=' and appended to `button`.href.
  */
-function exportids(table, button){
-    button.click(function(){
-        var ids = [];
+function exportids(form){
+    form.submit(function(){
+        var table = $($(this).data("table"));
+        var field = $($(this).data("id-field"));
 
-        table.find(".selected").each(function(i, row){
-            ids.push($("td", row).first().text());
+        $.each($.map(table.find(".selected"), function(row){
+            // Select first column of row, and fetch its text (probably an ID)
+            return $("td", row).first().text();
+        }), function(i, id){
+            // For each id, create a input element
+            form.get(0).appendChild(field.clone().val(id).get(0));
         });
 
-        var url = "?id=" + ids.join("&id=");
-        button.attr("href", button.attr("href") + url);
-    })
+        field.remove();
+        form.find("[type=submit]").click();
+    });
+
 }
