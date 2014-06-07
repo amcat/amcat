2aa7ab54ff7a4c45f575d5d59c515b2dd75d37ac
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-06-07 17:17:29 +0200

    add url to article meta

--- api/rest/resources/article.py
+++ api/rest/resources/article.py
@@ -37,22 +37,22 @@ class ArticleMetaFilter(AmCATFilterSet):
     date_from = filters.DateFilter(name='date', lookup_type='gte')
     date_to = filters.DateFilter(name='date', lookup_type='lt')
     articleset = InFilter(name='articlesets_set', queryset=ArticleSet.objects.all())
-    
+
     class Meta:
         model = Article
         order_by=True
-        
+
 class ArticleMetaSerializer(AmCATModelSerializer):
     class Meta:
         model = Article
         fields = ("id", "date", "project", "medium", "headline",
-                    "section", "pagenr", "author", "length")
+                    "section", "pagenr", "author", "length", "url")
 
 class ArticleMetaResource(AmCATResource):
     model = Article
     serializer_class = ArticleMetaSerializer
     filter_class = ArticleMetaFilter
-    
+
     @classmethod
     def get_model_name(cls):
         return "ArticleMeta".lower()
@@ -72,23 +72,23 @@ from api.rest.viewsets.articleset import ArticleSetViewSet
 
 class TestArticle(ApiTestCase):
     authentication_classes = (SessionAuthentication, BasicAuthentication)
-    
-    
+
+
     @amcattest.use_elastic
     def test_create(self):
         s = amcattest.create_test_set()
-                            
+
         # is the set empty? (aka can we get the results)
         url = ArticleViewSet.get_url(project=s.project.id, articleset=s.id)
         result = self.get(url)
         self.assertEqual(result['results'], [])
 
         body = {'text' : 'bla', 'headline' : 'headline', 'date' : '2013-01-01T00:00:00', 'medium' : 'test_medium'}
-        
+
         result = self.post(url, body, as_user=s.project.owner)
         if isinstance(result, list): result, = result
         self.assertEqual(result['headline'], body['headline'])
-        
+
         result = self.get(url)
         self.assertEqual(len(result['results']), 1)
         a = result['results'][0]
@@ -102,12 +102,12 @@ class TestArticle(ApiTestCase):
         r = list(amcates.ES().query(filters=dict(sets=s.id), fields=["text", "headline", 'medium']))
         self.assertEqual(len(r), 1)
         self.assertEqual(r[0].medium, "test_medium")
-        self.assertEqual(r[0].headline, "headline") 
+        self.assertEqual(r[0].headline, "headline")
 
     @amcattest.use_elastic
     def test_multiple(self):
         """Can we create multiple objects?"""
-        
+
         s = amcattest.create_test_set()
         url = ArticleViewSet.get_url(project=s.project.id, articleset=s.id)
         base = {'text' : 'bla', 'headline' : 'headline', 'date' : '2013-01-01T00:00:00', 'medium' : 'test_medium'}
@@ -131,7 +131,7 @@ class TestArticle(ApiTestCase):
         child1 = dict(base, headline='c1')
         child2 = dict(base, headline='c2')
         parent = dict(base, headline='parent')
-        
+
         body = dict(parent, children = json.dumps([child1, child2]))
         self.post(url, body, as_user=s.project.owner)
 
@@ -141,37 +141,37 @@ class TestArticle(ApiTestCase):
         self.assertEqual(result['c1']['parent'], result['parent']['id'])
         self.assertEqual(result['c2']['parent'], result['parent']['id'])
         self.assertEqual(result['parent']['parent'], None)
-        
+
     @amcattest.use_elastic
     def test_parents_multiple(self):
         """Can we add multiple objects with children?"""
         s = amcattest.create_test_set()
         url = ArticleViewSet.get_url(project=s.project.id, articleset=s.id)
         base = {'text' : 'bla', 'headline' : 'headline', 'date' : '2013-01-01T00:00:00', 'medium' : 'test_medium'}
-                
+
         child = dict(base, headline='c')
         parent = dict(base, headline='p')
         leaf = dict(base, headline='l')
 
         body = json.dumps([leaf, dict(parent, children=[child])])
         self.post(url, body, as_user=s.project.owner, request_options=dict(content_type='application/json'))
-        
+
         result = {a['headline'] : a for a in self.get(url)['results']}
         self.assertEqual(len(result), 3)
         self.assertEqual(result['c']['parent'], result['p']['id'])
         self.assertEqual(result['p']['parent'], None)
         self.assertEqual(result['l']['parent'], None)
-        
-        
+
+
     def test_permissions(self):
         from amcat.models import Role, ProjectRole
         metareader = Role.objects.get(label='metareader', projectlevel=True)
         reader = Role.objects.get(label='reader', projectlevel=True)
-        
+
         p1 = amcattest.create_test_project(guest_role=None)
         p2 = amcattest.create_test_project(guest_role=metareader)
-        
-        s1 = amcattest.create_test_set(project=p1)        
+
+        s1 = amcattest.create_test_set(project=p1)
         s2 = amcattest.create_test_set(project=p2)
 
         p1.articlesets.add(s2)
@@ -180,7 +180,7 @@ class TestArticle(ApiTestCase):
 
         body = {'text' : 'bla', 'headline' : 'headline', 'date' : '2013-01-01T00:00:00', 'medium' : 'test_medium'}
         # anonymous user shoud be able to read p2's articlesets but not articles (requires READER), and nothing on p1
-                                                  
+
         self.get(url(project=p1.id, articleset=s1.id), check_status=401)
         self.get(url(project=p2.id, articleset=s2.id), check_status=401)
 
@@ -189,7 +189,7 @@ class TestArticle(ApiTestCase):
 
         # it is illegal to view an articleset through a project it is not a member of
         self.get(url(project=p2.id, articleset=s1.id), check_status=404)
-        
+
         u = p1.owner
         ProjectRole.objects.create(project=p2, user=u, role=reader)
 
@@ -206,7 +206,7 @@ class TestArticle(ApiTestCase):
         # User u should be able to add articles to set 1 via project 1, but not p2/s2
         self.post(url(project=p1.id, articleset=s1.id), body, as_user=u, check_status=201)
         self.post(url(project=p2.id, articleset=s2.id), body, as_user=u, check_status=403)
-        
+
         # Neither u (p1.owner) nor p2.owner should be able to modify set 2 via project 1
         self.post(url(project=p1.id, articleset=s2.id), body, as_user=u, check_status=403)
         self.post(url(project=p1.id, articleset=s2.id), body, as_user=p2.owner, check_status=403)
