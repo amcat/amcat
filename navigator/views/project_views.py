c0d151d84539884261e2cb89dbcb9cf208f6cde6
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-06-09 01:22:27 +0200

    Set project list to use '(un)set fav' button rather than star icon

--- navigator/views/project_views.py
+++ navigator/views/project_views.py
@@ -21,7 +21,7 @@ from django.core.urlresolvers import reverse
 
 from navigator.views.datatableview import DatatableMixin
 from django.views.generic.list import ListView
-from api.rest.datatable import FavouriteDatatable
+from api.rest.datatable import Datatable
 from amcat.models import Project
 from navigator.views.projectview import ProjectViewMixin, HierarchicalViewMixin, BreadCrumbMixin
 from django.views.generic.edit import UpdateView
@@ -31,10 +31,29 @@ from amcat.scripts.actions.add_project import AddProject
 class ProjectListView(BreadCrumbMixin, DatatableMixin, ListView):
     model = Project
     template_name = "project/project_list.html"
-    
+
+
+    def get(self, *args, **kargs):
+        favaction = self.request.GET.get('favaction')
+        if (favaction is not None):
+            ids = {int(id) for id in self.request.GET.getlist('ids')}
+            favs = self.request.user.get_profile().favourite_projects
+            favids = set(favs.values_list("pk", flat=True))
+            if favaction == "Set":
+                ids -= favids
+                func = favs.add
+            else:
+                ids &= favids
+                func = favs.remove
+            if ids:
+                [func(id) for id in ids]
+
+        return super(ProjectListView, self).get(*args, **kargs)
+
     def get_context_data(self, **kwargs):
         context = super(ProjectListView, self).get_context_data(**kwargs)
         context["what"] = self.kwargs.get('what', 'favourites')
+        context["favaction"] = "Unset" if context['what'] == 'favourites' else "Set"
         context["main_active"] = 'Projects'
         return context
 
@@ -42,13 +61,14 @@ class ProjectListView(BreadCrumbMixin, DatatableMixin, ListView):
         return [("Projects", "#")]
 
     def filter_table(self, table):
+        table = table.rowlink_reverse('article set-list', args=['{id}'])
         what = self.kwargs.get('what', 'favourites')
         if what == 'favourites':
             # ugly solution - get project ids that are favourite and use that to filter, otherwise would have to add many to many to api?
             # (or use api request.user to add only current user's favourite status). But good enough for now...
             ids = self.request.user.get_profile().favourite_projects.all().values_list("id")
             ids = [id for (id, ) in ids]
-            if ids: 
+            if ids:
                 return table.filter(pk=ids, active=True)
             else:
                 return table.filter(name="This is a really stupid way to force an empty table (so sue me!)")
@@ -57,35 +77,16 @@ class ProjectListView(BreadCrumbMixin, DatatableMixin, ListView):
         elif what == "all":
             return table
 
-        
-    def get_datatable(self):
-        """Create the Datatable object"""
 
-        url = reverse('project', args=[123])
-        table = FavouriteDatatable(resource=self.get_resource(), label="project",
-                                   set_url=url + "?star=1", unset_url=url+"?star=0")
-        table = table.rowlink_reverse('article set-list', args=['{id}'])
-
-        table = self.filter_table(table)
-        return table
 
 from django import forms
-from amcat.models import Role    
+from amcat.models import Role
 class ProjectDetailsView(HierarchicalViewMixin, ProjectViewMixin, BreadCrumbMixin, UpdateView):
     context_category = 'Settings'
     parent = None
     base_url = "projects"
     model = Project
 
-    def get(self, *args, **kargs):
-        star = self.request.GET.get('star')
-        if (star is not None):
-            favs = self.request.user.get_profile().favourite_projects
-            (favs.add if int(star) else favs.remove)(self.project.id)
-                
-        return super(ProjectDetailsView, self).get(*args, **kargs)
-
-    
     def get_success_url(self):
         return reverse(self.get_view_name(), args=(self.project.id,))
 
@@ -94,7 +95,7 @@ class ProjectDetailsView(HierarchicalViewMixin, ProjectViewMixin, BreadCrumbMixi
     def _get_breadcrumb_url(cls, kwargs, view):
         return reverse("article set-list", args=(kwargs['project_id'],))
 
-        
+
     class form_class(forms.ModelForm):
         class Meta:
             model = Project
@@ -108,12 +109,12 @@ class ProjectAddView(BreadCrumbMixin, ScriptView):
     template_name = "script_base.html"
     script = AddProject
     model = Project
-    
+
     def get_context_data(self, **kwargs):
         context = super(ProjectAddView, self).get_context_data(**kwargs)
         context["cancel_url"] = reverse("projects")
         return context
-        
+
     def get_form(self, form_class):
         if self.request.method == 'GET':
             return form_class.get_empty(user=self.request.user)
