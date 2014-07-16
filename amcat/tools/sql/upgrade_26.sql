b92ea8d20a2e7f843519edff9fd0527ccb921781
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2014-07-16 14:37:29 +0200

    add update file

--- amcat/tools/sql/upgrade_26.sql
+++ amcat/tools/sql/upgrade_26.sql
@@ -1,3 +1,4 @@
 ALTER TABLE codingschemas_fields ADD COLUMN description text;
 ALTER TABLE codingschemas_fields ADD COLUMN keywords text;
+ALTER TABLE codingschemas_fields ADD COLUMN pagenr integer;
 
