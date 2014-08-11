a0116e0989ec903715498fb99eec1d543cf70764
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-08-11 10:23:58 +0200

    Added extra testcases for Factivia, changed scraper for those articles.

--- amcat/scripts/article_upload/factivia.py
+++ amcat/scripts/article_upload/factivia.py
@@ -28,19 +28,14 @@ from amcat.scripts.article_upload.upload import UploadScript
 
 from amcat.tools import amcattest
 from amcat.tools import toolkit
-from amcat.tools.caching import cached
 
-META = [
-    "headline", "author", "length", "date",
-    "medium", None, "pagenr"
-]
+META = ["headline", "length", "date", "medium", None, "pagenr"]
 
 PROCESSORS = {
-    "author": lambda a: a.lstrip("Von "),
     "length": lambda l: int(l.rstrip("words")),
     "date": toolkit.read_date,
     "medium": Medium.get_or_create,
-    "pagenr": int
+    "pagenr": lambda p: int(p) if p.strip().isdigit() else None
 }
 
 
@@ -59,11 +54,19 @@ class Factivia(UploadScript):
         # We select all 'div' elements directly under '.article'
         divs = document.cssselect("* > div")
 
-        # Some articles do not mention an author, so we need to remove it from
-        # the list of metadata
-        author_field = divs[META.index("author")]
-        if "Von" not in author_field.text_content():
-            metadata.remove("author")
+        # Check for author field. If present: remove from metadata
+        # fields list
+        try:
+            author_field = document.cssselect(".author")[0]
+        except IndexError:
+            pass
+        else:
+            article.author = author_field.text_content().lstrip("Von").strip()
+            divs.remove(author_field)
+
+        # Strip everything before headline
+        headline_field = document.cssselect("b.deHeadline")[0].getparent()
+        divs = divs[divs.index(headline_field):]
 
         # Parse metadata. Loop through each 'div' within an article, along with
         # its field name according to META (thus based on its position)
@@ -85,39 +88,68 @@ class Factivia(UploadScript):
 
 class FactiviaTest(amcattest.AmCATTestCase):
     ### SCRAPE LOGIC ###
-    @property
-    def scraper(self):
-        test_file = path.join(path.dirname(__file__), "test_files/factivia.htm")
+    def get_scraper(self, filename):
+        test_file = path.join(path.dirname(__file__), "test_files/factivia", filename)
 
         return Factivia(options={
             'project': amcattest.create_test_project().id,
             'articlesets': [amcattest.create_test_set().id],
             'file': SimpleUploadedFile.from_dict({
-                "filename": "factivia.htm",
+                "filename": filename,
                 "content": open(test_file).read(),
                 "content_type": "text/html"
             })
         })
 
-    @property
-    def articles(self):
-        return self.get_articles()
-
-    @cached
-    def get_articles(self):
-        return ArticleSet.objects.get(id__in=self.scraper.run()).articles.all()
+    def get_articles(self, filename="factivia.htm"):
+        articlesets = self.get_scraper(filename).run()
+        return ArticleSet.objects.get(id__in=articlesets).articles.all()
 
     ### TESTS ###
     @amcattest.use_elastic
-    def test_scraping(self):
-        self.assertEqual(9, len(self.articles))
+    def test_factivia(self):
+        articles = self.get_articles()
 
-        first_article = self.articles.get(headline="Laut Rechtsprofessoren liegt die SVP falsch")
+        self.assertEqual(9, len(articles))
+
+        first_article = articles.get(headline="Laut Rechtsprofessoren liegt die SVP falsch")
         self.assertEqual(475, first_article.length)
         self.assertEqual(datetime(2013, 11, 19), first_article.date)
         self.assertEqual("Die S\xfcdostschweiz", first_article.medium.name)
         self.assertEqual(14, first_article.pagenr)
+        self.assertEqual("Lorenz Honegger", first_article.author)
 
         self.assertIn("Die SVP sieht sich in ihrem Argwohn", first_article.text)
         self.assertIn("Heimatstaat von Folter oder Tod bedroht sind", first_article.text)
         self.assertIn("Ob die umstrittene Stelle aus der Initiative gestrichen wird", first_article.text)
+
+    @amcattest.use_elastic
+    def test_az01(self):
+        # Just check for errorlessness
+        self.get_articles("az01.htm")
+
+    @amcattest.use_elastic
+    def test_so00(self):
+        # Just check for errorlessness
+        self.get_articles("SO00.htm")
+
+    @amcattest.use_elastic
+    def test_ww04(self):
+        articles = self.get_articles("ww04.htm")
+
+        article = articles.get(headline="Vor dieser Stadt wird gewarnt")
+        self.assertEqual(2068, article.length)
+        self.assertEqual(datetime(2012, 6, 28), article.date)
+        self.assertEqual("Weltwoche", article.medium.name)
+        self.assertEqual(None, article.pagenr)
+        self.assertEqual("Lucien Scherrer", article.author)
+
+    @amcattest.use_elastic
+    def test_zt04(self):
+        articles = self.get_articles("zt04.htm")
+
+        article = articles.get(headline="Das ganze volle Leben in der Zeitung")
+        self.assertEqual(569, article.length)
+        self.assertEqual(datetime(2011, 8, 10), article.date)
+        self.assertEqual("Zofinger Tagblatt", article.medium.name)
+        self.assertEqual(None, article.pagenr)
