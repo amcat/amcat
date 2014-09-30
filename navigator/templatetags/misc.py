03bd78218bd72db184cceb5a591531a582e1467e
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-09-30 12:59:30 +0200

    Show tokens in clauses view, cleanup html, stub code for frames

--- navigator/templatetags/misc.py
+++ navigator/templatetags/misc.py
@@ -5,7 +5,22 @@ register = template.Library()
 @register.filter
 def AMCAT_VERSION(a):
     """Return current AmCAT version based on mercurial repository"""
+    # IS THIS ACTUALLY USED ANYWHERE?
     return settings.AMCAT_VERSION
 
 AMCAT_VERSION.is_safe = True
 
+# http://stackoverflow.com/questions/8000022
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+@register.filter
+def join_list(l, sep):
+    "Join l by sep if it is a list"
+    if not isinstance(l, (str, unicode)):
+        try:
+            return sep.join(l)
+        except TypeError:
+            pass
+    return l
--- navigator/views/clause_views.py
+++ navigator/views/clause_views.py
@@ -1,4 +1,5 @@
-from saf import SAF
+from saf.clause import *
+from saf.saf import SAF
 import json
 
 from django.views.generic.edit import FormView
@@ -8,37 +9,11 @@ from navigator.views.projectview import ProjectViewMixin, HierarchicalViewMixin,
 from navigator.views.project_views import ProjectDetailsView
 from amcat.tools.amcatxtas import get_adhoc_result
 from amcat.models import Codebook
+from itertools import chain
 
-def get_source(saf, predicate):
-    "Return the source tokens (if any) of a source that contains the whole predicate"
-    predicate = set(predicate)
-    result = set()
-    for source in saf.sources:
-        if predicate.issubset(set(source['quote'])):
-            result |= set(source['source'])
-    return result
-
-def contained_tokens(saf, predicate):
-    "Return all tokens in predicates contained in this predicate (list of token ids)"
-    result = set()
-    predicate = set(predicate)
-    for clause in saf.clauses:
-        p2 = set(clause['predicate']) | set(clause['subject'])
-        if p2 != predicate and p2.issubset(predicate):
-            result |= p2
-    return result
-
-def get_reduced_clauses(saf):
-    for clause in saf.clauses:
-        clause = clause.copy()
-        # remove contained tokens
-        clause['source'] = get_source(saf, clause['predicate'])
-        clause['predicate'] = [t for t in clause['predicate'] if t not in contained_tokens(saf, clause['predicate'])]
-        # resolve tokens
-        for place in "source", "subject", "predicate":
-            clause[place] = [saf.get_token(c) for c in clause[place]]
-            clause[place] = sorted(clause[place], key=lambda t:t['offset'])
-        yield clause
+def flatten(listOfLists):
+    "Flatten one level of nesting - https://docs.python.org/2/library/itertools.html"
+    return chain.from_iterable(listOfLists)
 
 class ClauseView(HierarchicalViewMixin,ProjectViewMixin, BreadCrumbMixin, FormView):
     class form_class(forms.Form):
@@ -68,81 +43,39 @@ class ClauseView(HierarchicalViewMixin,ProjectViewMixin, BreadCrumbMixin, FormVi
         saf = SAF(parse)
         tokens  = saf._tokens
 
-        clauses = list(get_reduced_clauses(saf))
-
-        # make list of tokens with role
-        roles = {} # role per token
-        for i, clause in enumerate(clauses):
-            for place in "subject", "predicate":
-                for token in clause[place]:
-                    roles[token['id']] = (place, i)
-        src_roles = {} # source role per token
-        for i, source in enumerate(saf.sources):
-            for place in "source", "quote":
-                for token in source[place]:
-                    src_roles[token] = (place, i)
-
-        tokens = list(saf.get_tokens(1))
-        for t in tokens:
-            t['role'] = roles.get(t['id'])
-            t['src_role'] = src_roles.get(t['id'])
-
-
         # add codebook
-        codebook = Codebook.objects.get(pk=568)
-        codebook_dict = {}
-        for code in codebook.codes:
-            concept = None
-            lemmata = []
-            for label in code.labels.all():
-                if label.language.label == "en":
-                    concept = label.label
-                else:
-                    lemmata += [x.strip() for x in label.label.split(",")]
-            codebook_dict[concept] = lemmata
+        codebook_id = 3
+        codebook = Codebook.objects.get(pk=codebook_id)
+        saf.saf['codes'] = list(match_codes(saf, codebook))
 
-        # add codes
-        for t in saf.get_tokens(1):
-            tlemma = t['lemma'].lower()
-            for code, lemmata in codebook_dict.iteritems():
-                for lemma in lemmata:
-                    lemma = lemma.lower()
-                    if lemma == tlemma or (lemma.endswith("*") and tlemma.startswith(lemma[:-1])):
-                        t['codes'] = t.get('codes', []) + [code]
+        # get tokens and clauses
+        tokens = saf.resolve()
+        clauses = list(saf.get_reduced_clauses())
 
-        # add coreferences
-        corefs = dict(get_coreferences(saf.coreferences) if 'coreferences' in saf.saf else [])
-        coref_groups = {group: i+1 for  (i, group) in enumerate({tuple(v) for v in corefs.values()})}
-        codes_per_group = {}
-        for group, i in coref_groups.iteritems():
-            for token in group:
-                t = saf.get_token(token)
-                t['coref'] = i
-                print t
-                for code in t.get('codes', []):
-                    codes_per_group[i] = codes_per_group.get(i, set()) | {code}
+        # add frames
+        for clause in clauses:
+            add_frames(saf, clause)
 
-        for group, i in coref_groups.iteritems():
-            if i in codes_per_group and len(codes_per_group[i]) == 1:
-                code = list(codes_per_group[i])[0]
-                for token in group:
-                    saf.get_token(token)['codes'] = [code]
+        # provide list of all codes
+        codes = set(flatten(t.get('codes', []) for t in tokens))
 
+        # resolve all places in the clauses
+        for clause in clauses:
+            for place in ['source', 'subject', 'predicate']:
+                clause[place] = saf.resolve(clause[place])
+        print clauses
         # make tree
         from syntaxrules.syntaxtree import SyntaxTree, VIS_IGNORE_PROPERTIES
         import base64
-        VIS_IGNORE_PROPERTIES.append("role")
-        VIS_IGNORE_PROPERTIES.append("src_role")
+        VIS_IGNORE_PROPERTIES.append("clause_role")
+        VIS_IGNORE_PROPERTIES.append("clause_id")
+        VIS_IGNORE_PROPERTIES.append("source_role")
+        VIS_IGNORE_PROPERTIES.append("source_id")
         VIS_IGNORE_PROPERTIES.append("pos")
 
         t = SyntaxTree(saf.saf)
         g = t.get_graphviz()
         tree = base64.b64encode(g.draw(format='png', prog='dot'))
-
-        codes = set()
-        for t in tokens:
-            for code in t.get('codes', []):
-                codes.add(code)
 
         ctx = dict(form=form,
                    clauses=clauses,
@@ -151,40 +84,19 @@ class ClauseView(HierarchicalViewMixin,ProjectViewMixin, BreadCrumbMixin, FormVi
                    codes=codes,
                    tree=tree)
 
-
-
-
-
         return self.render_to_response(self.get_context_data(**ctx))
 
-def merge(lists):
-    """
-    Merge the lists so lists with overlap are joined together
-    (i.e. [[1,2], [3,4], [2,5]] --> [[1,2,5], [3,4]])
-    from: http://stackoverflow.com/a/9400562
-    """
-    newsets, sets = [set(lst) for lst in lists if lst], []
-    while len(sets) != len(newsets):
-        sets, newsets = newsets, []
-        for aset in sets:
-            for eachset in newsets:
-                if not aset.isdisjoint(eachset):
-                    eachset.update(aset)
-                    break
-            else:
-                newsets.append(aset)
-    return newsets
 
+def get_frames(su, pr):
+    if "israel" in su and "attack" in pr:
+        yield "ISRAEL_MEANS"
+    if "palestine" in su and "attack" in pr:
+        yield "ISRAEL_PROBLEM"
+    if "israel" in pr and "attack" in pr:
+        yield "ISRAEL_PROBLEM"
 
-def get_coreferences(coreferences):
-    """Decode the SAF coreferences as (node: coreferencing_nodes) pairs"""
-    coref_groups = []
-    for coref in coreferences:
-        nodes = []
-        for place in coref:
-            nodes += place
-            # take only the heads of each coref group
-        coref_groups.append(nodes)
-    for nodes in merge(coref_groups):
-        for node in nodes:
-            yield node, nodes
+def add_frames(saf, clause):
+    su_codes = set(flatten(saf.get_token(t).get('codes', []) for t in clause['subject']))
+    pr_codes = set(flatten(saf.get_token(t).get('codes', []) for t in clause['predicate']))
+    frames=set(get_frames(su_codes, pr_codes))
+    clause.update(dict(subject_codes=su_codes, predicate_codes=pr_codes, frames=frames))
