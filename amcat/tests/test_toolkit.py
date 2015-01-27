cfb563e54073c698ad14084c3c191bcffa2457ea
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-01-27 10:56:44 +0100

    Add date format 23aug2013

--- amcat/tests/test_toolkit.py
+++ amcat/tests/test_toolkit.py
@@ -67,6 +67,7 @@ class TestToolkit(amcattest.AmCATTestCase):
             (u'30 ao\xfbt 2002', datetime.datetime(2002, 8, 30, 0, 0, 0), False, False),
             ('31. Maerz 2003', datetime.datetime(2003, 3, 31, 0, 0, 0), False, False),
             ('September 1, 2008 Monday 12:44 PM AEST', datetime.datetime(2008, 9, 1, 12, 44), False, False),
+            ('23aug2013', datetime.datetime(2013, 8, 23, 0, 0, 0), False, False),
             ):
             if inspect.isclass(date) and issubclass(date, Exception):
                 self.assertRaises(date, toolkit.readDate, s, lax=False, american=american)
