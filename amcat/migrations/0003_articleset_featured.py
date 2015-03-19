218714ba30f3db644706998c79b762ad7bb2685b
Martijn Bastiaan <martijn@hmbastiaan.nl>
2015-03-19 16:49:08 +0100

    Added articleset.featured migration

---
+++ amcat/migrations/0003_articleset_featured.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('amcat', '0002_query'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='articleset',
+            name='featured',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+    ]
