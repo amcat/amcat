bf3d7da02004eb679d1d0d5cb245fe54f06baff2
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2015-04-22 21:43:00 +0200

    Added 'project' argument in SaveAsSetAction

--- amcat/scripts/query/saveset.py
+++ amcat/scripts/query/saveset.py
@@ -22,6 +22,9 @@ from django.shortcuts import render
 from django.template import Context
 from django.template.loader import get_template
 from amcat.models import ArticleSet
+from amcat.models import Project
+from amcat.models import Role
+from amcat.models.authorisation import ROLE_PROJECT_WRITER
 
 from amcat.scripts.query import QueryAction, QueryActionForm
 from amcat.tools.keywordsearch import SelectionSearch
@@ -31,16 +34,24 @@ OK_TEMPLATE = get_template("query/ok.html")
 
 
 class SaveAsSetForm(QueryActionForm):
+    project = forms.ModelChoiceField(queryset=Project.objects.none(), empty_label=None)
     name = forms.CharField(required=True)
-    provenance = forms.CharField(required=False)
+    #provenance = forms.CharField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(SaveAsSetForm, self).__init__(*args, **kwargs)
+        rw_role = Role.objects.get(id=ROLE_PROJECT_WRITER)
+        self.fields["project"].queryset = self.user.userprofile.get_projects(rw_role)
+        self.fields["project"].initial = self.project
 
     def clean_name(self):
         new_name = self.cleaned_data["name"]
+        project = self.cleaned_data["project"]
 
-        all_articlesets = self.project.articlesets_set.all()
+        all_articlesets = project.articlesets_set.all()
         if all_articlesets.filter(name__iexact=new_name).exists():
-            error_msg = "An articleset called {} already exists."
-            raise ValidationError(error_msg.format(self.cleaned_data["name"]))
+            error_msg = "An articleset called {0!r} already exists in project {1.name!r}."
+            raise ValidationError(error_msg.format(new_name, project))
 
         return new_name
 
@@ -51,15 +62,16 @@ class SaveAsSetAction(QueryAction):
 
     def run(self, form):
         name = form.cleaned_data["name"]
-        provenance = form.cleaned_data["provenance"]
-        aset = ArticleSet.objects.create(name=name, provenance=provenance, project=self.project)
+        #provenance = form.cleaned_data["provenance"]
+        project = form.cleaned_data["project"]
+        aset = ArticleSet.objects.create(name=name, project=project)
         self.monitor.update(10, "Executing query..")
         article_ids = list(SelectionSearch(form).get_article_ids())
         self.monitor.update(60, "Saving to set..")
         aset.add_articles(article_ids)
 
         return OK_TEMPLATE.render(Context({
-            "project": self.project,
+            "project": project,
             "aset": aset,
             "len": len(article_ids)
         }))
