2ee99a106e3d63cd6775d8aa98169ab02ca02950
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-10-11 20:38:06 +0200

    Add git info to status

--- api/rest/views/status.py
+++ api/rest/views/status.py
@@ -4,11 +4,27 @@ from rest_framework.status import HTTP_200_OK
 
 from amcat.amcatcelery import status
 from amcat.tools.amcates import ES
+from git import Repo
+import time
 
 class StatusView(APIView):
     def get(self, request):
-        data = status()
-        data['celery_worker'] = status.delay().wait()
-        data['elastic'] = ES().status()
+        data = {"amcat": status(),
+                'celery_worker': status.delay().wait(),
+                'elastic': ES().status(),
+                'git': git_status()}
         return Response(data, status=HTTP_200_OK)
 
+
+def git_status():
+    def date2iso(date):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(date))
+    # there is probably a better place for this!
+    repo = Repo()
+    return {
+        "active_branch": unicode(repo.active_branch),
+        "last_commit": {
+            "summary": repo.head.commit.summary,
+            "committed_date": date2iso(repo.head.commit.committed_date),
+            "commtter": unicode(repo.head.commit.committer)}}
+        
