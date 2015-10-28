8ecbe9d4f4cfd8d7bfa143a71db10bdc5e8dbf95
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-10-28 13:33:06 +0100

    make codingschema.highlight_language really optional (fixes #378)

---
+++ amcat/migrations/0008_auto_20151028_1300.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('amcat', '0007_optionalschemas'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='codingschema',
+            name='highlight_language',
+            field=models.ForeignKey(blank=True, to='amcat.Language', null=True),
+        ),
+    ]
