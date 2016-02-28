eef5e7fc55e86fcc928c247031fb9c31cfeb339f
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-02-29 00:04:49 +0100

    Always log API exceptions

--- api/rest/exception.py
+++ api/rest/exception.py
@@ -3,11 +3,10 @@ from rest_framework import exceptions
 import logging
 
 def exception_handler(exc, context):
-    if isinstance(exc, exceptions.APIException):
-        try:
-            raise
-        except:
-            logging.exception("API Exception")
+    try:
+        raise
+    except:
+        logging.exception("API Exception")
         
     return views.exception_handler(exc, context)
     
