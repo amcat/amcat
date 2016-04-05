b585a3dd18d8415d73f11575c437a5832f6fd89b
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-04-05 18:40:00 +0200

    merge migrations

---
+++ amcat/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('amcat', '0010_task_user_null'),
+        ('amcat', '0011_article_length_not_null'),
+    ]
+
+    operations = [
+    ]
