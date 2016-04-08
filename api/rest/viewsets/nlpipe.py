89a1e7a83441b4c192c279906d3ddcc4a16ed85d
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-04-08 17:02:33 +0200

    Get ids from elasticsearch for nlpipe API
    Conflicts:
    	api/rest/viewsets/nlpipe.py

--- api/rest/viewsets/nlpipe.py
+++ api/rest/viewsets/nlpipe.py
@@ -37,6 +37,8 @@ from rest_framework import serializers
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.viewsets import ModelViewSet, ViewSet, GenericViewSet
 
+
+
 from amcat.models import Article, ArticleSet
 
 from api.rest.viewsets.articleset import ArticleSetViewSetMixin
@@ -44,6 +46,7 @@ from api.rest.viewsets.project import ProjectViewSetMixin
 from api.rest.viewsets.article import ArticleViewSetMixin
 from api.rest.mixins import DatatablesMixin
 
+from nlpipe.backend import get_cached_document_ids
 from nlpipe.pipeline import get_results
 from nlpipe.celery import app
 
@@ -57,6 +60,18 @@ def _get_tokens(term_vector, pos_inc=0, offset_inc=0):
             token['start_offset'] += offset_inc
             token['end_offset'] += offset_inc
             token['term'] = term
+            yield token
+
+def _tokens_from_vectors(vectors, fields, aid):
+    pos_inc, offset_inc = 0, 0
+    for field in fields:
+        tokens = _get_tokens(vectors[field]['terms'],
+                             pos_inc=pos_inc, offset_inc=offset_inc)
+        tokens = sorted(tokens, key = lambda t:t['position'])
+        pos_inc = (tokens[-1]['position'] + 1) if tokens else 0
+        offset_inc = (tokens[-1]['end_offset'] + 1) if tokens else 0
+        for token in tokens:
+            token['aid'] = aid
             yield token
 
 def _termvector(aid):
@@ -77,24 +92,21 @@ def _termvector(aid):
 
     
 def _termvectors(aids):
-    return {aid: list(_termvector(aid)) for aid in aids}
+    fields = ["headline", "text"]
+    for doc in amcates.ES().term_vectors(aids, fields):
+        aid = doc['_id']
+        yield aid, list(_tokens_from_vectors(doc['term_vectors'], fields, aid))
 
 
 class NLPipeLemmataSerializer(serializers.Serializer):
-
     class Meta:
         class list_serializer_class(serializers.ListSerializer):
             def to_representation(self, data):
 
-                only_cached = self.context['request'].GET.get('only_cached', 'N')
-                only_cached = only_cached[0].lower() in ['1', 'y']
-                aids = [a.pk for a in data]
                 if self.context['request'].GET.get('module', 'elastic') == "elastic":
-                    self.child._cache = _termvectors(aids)
+                    self.child._cache = dict(_termvectors(data))
                 else:
-                    self.child._cache = get_results(aids, self.child.module, only_cached=only_cached)
-                if only_cached:
-                    data = [a for a in data if a.pk in self.child._cache]
+                    self.child._cache = get_results(data, self.child.module)
                 result = serializers.ListSerializer.to_representation(self, data)
                 # flatten list of lists
                 result = itertools.chain(*result)
@@ -112,7 +124,7 @@ class NLPipeLemmataSerializer(serializers.Serializer):
         return getattr(tasks, module)
     
     def to_representation(self, article):
-        result = self._cache[article.pk]
+        result = self._cache[str(article)]
         if isinstance(result, list):
             return result
         else:
@@ -129,7 +141,7 @@ class NLPipeLemmataSerializer(serializers.Serializer):
             tokens = [tokendict[id] for id in term.get_span().get_span_ids()]
             for token in tokens:
                 tid = term.get_id()
-                tok = {"aid": article.pk,
+                tok = {"aid": article,
                        "token_id": token.get_id(),
                        "offset": token.get_offset(),
                        "sentence": token.get_sent(),
@@ -152,7 +164,35 @@ class NLPipeLemmataViewSet(ProjectViewSetMixin, ArticleSetViewSetMixin, Datatabl
     queryset = Article.objects.all()
     serializer_class = NLPipeLemmataSerializer
 
+    @property
+    def module(self):
+        module = self.request.GET.get('module')
+        if not module:
+            raise ValidationError("Please specify the NLP module to use with a module= GET parameter")
+        from nlpipe import tasks
+        if not hasattr(tasks, module):
+            raise ValidationError("Module {module} not known".format(**locals()))
+        
+        return getattr(tasks, module)
+    
     def filter_queryset(self, queryset):
+
+        logging.info("Getting ids")
+        ids = list(self.articleset.get_article_ids_from_elastic())
+        logging.info("Got {} ids".format(len(ids)))
+
+
+        only_cached = self.request.GET.get('only_cached', 'N')
+        only_cached = only_cached[0].lower() in ['1', 'y']
+
+        if only_cached:
+            logging.info("Filtering ids")
+            ids = list(get_cached_document_ids(ids, self.module.doc_type))
+            logging.info("{} ids left".format(len(ids)))
+            
+        
+        return ids
+              
         queryset = super(NLPipeLemmataViewSet, self).filter_queryset(queryset)
         # only(.) would be better on serializer, but meh
         try:
@@ -189,7 +229,7 @@ class PreprocessViewSet(ProjectViewSetMixin, ArticleSetViewSetMixin, DatatablesM
         from nlpipe.document import count_cached
         for module, n in count_cached(ids):
             result.append(ModuleCount(module, n))
-        
+
         return result
 
     def get_filter_fields(self):
