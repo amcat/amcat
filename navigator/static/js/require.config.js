bc76cf96bd23f6b7efd0c2405565639d2ac501e1
Christian Stuart <c.j.stuart@students.uu.nl>
2016-04-25 21:11:31 +0200

    Revert "Firefox is confused when heatmap is called heatmap, so it's renamed to heatmap-fixed.."
    This reverts commit b27209146381b4cb386dbf00466574df6ffa9c0e.

--- navigator/static/js/require.config.js
+++ navigator/static/js/require.config.js
@@ -12,9 +12,7 @@ requirejs.config({
         "highcharts.core": "highcharts/highcharts",
         "highcharts.data": "highcharts/modules/data",
         "highcharts.exporting": "highcharts/modules/exporting",
-        // For some reasons, Firefox refuses to load the heatmap plugin if it's
-        // called just 'heatmap'. Soo..... :|
-        "highcharts.heatmap": "highcharts/modules/heatmap-fixed",
+        "highcharts.heatmap": "highcharts/modules/heatmap",
         "jquery.depends": AMCAT_STATIC_URL + "js/jquery.depends",
         "bootstrap": "bootstrap/dist/js/bootstrap",
         "bootstrap-multiselect": "bootstrap-multiselect/dist/js/bootstrap-multiselect",
