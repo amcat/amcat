378beff1a01ac6c72170feeaf58431e87934ebf6
Christian Stuart <c.j.stuart@students.uu.nl>
2016-05-24 15:21:17 +0200

    resolve_queries test cherrypick fix and  cleanup

--- amcat/tools/tests/test_keywordsearch.py
+++ amcat/tools/tests/test_keywordsearch.py
@@ -3,7 +3,7 @@ from amcat.tools import amcattest
 from amcat.tools.amcattest import create_test_codebook
 from amcat.tools.keywordsearch import SearchQuery, resolve_queries
 from amcat.tools import djangotoolkit
-from amcat.tools.toolkit import stripAccents
+from amcat.tools.toolkit import strip_accents
 
 
 class TestKeywordSearch(amcattest.AmCATTestCase):
@@ -17,52 +17,45 @@ class TestKeywordSearch(amcattest.AmCATTestCase):
 
 class TestSearchQuery(amcattest.AmCATTestCase):
 
-    def test_from_string(self):
-        label = u'\u5728\u8377\u5170\u98ce\u8f66'
-        query_text = u'\xda\xd1\xcd\xa2\xd3\xd0\xc9 \xde\xc9X\xde'
-        query = SearchQuery.from_string(u'{}#{}'.format(label, query_text))
-        self.assertEqual(query.label, stripAccents(label))
-        self.assertEqual(query.query, stripAccents(query_text))
-
-    def test_resolve_queries(self):
-        l_lang = djangotoolkit.get_or_create(Language, label='l_lang')
-        r_lang = djangotoolkit.get_or_create(Language, label='r_lang')
-        codes = [
+    def setUp(self):
+        self.codes = [
             # (label, value, parent)
             ("root", "root r", None),
             ("code1", "Code One", "root"),
-            (u'\u5728\u8377\u5170\u98ce\u8f66', u'\xda\xd1\xcd\xa2\xd3\xd0\xc9 \xde\xc9X\xde', None),
-        ]
-        queries = {
-            SearchQuery(u"<{}>".format(label)): result for label, result, _ in codes
-        }
-        codebook = self._get_test_codebook(codes, l_lang, r_lang)
-
-        for query, expected_result in queries.items():
-            result_query = list(resolve_queries([query], codebook, l_lang, r_lang))[0].query
-            print(expected_result, result_query)
-            self.assertEqual(result_query, u"({})".format(unicode(expected_result)))
-
-    def test_resolve_queries_recursive(self):
-        l_lang = djangotoolkit.get_or_create(Language, label='l_lang')
-        r_lang = djangotoolkit.get_or_create(Language, label='r_lang')
-
-        codes = [
-            # (label, value, parent)
-            ("codes", "Code Collection", None),
+            (u'\u5728\u8377\u5170\u98ce\u8f66', u'\xda\xd1\xcd\xa2\xd3\xd0\xc9 \xde\xc9X\xde', "root"),
+            ("codes", "Code Collection", "root"),
             ("one", "eins OR een", "codes"),
             ("two", "zwei AND deux OR twee", "codes"),
             ("three", "drei OR drie AND trois", "two")
         ]
-        codebook = self._get_test_codebook(codes, l_lang, r_lang)
+        self.l_lang = djangotoolkit.get_or_create(Language, label='l_lang')
+        self.r_lang = djangotoolkit.get_or_create(Language, label='r_lang')
+        self.codebook = self._get_test_codebook(self.codes, self.l_lang, self.r_lang)
 
-        query = SearchQuery("<codes+>")
-        expected_results = set(word for code in codes for word in code[1].split(" OR "))
+    def test_from_string(self):
+        label = u'\u5728\u8377\u5170\u98ce\u8f66'
+        query_text = u'\xda\xd1\xcd\xa2\xd3\xd0\xc9 \xde\xc9X\xde'
+        query = SearchQuery.from_string(u'{}#{}'.format(label, query_text))
+        self.assertEqual(query.label, strip_accents(label))
+        self.assertEqual(query.query, strip_accents(query_text))
 
-        result_query = list(resolve_queries([query], codebook, l_lang, r_lang))[0].query
-        query_words = result_query[1:-1].split(" OR ")
+    def test_resolve_queries(self):
+        queries = {
+            SearchQuery(u"<{}>".format(label)): result for label, result, _ in self.codes
+        }
 
-        self.assertItemsEqual(query_words, expected_results)
+        for query, expected_result in queries.items():
+            result_query = list(resolve_queries([query], self.codebook, self.l_lang, self.r_lang))[0].query
+            self.assertEqual(result_query, u"({})".format(expected_result))
+
+    def test_resolve_queries_recursive(self):
+        query = SearchQuery("<root+>")
+
+        expected_results = set(word for code in self.codes for word in code[1].split(" OR "))
+
+        result_query = list(resolve_queries([query], self.codebook, self.l_lang, self.r_lang))[0].query
+        query_words = set(result_query[1:-1].split(" OR "))
+        self.assertSetEqual(query_words, expected_results)
 
     def _get_test_codebook(self, codes, l_lang, r_lang):
         codebook = create_test_codebook()
