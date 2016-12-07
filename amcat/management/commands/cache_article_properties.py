7644cfa52f66aa407d8a2a0eff305c4256558a2a
Martijn Bastiaan <martijn@hmbastiaan.nl>
2016-12-07 14:33:56 +0100

    Added command to refresh all article property caches

---
+++ amcat/management/commands/cache_article_properties.py
@@ -0,0 +1,43 @@
+###########################################################################
+#          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
+#                                                                         #
+# This file is part of AmCAT - The Amsterdam Content Analysis Toolkit     #
+#                                                                         #
+# AmCAT is free software: you can redistribute it and/or modify it under  #
+# the terms of the GNU Affero General Public License as published by the  #
+# Free Software Foundation, either version 3 of the License, or (at your  #
+# option) any later version.                                              #
+#                                                                         #
+# AmCAT is distributed in the hope that it will be useful, but WITHOUT    #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or   #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public     #
+# License for more details.                                               #
+#                                                                         #
+# You should have received a copy of the GNU Affero General Public        #
+# License along with AmCAT.  If not, see <http://www.gnu.org/licenses/>.  #
+###########################################################################
+import tqdm
+import multiprocessing
+
+from multiprocessing.pool import ThreadPool
+from django.core.management import BaseCommand
+
+from amcat.models import ArticleSet
+
+
+def refresh(aset_id):
+    ArticleSet.objects.get(aset_id)._refresh_property_cache()
+
+
+class Command(BaseCommand):
+    help = 'Recalculate all flexible properties caches.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--parallel', type=int, default=multiprocessing.cpu_count())
+
+    def handle(self, *args, **options):
+        threads = options["parallel"]
+        pool = ThreadPool(threads)
+        articleset_ids = ArticleSet.objects.values_list("id", flat=True)
+        results = pool.imap_unordered(refresh, articleset_ids)
+        tqdm.tqdm(results, total=len(articleset_ids))
