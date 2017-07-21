81d207865f22e67da4acebe944cc2daf5278cdfc
AmCAT <amcat@amcat.nl>
2017-07-21 13:33:54 +0200

    Make coosto accept different header name

--- amcat/scripts/article_upload/coosto.py
+++ amcat/scripts/article_upload/coosto.py
@@ -3,10 +3,12 @@ from .upload import UploadScript
 import csv, datetime, json
 from amcat.models import Article, Medium
 from amcat.scripts.article_upload.fileupload import ZipFileUploadForm
+import logging
+log = logging.getLogger(__name__)
 
-# At least this many fields have to match in order to detect it as that language. Set to 3,
-# because EN and NL share 2 values: 'url' and 'type'
-LANG_MATCH_THRESHOLD = 3
+# At least this many fields have to match in order to detect it as that language. Set to 4,
+# because EN and NL share 3 values: 'url', 'sentiment', and 'type'
+LANG_MATCH_THRESHOLD = 4
 
 
 class Language:
@@ -36,44 +38,55 @@ class Language:
         if isinstance(items, dict):
             items = items.keys()
         items = set(items)
-        known_cols = {v for k, v in cls.__dict__.items() if k[:2] != "__"}
+        attrs = (v for k, v in cls.__dict__.items() if k[:2] != "__")
+        known_cols = {field for attr in attrs for field in attr}
         return len(items & known_cols) >= LANG_MATCH_THRESHOLD
 
+    @classmethod
+    def popfield(cls, row, attr):
+        """
+        Pop the field from the row corresponding to the attribute
+        """
+        key = getattr(cls, attr)
+        for k in key:
+            if k in row:
+                return row.pop(k)
+        raise KeyError("Could not find key {key} in row {row}".format(**locals()))
 
 class Lang_NL(Language):
-    query = "zoekopdracht"
-    date = "datum"
-    url = "url"
-    sentiment = "sentiment"
-    type = "type"
-    discussion_length = "discussielengte"
-    reach = "bereik"
-    author = "auteur"
-    followers = "volgers"
-    influence = "invloed"
-    GPS_latitude = "GPS breedtegraad"
-    GPS_longitude = "GPS lengtegraad"
-    message_text = "bericht tekst"
-    source = "type bron"
-    title = "titel"
+    query = ["zoekopdracht"]
+    date = ["datum"]
+    url = ["url"]
+    sentiment = ["sentiment"]
+    type = ["type"]
+    discussion_length = ["discussielengte"]
+    reach = ["bereik", "views"]
+    author = ["auteur"]
+    followers = ["volgers"]
+    influence = ["invloed"]
+    GPS_latitude = ["GPS breedtegraad"]
+    GPS_longitude = ["GPS lengtegraad"]
+    message_text = ["bericht tekst"]
+    source = ["type bron"]
+    title = ["titel"]
 
 
 class Lang_EN(Language):
-    query = "query"
-    date = "date"
-    url = "url"
-    sentiment = "sentiment"
-    type = "type"
-    discussion_length = "discussion length"
-    reach = "reach"
-    author = "author"
-    followers = "followers"
-    influence = "influence"
-    GPS_latitude = "GPS latitude"
-    GPS_longitude = "GPS longitude"
-    message_text = "message text"
-    source = "source"
-    title = "title"
+    query = ["query"]
+    date = ["date"]
+    url = ["url"]
+    sentiment = ["sentiment"]
+    type = ["type"]
+    discussion_length = ["discussion length"]
+    reach = ["reach"]
+    author = ["author"]
+    followers = ["followers"]
+    influence = ["influence"]
+    GPS_latitude = ["GPS latitude"]
+    GPS_longitude = ["GPS longitude"]
+    message_text = ["message text"]
+    source = ["source"]
+    title = ["title"]
 
 
 class CoostoForm(UploadScript.options_form, ZipFileUploadForm):
@@ -97,16 +110,16 @@ class CoostoUpload(UploadScript):
 
     def _scrape_unit(self, row):
         row = {k: v.decode("utf-8") for k, v in row.iteritems()}
-        query = row.pop(self.lang.query)
+        query = self.lang.popfield(row, "query")
         self.queries.add(query)
-        medium = Medium.get_or_create(row.pop(self.lang.source))
-        date = row.pop(self.lang.date)
+        medium = Medium.get_or_create(self.lang.popfield(row, "source"))
+        date = self.lang.popfield(row, "date")
         date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M")
-        headline = row.pop(self.lang.title)
-        pagenr = row.pop(self.lang.reach) or None
-        text = row.pop(self.lang.message_text)
-        url = row.pop(self.lang.url)
-        author = row.pop(self.lang.author)
+        headline = self.lang.popfield(row, "title")
+        pagenr = self.lang.popfield(row, "reach") or None
+        text = self.lang.popfield(row, "message_text")
+        url = self.lang.popfield(row, "url")
+        author = self.lang.popfield(row, "author")
         metastring = json.dumps(row)
 
         a = Article(headline=headline, pagenr=pagenr,
@@ -120,6 +133,7 @@ class CoostoUpload(UploadScript):
             lang = next(lang for lang in self.languages if lang.matches(csvDictReader.fieldnames))
         except StopIteration:
             raise Exception("Couldn't find a matching language")
+        log.info("Coosto language detected: {}".format(lang.__name__))
         return lang
 
     def get_provenance(self, file, articles):
