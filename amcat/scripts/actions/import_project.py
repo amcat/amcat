266ba44bbdfda78cbd1830bdeccdddd6512f3e6d
Wouter van Atteveldt <wouter@vanatteveldt.com>
2017-08-25 16:42:39 +0200

    Add codingjobs and codingschemas to import_project

--- amcat/scripts/actions/import_project.py
+++ amcat/scripts/actions/import_project.py
@@ -22,7 +22,6 @@ class ImportProject(Script):
         from amcat.models import Project
         self.setids = {}  # old_id -> new_id
         self.articles = {}  # old_id -> new_id
-        self.codebooks = {}  # old_id -> new_id
         self.zipfile = ZipFile(file.file.name)
         self.project = project if project else Project.objects.create(name=file.file.name, owner_id=1)
         logging.info("Importing {file.file.name} into project {self.project.id}:{self.project.name}"
@@ -31,38 +30,35 @@ class ImportProject(Script):
         self.import_articles()
         self.import_articlesets_articles()
         self.import_codes()
-        self.import_codebooks()
+
+        self.codebooks = self.import_codebooks()
         self.import_codebookcodes()
+        self.import_codingschemas()
+
         logging.info("Done!")
 
-    def _get_dicts(self, fn):
-        if fn not in self.zipfile.namelist():
-            logging.info("No {fn}".format(**locals()))
-        else:
-            logging.info("Reading {fn}".format(**locals()))
-            for line in self.zipfile.open(fn):
-                yield json.loads(line.decode("utf-8"))
+    def _save_with_id_mapping(self, objects):
+        for o in objects:
+            old_id = o.object.pk
+            o.object.pk = None
+            if hasattr(o.object, "project_id"):
+                o.object.project_id = self.project.id
+            o.save()
+            yield old_id, o.object.pk
 
-    def _deserialize(self, fn):
-        if fn not in self.zipfile.namelist():
-            logging.info("No {fn}".format(**locals()))
-            return []
-        else:
-            result = list(serializers.deserialize("json", self.zipfile.open(fn)))
-            logging.info("Read {} entries from {fn}".format(len(result), **locals()))
-            return result
+    def import_codingschemas(self):
+        schemas = dict(self._save_with_id_mapping(self._deserialize("codingschemas.json")))
+        for f in self._deserialize("codingschemafields.json"):
+            f.object.pk = None
+            f.object.codingschema_id = schemas[f.object.codingschema_id]
+            f.save()
 
     def import_codes(self):
         Code.objects.bulk_create(c.object for c in self._deserialize("codes.json") if not c.object.pk)
         Label.objects.bulk_create(c.object for c in self._deserialize("labels.json") if not c.object.pk)
 
     def import_codebooks(self):
-        for c in self._deserialize("codebooks.json"):
-            old_id = c.object.pk
-            c.object.pk = None
-            c.object.project_id = self.project.id
-            c.save()
-            self.codebooks[old_id] = c.object.pk
+        return dict(self._save_with_id_mapping(self._deserialize("codebooks.json")))
 
     def import_codebookcodes(self):
         cbcodes = [c.object for c in self._deserialize("codebookcodes.json")]
@@ -90,6 +86,22 @@ class ImportProject(Script):
             aids = [self.articles[aid] for aid in a['articles']]
             setid = self.setids[a['articleset']]
             ArticleSet.objects.get(pk=setid).add_articles(aids)
+    def _get_dicts(self, fn):
+        if fn not in self.zipfile.namelist():
+            logging.info("No {fn}".format(**locals()))
+        else:
+            logging.info("Reading {fn}".format(**locals()))
+            for line in self.zipfile.open(fn):
+                yield json.loads(line.decode("utf-8"))
+
+    def _deserialize(self, fn):
+        if fn not in self.zipfile.namelist():
+            logging.info("No {fn}".format(**locals()))
+            return []
+        else:
+            result = list(serializers.deserialize("json", self.zipfile.open(fn)))
+            logging.info("Read {} entries from {fn}".format(len(result), **locals()))
+            return result
 
 
 if __name__ == '__main__':
