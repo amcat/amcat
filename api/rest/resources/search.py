eca529244a63b628e800eeeaf96f70d4b8fc08b5
Christian Stuart <c.j.stuart@students.uu.nl>
2017-08-26 12:20:07 +0200

    Add property fields and parameter to search resource

--- api/rest/resources/search.py
+++ api/rest/resources/search.py
@@ -21,12 +21,13 @@ import copy
 import functools
 import itertools
 import re
-from typing import Container
+from typing import Container, Mapping
 
 from django.http import QueryDict
 from django_filters import filters, filterset
 from rest_framework.exceptions import ParseError, NotFound, PermissionDenied
 from rest_framework.fields import CharField, IntegerField, DateTimeField
+from rest_framework import fields
 from rest_framework.serializers import Serializer
 
 from amcat.models import Project, Article, ROLE_PROJECT_METAREADER
@@ -41,6 +42,16 @@ FILTER_ID_FIELDS = frozenset({"ids", "pk"})
 
 RE_KWIC = re.compile("(?P<left>.*?)<mark>(?P<keyword>.*?)</mark>(?P<right>.*)", re.DOTALL)
 
+TYPE_FIELDS = {
+    "default": fields.CharField,
+    "int": fields.IntegerField,
+    "num": fields.FloatField,
+    "date": fields.DateTimeField,
+    "url": fields.CharField,
+    "id": fields.IntegerField,
+    "tag": fields.CharField,
+    "text": fields.CharField
+}
 
 class LazyES(object):
     def __init__(self, user=None, queries=None, filters=None, fields=None, hits=False):
@@ -174,6 +185,13 @@ class ScoreField(IntegerField):
         source = self.source
         return obj.get(source, 0)
 
+class DictValueField(CharField):
+    def __init__(self, key, **kwargs):
+        super().__init__(**kwargs)
+        self.key = key
+
+    def to_representation(self, value):
+        return super().to_representation(value[self.key])
 
 class SearchResourceSerialiser(Serializer):
     id = IntegerField()
@@ -213,6 +231,32 @@ class SearchResourceSerialiser(Serializer):
             self.fields['left'] = KWICField(kwic='left')
             self.fields['keyword'] = KWICField(kwic='keyword')
             self.fields['right'] = KWICField(kwic='right')
+
+        self.property_fields = self.get_property_fields(ctx.get("properties", []))
+
+    def get_property_fields(self, properties) -> Mapping[str, fields.Field]:
+        fields = {}
+        for property in properties:
+            try:
+                name, dt = property.split('_')
+            except ValueError:
+                fields[property] = TYPE_FIELDS['default']()
+            else:
+                fields[name] = TYPE_FIELDS[dt]
+        return fields
+
+    def get_fields(self):
+        fields = super().get_fields()
+        fields.update((k, v) for k, v in self.property_fields.items() if k not in fields)
+        return fields
+
+    def to_representation(self, instance: Article):
+        rep = super().to_representation(instance)
+        for property, field in self.property_fields.items():
+            if property in rep:
+                continue
+            rep[property] = field.to_representation(instance.properties.get(property))
+        return rep
 
     class Meta:
         model = Article
@@ -273,6 +317,11 @@ class SearchResource(AmCATResource):
     @cached
     def columns(self):
         return self.params.getlist("col")
+
+    @property
+    @cached
+    def properties(self):
+        return self.params.getlist("properties")
 
     @property
     @cached
@@ -349,6 +398,7 @@ class SearchResource(AmCATResource):
         ctx = super(SearchResource, self).get_serializer_context()
         ctx["queries"] = self.queries
         ctx["columns"] = self.columns
+        ctx["properties"] = self.properties
         ctx["minimal"] = self.params.get('minimal')
         return ctx
 
