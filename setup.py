b089d47dc13ac98b63fecd74bb36baa19a476541
Wouter van Atteveldt <wouter@vanatteveldt.com>
2019-11-23 18:33:55 +0100

    New pip version

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 import setuptools
 
 from os import path
-from pip.req import parse_requirements
-from pip.download import PipSession
+try:
+    from pip.req import parse_requirements
+except ImportError:
+    # We should probably move away from this!
+    from pip._internal.req import parse_requirements
+
+try:
+    from pip.download import PipSession
+except ImportError:
+    from pip._internal.download import PipSession
+        
 
 here = path.abspath(path.join(path.dirname(path.abspath(__file__))))
 
